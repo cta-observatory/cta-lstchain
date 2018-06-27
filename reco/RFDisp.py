a3fa290a4749d861047c8b2603a3d26cb64938f3
misabelber <m.isabel.bernardos@gmail.com>
2018-06-27 17:14:33 +0200

    Add the possibility of store events in hdf5 format

--- reco/RFDisp.py
+++ reco/RFDisp.py
@@ -17,12 +17,14 @@ import scipy
 import Disp
 import sys
 import pandas as pd
+import h5py
 from astropy.table import Table
 
 
 #Read data into pandas DataFrame
-file = "/home/queenmab/DATA/LST1/Events/Gamma_events.fits" #File with events
-dat = Table.read(file,format='fits')
+filetype = 'hdf5'
+filename = "/home/queenmab/DATA/LST1/Events/gamma_events.hdf5" #File with events
+dat = Table.read(filename,format=filetype)
 df = dat.to_pandas()
 
 #Get some telescope parameters
