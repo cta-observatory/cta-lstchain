4aba6c96618bf785aaafb4a5077a2859181568c7
misabelber <m.isabel.bernardos@gmail.com>
2018-09-12 13:48:58 +0200

    Add tool for position reco with disp, and theta2 plotting

--- reco/RFGHseparation.py
+++ reco/RFGHseparation.py
@@ -27,8 +27,8 @@ import h5py.highlevel
 filetype = 'hdf5'
 gammafile = "/home/queenmab/DATA/LST1/Events/gamma_events_point.hdf5" #File with events
 protonfile = "/home/queenmab/DATA/LST1/Events/proton_events_diff.hdf5" #File with events
-dat_gamma = Table.read(gammafile,format=filetype)
-dat_proton = Table.read(protonfile,format=filetype)
+dat_gamma = Table.read(gammafile,format=filetype,path="gamma")
+dat_proton = Table.read(protonfile,format=filetype,path="proton")
 
 df_gamma = dat_gamma.to_pandas()
 df_proton = dat_proton.to_pandas()
@@ -60,6 +60,10 @@ disp_proton = Disp.calc_DISP(sourcepos_proton[0],
 
 
 #Add dist to the DataFrame
+df_gamma['SrcX'] = sourcepos_gamma[0]
+df_gamma['SrcY'] = sourcepos_gamma[1]
+df_proton['SrcX'] = sourcepos_proton[0]
+df_proton['SrcY'] = sourcepos_proton[1] 
 df_gamma['disp'] = disp_gamma
 df_proton['disp'] = disp_proton
 df_gamma['hadroness'] = np.zeros(df_gamma.shape[0])
@@ -68,6 +72,10 @@ df_proton['hadroness'] = np.ones(df_proton.shape[0])
 #Cut events in the border:
 df_gamma = df_gamma[abs(df_gamma['r'])<0.94]
 df_proton = df_proton[abs(df_proton['r'])<0.94]
+
+#Cut showers with low intensity
+df_gamma = df_gamma[abs(df_gamma['intensity'])>60]
+df_proton = df_proton[abs(df_proton['intensity'])>60]
 
 # Set Training and Test sets
 df_gamma['is_train'] = np.random.uniform(0,1,len(df_gamma))<= 0.5
@@ -97,37 +105,64 @@ regr_rf_disp = RandomForestRegressor(max_depth=max_depth, random_state=2,n_estim
 regr_rf_disp.fit(train_gammas[features], train_gammas['disp'])
 disprec = regr_rf_disp.predict(test[features])
 
+#Reconstruct position of the Source from Disp
+
+posdisp = Disp.Disp_to_Pos(test['disp'],test['x'],test['y'],test['psi'])
+theta2_true = (test['SrcX']-posdisp[0])**2+(test['SrcY']-posdisp[1])**2
+posrec = Disp.Disp_to_Pos(disprec,test['x'],test['y'],test['psi'])
+theta2_reco = (test['SrcX']-posrec[0])**2+(test['SrcY']-posrec[1])**2
+
+test['theta2_true'] = theta2_true
+test['theta2_reco'] = theta2_reco
+
 #Plot Energy and Disp reconstructions
 plt.subplot(121)
 difE = (((test['mcEnergy']-erec)/test['mcEnergy'])*np.log10(10))
 print(difE.mean(),difE.std())
 plt.hist(difE,bins=100)
-plt.xlabel('$\\frac{E_{test}-E_{rec}}{E_{test}}$',fontsize=30)
+plt.xlabel('$\\frac{E_{test}-E_{rec}}{E_{test}}$',fontsize=15)
 plt.figtext(0.6,0.7,'Mean: '+str(round(scipy.stats.describe(difE).mean,6)),fontsize=15)
 plt.figtext(0.6,0.65,'Variance: '+str(round(scipy.stats.describe(difE).variance,6)),fontsize=15)
 
 plt.subplot(122)
 hE = plt.hist2d(test['mcEnergy'],erec,bins=100)
 plt.colorbar(hE[3])
-plt.xlabel('$log_{10}E_{test}$',fontsize=24)
-plt.ylabel('$log_{10}E_{rec}$',fontsize=24)
+plt.xlabel('$log_{10}E_{test}$',fontsize=15)
+plt.ylabel('$log_{10}E_{rec}$',fontsize=15)
 plt.plot(test['mcEnergy'],test['mcEnergy'],"-",color='red')
 plt.show()
 
-plt.subplot(121)
+plt.subplot(221)
 difD = ((test['disp']-disprec)/test['disp'])
 print(difD.mean(),difD.std())
-plt.hist(difD,bins=100,range=[-10,5])
-plt.xlabel('$\\frac{Disp_{test}-Disp_{rec}}{Disp_{test}}$',fontsize=30)
+plt.hist(difD,bins=100,range=[-2,1.5])
+plt.xlabel('$\\frac{Disp_{test}-Disp_{rec}}{Disp_{test}}$',fontsize=15)
 plt.figtext(0.6,0.7,'Mean: '+str(round(scipy.stats.describe(difD).mean,6)),fontsize=15)
 plt.figtext(0.6,0.65,'Variance: '+str(round(scipy.stats.describe(difD).variance,6)),fontsize=15)
 
-plt.subplot(122)
+plt.subplot(222)
 hD = plt.hist2d(test['disp'],disprec,bins=100,range=([0,2],[0,2]))
 plt.colorbar(hD[3])
-plt.xlabel('$Disp_{test}$',fontsize=24)
-plt.ylabel('$Disp_{rec}$',fontsize=24)
+plt.xlabel('$Disp_{test}$',fontsize=15)
+plt.ylabel('$Disp_{rec}$',fontsize=15)
 plt.plot(test['disp'],test['disp'],"-",color='red')
+
+plt.subplot(223)
+plt.hist(test['theta2_true'],bins=50,range=[0,60],histtype=u'step',label=r'With Hillas Disp')
+plt.hist(test['theta2_reco'],bins=50,range=[0,60],histtype=u'step',label=r'With Reconstructed Disp')
+plt.yscale('log')
+plt.legend()
+plt.xlabel(r'$\theta^{2}$',fontsize=15)
+plt.ylabel(r'# of Gamma and Proton events',fontsize=15)
+
+plt.subplot(224)
+plt.hist(test[test['hadroness']<1]['theta2_true'],bins=50,range=[0,20],histtype=u'step',label=r'With Hillas Disp')
+plt.hist(test[test['hadroness']<1]['theta2_reco'],bins=50,range=[0,20],histtype=u'step',label=r'With Reconstructed Disp')
+plt.yscale('log')
+plt.legend()
+plt.xlabel(r'$\theta^{2}$',fontsize=15)
+plt.ylabel(r'# of Only Gamma events',fontsize=15)
+
 plt.show()
 
 #Perform Gamma/Hadron separation:
@@ -135,7 +170,7 @@ plt.show()
 #Now we build a new training set with reconstructed Energy and Disp:
 
 #Set a cut in energy, 0 for no cuts.
-Energy_cut = 0
+Energy_cut = -1.
 
 test['Erec'] = erec
 test['Disprec'] = disprec
@@ -173,7 +208,7 @@ for index in indices:
     ordered_features=ordered_features+[features[index]]
 
 plt.subplot(121)
-plt.title("Feature importances for G/H separation")
+plt.title("Feature importances for G/H separation",fontsize=15)
 plt.bar(range(len(features)), importances[indices],
        color="r", yerr=std[indices], align="center")
 plt.xticks(range(len(features)), ordered_features)
@@ -188,9 +223,15 @@ fpr_rf, tpr_rf, _ = roc_curve(test['hadroness'], check)
 
 plt.subplot(122)
 plt.plot(fpr_rf, tpr_rf, label='Energy Cut: '+'%.3f'%(pow(10,Energy_cut)/1000)+' TeV')
-plt.xlabel('False positive rate')
-plt.ylabel('True positive rate')
+plt.xlabel('False positive rate',fontsize=15)
+plt.ylabel('True positive rate',fontsize=15)
 plt.legend(loc='best')
 plt.show()
 
-
+plt.hist(test[test['hadroness']<1]['theta2_true'],bins=50,range=[0,2],histtype=u'step',label =r'With Hillas Disp')
+plt.hist(test[test['hadroness']<1]['theta2_reco'],bins=50,range=[0,2],histtype=u'step',label =r'With Reconstructed Disp')
+plt.yscale('log')
+plt.xlabel(r'$\theta^{2}$',fontsize=15)
+plt.ylabel(r'# Gamma events (after g/h separation)',fontsize=15)
+plt.legend()
+plt.show()
