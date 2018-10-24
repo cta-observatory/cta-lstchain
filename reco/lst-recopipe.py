fb086fa7efbec2495050795f205d8da86ea5d9be
misabelber <m.isabel.bernardos@gmail.com>
2018-10-24 11:05:06 +0200

    Changed name of the modules. Adapted the code to a more readable format. Eliminated local paths

--- reco/lst-recopipe.py
+++ reco/lst-recopipe.py
@@ -1,26 +1,84 @@
-import parameters
-import reconstruction
+"""Pipeline for reconstruction of Energy, Disp and gamma/hadron
+separation of events stored in a simtelarray file.
+Result is a dataframe with dl2 data.
+Already trained Random Forests are required.
+
+Usage:
+
+$> python lst-recopipe arg1 arg2 ...
+
+"""
+import calib_dl0_to_dl1
+import reco_dl1_to_dl2
+import plot_dl2
 from sklearn.externals import joblib
+from ctapipe.utils import get_dataset_path
+import matplotlib.pyplot as plt 
+import argparse
 
-#Simtelarray file with data
+parser = argparse.ArgumentParser(description = "Train Random Forests.")
 
-datafile = '/scratch/bernardos/LST1/Gamma/Point_Prod-3_LaPalma_flashcam-prod3j/gamma_20deg_0deg_run11716___cta-prod3-lapalma-2147m-LaPalma-FlashCam.simtel.gz'
+# Required arguments
+parser.add_argument('--datafile', '-f', type=str,
+                    dest='datafile',
+                    help='path to the file with simtelarray events',
+                    default=get_dataset_path('gamma_test_large.simtel.gz'))
 
-#Get out the data from the Simtelarray file:
+parser.add_argument('--pathmodels', '-p', action='store', type=str,
+                     dest='path_models',
+                     help='Path where to find the trained RF',
+                     default='./results/')
+parser.add_argument('--storeresults', '-s', action='store', type=bool,
+                    dest='storeresults',
+                    help='Boolean. True for storing the reco dl2 events'
+                    'Default=False, any user input will be considered True',
+                    default=False)
+# Optional argument
+parser.add_argument('--outdir', '-o', action='store', type=str,
+                     dest='outdir',
+                     help='Path where to store the reco dl2 events',
+                     default='./results/')
 
-data = parameters.get_events(datafile,False)
+args = parser.parse_args()
 
-#Load the trained RF for reconstruction:
-path_models = "/scratch/bernardos/LST1/Models/"
-fileE = path_models+"RFreg_Energy.sav"
-fileD = path_models+"RFreg_Disp.sav"
-fileH = path_models+"RFcls_GH.sav"
+if __name__ == '__main__':
 
-RFreg_Energy = joblib.load(fileE)
-RFreg_Disp = joblib.load(fileD)
-RFcls_GH = joblib.load(fileH)
+    #Get out the data from the Simtelarray file:
 
-#Apply the models to the data
-features = ['intensity','time_gradient','width','length','w/l','phi','psi']
-reconstruction.ApplyModels(data,features,RFcls_GH,RFreg_Energy,RFreg_Disp)
+    data = calib_dl0_to_dl1.get_events(args.datafile,False)
 
+    #Load the trained RF for reconstruction:
+    fileE = args.path_models+"RFreg_Energy.sav"
+    fileD = args.path_models+"RFreg_Disp.sav"
+    fileH = args.path_models+"RFcls_GH.sav"
+    
+    RFreg_Energy = joblib.load(fileE)
+    RFreg_Disp = joblib.load(fileD)
+    RFcls_GH = joblib.load(fileH)
+    
+    #Apply the models to the data
+    features = ['intensity',
+                'time_gradient',
+                'width',
+                'length',
+                'w/l',
+                'phi',
+                'psi']
+    dl2 = data
+    reco_dl1_to_dl2.ApplyModels(data,dl2,features,RFcls_GH,RFreg_Energy,RFreg_Disp)
+
+    if args.storeresults==True:
+        #Store results
+        outfile = args.outdir+"dl2_events.hdf5"
+        dl2.to_hdf(outfile,key="dl2_events",mode="w")
+
+    #Plot some results
+        
+    plot_dl2.plot_features(dl2)
+    plt.show()
+    plot_dl2.plot_E(dl2)
+    plt.show()
+    plot_dl2.plot_Disp(dl2)
+    plt.show()
+    plot_dl2.plot_pos(dl2)
+    plt.show()
