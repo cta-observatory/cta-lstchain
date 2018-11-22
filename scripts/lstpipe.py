044a92f63333094318d3f45617ef47d0ab557a59
Vuillaume <thomas.vuillaume@lapp.in2p3.fr>
2018-11-22 20:38:33 +0100

    removed comment on time_gradient features

--- scripts/lstpipe.py
+++ scripts/lstpipe.py
@@ -56,7 +56,7 @@ if __name__ == '__main__':
     #Train the models
     
     features = ['intensity',
-                # 'time_gradient',
+                'time_gradient',
                 'width',
                 'length',
                 'wl',
