fed09d4840b1ed6f72c0e8d4b067bc454bf563f8
rlopezcoto <rlopezcoto@gmail.com>
2018-11-27 23:19:52 +0100

    Update muon reconstruction script

--- scripts/muon_reconstruction.py
+++ scripts/muon_reconstruction.py
@@ -2,7 +2,7 @@
 Example to load raw data (hessio format), calibrate and reconstruct muon
 ring parameters, and write some parameters to an output table
 """
-
+import numpy as np
 import warnings
 from astropy.table import Table
 from ctapipe.calib import CameraCalibrator
@@ -67,11 +67,21 @@ class MuonDisplayerTool(Tool):
         output_parameters = {'MuonEff': [],
                              'ImpactP': [],
                              'RingWidth': [],
-                             'AngCont': []}
+                             'RingCont': [],
+                             'RingComp': [],
+                             'RingPixComp': [],
+                             'Core_x': [],
+                             'Core_y': [],
+                             'Impact_x_arr': [],
+                             'Impact_y_arr': [],
+                             'MCImpactP': [],
+                             'ImpactDiff': [],
+                             'RingSize': [],
+                             'RingRadius': [],
+                             'NTels': []}
 
         numev = 0
         num_muons_found = 0
-
 
         for event in event_source(self.infile):
             self.log.info("Event Number: %d, found %d muons", numev, num_muons_found)
@@ -86,9 +96,34 @@ class MuonDisplayerTool(Tool):
                 if self.display:
                     plot_muon_event(event, muon_evt)
 
+                ntels = len( event.r0.tels_with_data)
+                #if(len( event.r0.tels_with_data) <= 1):
+                    #continue
+                #print("event.r0.tels_with_data", event.r0.tels_with_data)
                 for tid in muon_evt['TelIds']:
                     idx = muon_evt['TelIds'].index(tid)
                     if muon_evt['MuonIntensityParams'][idx] is not None:
+                        print("pos, tid", event.inst.subarray.positions[tid], tid)
+                        tel_x = event.inst.subarray.positions[tid][0]
+                        tel_y = event.inst.subarray.positions[tid][1]
+                        core_x = event.mc.core_x  # MC Core x
+                        core_y = event.mc.core_y  # MC Core y
+                        rec_impact_x = muon_evt['MuonIntensityParams'][idx].impact_parameter_pos_x
+                        rec_impact_y = muon_evt['MuonIntensityParams'][idx].impact_parameter_pos_y
+                        print("rec_impact_x, rec_impact_y", rec_impact_x, rec_impact_y)
+                        print("event.mc.core_x, event.mc.core_y",
+                              event.mc.core_x, event.mc.core_y)
+                        impact_mc = np.sqrt(np.power(core_x - tel_x, 2) +
+                                         np.power(core_y - tel_y, 2))
+                        print("simulated impact", impact_mc)
+                        # Coordinate transformation to move the impact point to array coordinates
+                        rec_impact_x_arr = tel_x + rec_impact_x 
+                        rec_impact_y_arr = tel_y + rec_impact_y
+                        print("impact_x_arr, impact_y_arr", rec_impact_x_arr, rec_impact_y_arr)
+                        # Distance between core of the showe and impact parameter
+                        impact_diff = np.sqrt(np.power(core_x - rec_impact_x_arr, 2) +
+                                              np.power(core_y - rec_impact_y_arr, 2))
+                        print("impact_diff ",impact_diff )
                         self.log.info("** Muon params: %s",
                                       muon_evt['MuonIntensityParams'][idx])
 
@@ -101,9 +136,43 @@ class MuonDisplayerTool(Tool):
                         output_parameters['RingWidth'].append(
                             muon_evt['MuonIntensityParams'][idx].ring_width.value
                         )
-                        output_parameters['AngCont'].append(
+                        output_parameters['RingCont'].append(
                             muon_evt['MuonRingParams'][idx].ring_containment
                         )
+                        output_parameters['RingComp'].append(
+                            muon_evt['MuonIntensityParams'][idx].ring_completeness
+                        )
+                        output_parameters['RingPixComp'].append(
+                            muon_evt['MuonIntensityParams'][idx].ring_pix_completeness
+                        )
+                        output_parameters['Core_x'].append(
+                            event.mc.core_x.value
+                        )
+                        output_parameters['Core_y'].append(
+                            event.mc.core_y.value
+                        )
+                        output_parameters['Impact_x_arr'].append(
+                            rec_impact_x_arr.value
+                        )
+                        output_parameters['Impact_y_arr'].append(
+                            rec_impact_y_arr.value
+                        )
+                        output_parameters['MCImpactP'].append(
+                            impact_mc.value
+                        )
+                        output_parameters['ImpactDiff'].append(
+                            impact_diff.value
+                        )
+                        output_parameters['RingSize'].append(
+                            muon_evt['MuonIntensityParams'][idx].ring_size
+                        )
+                        output_parameters['RingRadius'].append(
+                            muon_evt['MuonRingParams'][idx].ring_radius.value
+                        )
+                        output_parameters['NTels'].append(
+                            ntels
+                        )
+
                         print_muon(muon_evt, printer=self.log.info)
                         num_muons_found += 1
 
@@ -112,6 +181,7 @@ class MuonDisplayerTool(Tool):
         t = Table(output_parameters)
         t['ImpactP'].unit = 'm'
         t['RingWidth'].unit = 'deg'
+        #t['MCImpactP'].unit = 'm'
         if self.outfile:
             t.write(self.outfile)
 
