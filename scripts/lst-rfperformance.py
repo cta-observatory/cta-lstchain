4cfb7c77de488d2c25de221ccffd0afccc01d7c7
misabelber <m.isabel.bernardos@gmail.com>
2019-01-15 14:51:08 +0100

    New rfperformance

--- scripts/lst-rfperformance.py
+++ scripts/lst-rfperformance.py
@@ -21,6 +21,7 @@ from lstchain.reco import utils
 parser = argparse.ArgumentParser(description = "Train Random Forests.")
 
 # Required argument
+'''
 parser.add_argument('--gammafile', '-fg', type=str,
                     dest='gammafile',
                     help='path to the dl1 file of gamma events for training')
@@ -28,6 +29,11 @@ parser.add_argument('--gammafile', '-fg', type=str,
 parser.add_argument('--protonfile', '-fp', type=str,
                     dest='protonfile',
                     help='path to the dl1 file of proton events for training')
+'''
+parser.add_argument('--inpath', '-i', action='store', type=str,
+                     dest='path_sets',
+                     help='Path to sets for train/test',
+                     default='../../cta-lstchain-extra/reco/models/sample_data/dl1')
 
 parser.add_argument('--storerf', '-s', action='store', type=bool,
                     dest='storerf',
@@ -51,40 +57,44 @@ if __name__ == '__main__':
                 'width',
                 'length',
                 'wl',
-                'phi',
-                'psi']
+                'skewness',
+                'kurtosis',
+                'impact']
+    train_gfile = args.path_sets+'/gammas_train.h5'
+    train_sepfile = args.path_sets+'/train_separation.h5'
+    test_sepfile = args.path_sets+'/test_separation.h5'
 
-    #Split data in train and test events:
-    df_gammas = pd.read_hdf(args.gammafile,
-                            key="gamma_events")
-    df_proton = pd.read_hdf(args.protonfile,
-                            key="proton_events")
+    train_g = pd.read_hdf(train_gfile)
+    train_sep = pd.read_hdf(train_sepfile)
+    test_sep = pd.read_hdf(test_sepfile)
     
-    train,test = reco_dl1_to_dl2.split_traintest(df_gammas,0.5)
-    test = test.append(df_proton,
-                       ignore_index=True)
+    RFreg_Energy, RFreg_Disp = reco_dl1_to_dl2.trainRFreco(train_g,features)
+    train_sep['e_rec'] = RFreg_Energy.predict(train_sep[features])
+    train_sep['disp_rec'] = RFreg_Energy.predict(train_sep[features])
+    test_sep['e_rec'] = RFreg_Energy.predict(test_sep[features])
+    test_sep['disp_rec'] = RFreg_Disp.predict(test_sep[features])
 
-    RFreg_Energy, RFreg_Disp = reco_dl1_to_dl2.trainRFreco(train,features)
-    
-    test['e_rec'] = RFreg_Energy.predict(test[features])
-    test['disp_rec'] = RFreg_Disp.predict(test[features])
-
-    test['src_x_rec'],test['src_y_rec'] = utils.disp_to_pos(test['disp_rec'],
-                                                      test['x'],
-                                                      test['y'],
-                                                      test['psi'])
+    train_sep['src_x_rec'],train_sep['src_y_rec'] = utils.disp_to_pos(train_sep['disp_rec'],
+                                                      train_sep['x'],
+                                                      train_sep['y'],
+                                                      train_sep['psi'])
+    test_sep['src_x_rec'],test_sep['src_y_rec'] = utils.disp_to_pos(test_sep['disp_rec'],
+                                                      test_sep['x'],
+                                                      test_sep['y'],
+                                                      test_sep['psi'])
     
     features_sep = list(features)
     features_sep.append('e_rec')
     features_sep.append('disp_rec')
-    
-    train,test = reco_dl1_to_dl2.split_traintest(test,0.75)
+        
     #Train the Classifier
     
-    RFcls_GH = reco_dl1_to_dl2.trainRFsep(train,
+    RFcls_GH = reco_dl1_to_dl2.trainRFsep(train_sep,
                           features_sep)
 
-    test['hadro_rec'] = RFcls_GH.predict(test[features_sep])
+    test_sep['prob'] = RFcls_GH.predict_proba(test_sep[features_sep])[0:,1]
+    #test_sep['hadro_rec'] = RFcls_GH.predict(test_sep[features_sep])
+    test_sep['hadro_rec'] = test_sep['prob']>0.4
     
     if args.storerf==True:
         fileE = args.path_models+"/RFreg_Energy.sav"
@@ -96,19 +106,22 @@ if __name__ == '__main__':
 
     
     #Plot some results
-    e_cuts = [-1,np.log10(500),np.log10(1000)]
-    
+    e_cuts = [-1,np.log10(250),np.log10(500),np.log10(1000)]
+
     for e_cut in e_cuts:
-        test = test[test['e_rec']>e_cut]
-        plot_dl2.plot_features(test)
+        test_sep = test_sep[test_sep['e_rec']>e_cut]
+        plt.hist(test_sep[test_sep['hadroness']==0]['prob'],bins=50)
+        plt.hist(test_sep[test_sep['hadroness']==1]['prob'],bins=50)
         plt.show()
-        plot_dl2.plot_e(test)
+        plot_dl2.plot_features(test_sep)
         plt.show()
-        plot_dl2.plot_disp(test)
+        plot_dl2.plot_e(test_sep)
         plt.show()
-        plot_dl2.plot_pos(test)
+        plot_dl2.plot_disp(test_sep)
+        plt.show()
+        plot_dl2.plot_pos(test_sep)
         plt.show()
         plot_dl2.plot_importances(RFcls_GH,features_sep)
         plt.show()
-        plot_dl2.plot_ROC(RFcls_GH,test,features_sep,e_cut)
+        plot_dl2.plot_ROC(RFcls_GH,test_sep,features_sep,e_cut)
         plt.show()
