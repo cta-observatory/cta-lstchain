a33298141edeeb37e0dd15abd71ee7129769b839
Franca Cassol <cassol@cppm.in2p3.fr>
2019-02-12 18:52:49 +0100

    Moved old containers module to lstcontainers module Added to lstchain the last version of the lsteventsource module (eventually to be PR to ctapipe) Added a container module with the present version of the monitoring data container:
    lstchain.io.containers.MonitoringDataContainer:
                       flatfield.*: Relative flat field data
                        pedestal.*: Pedestal data
                    pixel_status.*: Container of masks with pixel status

--- lstchain/io/containers.py
+++ lstchain/io/containers.py
@@ -1,154 +1,268 @@
 """
-Functions to handle custom containers for the mono reconstruction of LST1
+Container structures for data that should be read or written to disk
 """
 
-import astropy.units as u
-from astropy.units import Quantity
-import numpy as np
-from ctapipe.core import Container, Field
-from ctapipe.image import timing_parameters as time
-from ..reco import utils
-from numpy import nan
+from astropy import units as u
+#from astropy.time import Time
+#from numpy import nan
+#import numpy as np
+
+from ctapipe.core import Container, Field, Map
+from ctapipe.io.containers import *
+
+__all__ = [
+    'FlatFieldCameraContainer',
+    'FlatFieldContainer',
+    'PedestalCameraContainer',
+    'PedestalContainer',
+    'PixelStatusCameraContainer',
+    'PixelStatusContainer',
+    'LSTCameraContainer',
+    'MonitoringDataContainer',
+    'LSTDataContainer'
+]
 
 
-class DL1ParametersContainer(Container):
+class R0CameraContainer(Container):
     """
-    TODO: maybe fields could be inherited from ctapipe containers definition
-        For now I have not found an elegant way to do so
+    Storage of raw data from a single telescope
     """
-    intensity = Field(None, 'total intensity (size)')
-
-    x = Field(None, 'centroid x coordinate', unit=u.m)
-    y = Field(None, 'centroid x coordinate', unit=u.m)
-    r = Field(None, 'radial coordinate of centroid', unit=u.m)
-    phi = Field(None, 'polar coordinate of centroid', unit=u.rad)
-    length = Field(None, 'RMS spread along the major-axis', unit=u.m)
-    width = Field(None, 'RMS spread along the minor-axis', unit=u.m)
-    psi = Field(None, 'rotation angle of ellipse', unit=u.rad)
-    skewness = Field(None, 'measure of the asymmetry')
-    kurtosis = Field(None, 'measure of the tailedness')
-    disp_norm = Field(None, 'disp_norm [m]', unit=u.m)
-    disp_dx = Field(None, 'disp_dx [m]', unit=u.m)
-    disp_dy = Field(None, 'disp_dy [m]', unit=u.m)
-    disp_angle = Field(None, 'disp_angle [rad]', unit=u.rad)
-    disp_sign = Field(None, 'disp_sign')
-    disp_miss = Field(None, 'disp_miss [m]', unit=u.m)
-    hadroness = Field(None, 'hadroness')
-    src_x = Field(None, 'source x coordinate in camera frame', unit=u.m)
-    src_y = Field(None, 'source y coordinate in camera frame', unit=u.m)
-    time_gradient = Field(None, 'Time gradient in the camera')
-    intercept = Field(None, 'Intercept')
-
-    obs_id = Field(None, 'Observation ID')
-    event_id = Field(None, 'Event ID')
-    gps_time = Field(None, 'GPS time event trigger')
-
-    mc_energy = Field(None, 'Simulated Energy', unit=u.TeV)
-    mc_alt = Field(None, 'Simulated altitude', unit=u.rad)
-    mc_az = Field(None, 'Simulated azimuth', unit=u.rad)
-    mc_core_x = Field(None, 'Simulated impact point x position', unit=u.m)
-    mc_core_y = Field(None, 'Simulated impact point y position', unit=u.m)
-    mc_h_first_int = Field(None, 'Simulated first interaction height', unit=u.m)
-    mc_type = Field(None, 'Simulated particle type')
-    mc_az_tel = Field(None, 'Telescope altitude pointing', unit=u.rad)
-    mc_alt_tel = Field(None, 'Telescope azimuth pointing', unit=u.rad)
-    mc_x_max = Field(None, "MC Xmax value", unit=u.g / (u.cm ** 2))
-    mc_core_distance = Field(None, "Distance from the impact point to the telescope", unit=u.m)
-    mc_shower_primary_id = Field(None, "MC shower primary ID 0 (gamma), 1(e-),"
-                                    "2(mu-), 100*A+Z for nucleons and nuclei,"
-                                    "negative for antimatter.")
-
-    hadroness = Field(None, "Hadroness")
-    wl = Field(None, "width/length")
-
-    def fill_hillas(self, hillas):
-        """
-        fill Hillas parameters
-
-        hillas: HillasContainer
-        # TODO : parameters should not be simply copied but inherited
-        (e.g. conserving unit definition)
-        """
-        for k in hillas.keys():
-            try:
-                self[k] = hillas[k]
-            except:
-                print("{} cannot be copied in container".format(k))
-                pass
-
-    def fill_mc(self, event):
-        """
-        fill from mc
-        """
-        try:
-            self.mc_energy = event.mc.energy
-            self.mc_alt = event.mc.alt
-            self.mc_az = event.mc.az
-            self.mc_core_x = event.mc.core_x
-            self.mc_core_y = event.mc.core_y
-            self.mc_h_first_int = event.mc.h_first_int
-            # mcType = event.mc. # TODO: find type in event
-            self.mc_x_max = event.mc.x_max
-            self.mc_alt_tel = event.mcheader.run_array_direction[1]
-            self.mc_az_tel = event.mcheader.run_array_direction[0]
-        except:
-            print("mc information not filled")
-
-    def fill_event_info(self, event):
-        try:
-            self.gps_time = event.trig.gps_time
-            self.obs_id = event.r0.obs_id
-            self.event_id = event.r0.event_id
-        except:
-            print("event information not filled")
-
-    def get_features(self, features_names):
-        return np.array([self[k].value if isinstance(self[k], Quantity) else self[k]
-                         for k in features_names])
-
-    def set_mc_core_distance(self, event, telescope_id):
-        tel_pos = event.inst.subarray.positions[telescope_id]
-        distance = np.sqrt((event.mc.core_x - tel_pos[0]) ** 2 + (event.mc.core_y - tel_pos[1]) ** 2)
-        self.mc_core_distance = distance
-
-    def set_disp(self, source_pos, hillas):
-        disp = utils.disp_parameters(hillas, source_pos[0], source_pos[1])
-        self.disp_norm = disp.norm
-        self.disp_dx = disp.dx
-        self.disp_dy = disp.dy
-        self.disp_angle = disp.angle
-        self.disp_sign = disp.sign
-        self.disp_miss = disp.miss
-
-    def set_timing_features(self, geom, image, peakpos, hillas):
-        try:
-            peak_time = Quantity(peakpos) * u.Unit("ns")
-            timepars = time.timing_parameters(geom, image, peak_time, hillas)
-            self.time_gradient = timepars['slope'].value
-            self.intercept = timepars['intercept']
-        except:
-            pass
-
-    def set_source_camera_position(self, event, telescope_id):
-        # sourcepos = utils.cal_cam_source_pos(mc_alt, mc_az,
-        #                                      mc_alt_tel, mc_az_tel,
-        #                                      focal_length)
-        # self.src_x = sourcepos[0]
-        # self.src_y = sourcepos[1]
-        tel = event.inst.subarray.tel[telescope_id]
-        source_pos = utils.get_event_pos_in_camera(event, tel)
-        self.src_x = source_pos[0]
-        self.src_y = source_pos[1]
+    trigger_time = Field(None, "Telescope trigger time, start of waveform "
+                               "readout, None for MCs")
+    trigger_type = Field(0o0, "camera's event trigger type if applicable")
+    num_trig_pix = Field(0, "Number of trigger groups (sectors) listed")
+    trig_pix_id = Field(None, "pixels involved in the camera trigger")
+    image = Field(None, (
+        "numpy array containing integrated ADC data "
+        "(n_channels x n_pixels) DEPRECATED"
+    ))  # to be removed, since this doesn't exist in real data and useless in mc
+    waveform = Field(None, (
+        "numpy array containing ADC samples"
+        "(n_channels x n_pixels, n_samples)"
+    ))
+    num_samples = Field(None, "number of time samples for telescope")
+    pixel_status = Field(0o0, "status of the pixels")
 
 
-class DispContainer(Container):
+class FlatFieldCameraContainer(Container):
     """
-    Disp vector container
-    """
-    dx = Field(nan, 'x coordinate of the disp_norm vector')
-    dy = Field(nan, 'y coordinate of the disp_norm vector')
+    Container for relative camera flat-field coefficients
 
-    angle = Field(nan, 'Angle between the X axis and the disp_norm vector')
-    norm = Field(nan, 'Norm of the disp_norm vector')
-    sign = Field(nan, 'Sign of the disp_norm')
-    miss = Field(nan, 'miss parameter norm')
+    """
+
+    time_mean = Field(0, 'Mean time, seconds since reference', unit=u.s)
+    time_range = Field(
+        [],
+        'Range of time of the calibration data [t_min, t_max]',
+        unit=u.s
+    )
+    n_events = Field(0, 'Number of events used for statistics')
+    relative_gain_mean = Field(
+        None,
+        "np array of the relative flat-field coefficient mean (n_chan X N_pix)"
+    )
+    relative_gain_median = Field(
+        None,
+        "np array of the relative flat-field coefficient  median (n_chan X N_pix)"
+    )
+    relative_gain_rms = Field(
+        None,
+        "np array of the relative flat-field coefficient rms (n_chan X N_pix)"
+    )
+    relative_time_mean = Field(
+        None,
+        "np array of the relative time mean (n_chan X N_pix)",
+        unit=u.ns
+    )
+    relative_time_median = Field(
+        None,
+        "np array of the relative time  median (n_chan X N_pix)",
+        unit=u.ns)
+
+
+class FlatFieldContainer(Container):
+    """
+    Container for relative flat field coefficients
+    """
+    tels_with_data = Field([], "list of telescopes with data")
+    tel = Field(
+        Map(FlatFieldCameraContainer),
+        "map of tel_id to FlatFiledCameraContainer")
+
+
+class PedestalCameraContainer(Container):
+    """
+    Container for pedestals per camera
+    """
+    time_mean = Field(0, 'Mean time, seconds since reference', unit=u.s)
+    time_range = Field(
+        [],
+        'Range of time of the calibration data [t_min, t_max]',
+        unit=u.s
+    )
+    n_events = Field(0, 'Number of events used for statistics')
+    pedestal_mean = Field(
+        None,
+        "np array of pedestal average (n_chan X N_pix)"
+    )
+    pedestal_median = Field(
+        None,
+        "np array of the pedestal  median (n_chan X N_pix)"
+    )
+    pedestal_rms = Field(
+        None,
+        "np array of the pedestal rms (n_chan X N_pix)"
+    )
+    relative_pedestal_mean = Field(
+        None,
+        "np array of relative pedestal average (n_chan X N_pix)"
+    )
+    relative_pedestal_median = Field(
+        None,
+        "np array of the relative pedestal  median (n_chan X N_pix)"
+    )
+    relative_pedestal_rms = Field(
+        None,
+        "np array of the relative pedestal rms (n_chan X N_pix)"
+    )
+
+
+class PedestalContainer(Container):
+    """
+    Container for pedestal data
+    """
+    tels_with_data = Field([], "list of telescopes with data")
+    tel = Field(
+        Map(PedestalCameraContainer),
+        "map of tel_id to PedestalCameraContainer")
+
+
+class PixelStatusCameraContainer(Container):
+    hardware_mask = Field(
+        None,
+        "Mask from the hardware pixel status data (N_pix)"
+    )
+
+    pedestal_mask = Field(
+        None,
+        "Mask from the pedestal data analysis (N_pix)"
+    )
+
+    flatfield_mask = Field(
+        None,
+        "Mask from the flat-flield data analysis (N_pix)"
+    )
+
+
+class PixelStatusContainer(Container):
+    """
+    Container for pedestal data
+    """
+    tels_with_data = Field([], "list of telescopes with data")
+    tel = Field(
+        Map(PixelStatusCameraContainer),
+        "map of tel_id to PixelStatusContainer")
+
+
+class MonitoringDataContainer(Container):
+    """
+    Root container for MON data
+    """
+    flatfield = Field(FlatFieldContainer(), "Relative flat field data")
+    pedestal = Field(PedestalContainer(), "Pedestal data")
+    pixel_status = Field(PixelStatusContainer(), "Container of masks with pixel status")
+
+
+
+class LSTServiceContainer(Container):
+    """
+    Container for Fields that are specific to each LST camera configuration
+    """
+
+    # Data from the CameraConfig table
+    telescope_id = Field(-1, "telescope id")
+    cs_serial = Field(None, "serial number of the camera server")
+    configuration_id = Field(None, "id of the CameraConfiguration")
+    date = Field(None, "NTP start of run date")
+    num_pixels = Field(-1, "number of pixels")
+    num_samples = Field(-1, "num samples")
+    pixel_ids = Field([], "id of the pixels in the waveform array")
+    data_model_version = Field(None, "data model version")
+
+    idaq_version = Field(0o0, "idaq version")
+    cdhs_version = Field(0o0, "cdhs version")
+    algorithms = Field(None, "algorithms")
+    pre_proc_algorithms = Field(None, "pre processing algorithms")
+    module_ids = Field([], "module ids")
+    num_modules = Field(-1, "number of modules")
+
+
+class LSTEventContainer(Container):
+    """
+    Container for Fields that are specific to each LST event
+    """
+
+    # Data from the CameraEvent table
+    configuration_id = Field(None, "id of the CameraConfiguration")
+    event_id = Field(None, "local id of the event")
+    tel_event_id = Field(None, "global id of the event")
+    pixel_status = Field([], "status of the pixels (n_pixels)")
+    ped_id = Field(None, "tel_event_id of the event used for pedestal substraction")
+    module_status = Field([], "status of the modules (n_modules)")
+    extdevices_presence = Field(None, "presence of data for external devices")
+
+    tib_event_counter = Field(None, "TIB event counter")
+    tib_pps_counter = Field(None, "TIB pps counter")
+    tib_tenMHz_counter = Field(None, "TIB 10 MHz counter")
+    tib_stereo_pattern = Field(None, "TIB stereo pattern")
+    tib_masked_trigger = Field(None, "TIB trigger mask")
+
+    ucts_event_counter =  Field(None, "UCTS event counter")
+    ucts_pps_counter = Field(None, "UCTS pps counter")
+    ucts_clock_counter = Field(None, "UCTS clock counter")
+    ucts_timestamp = Field(None, "UCTS timestamp")
+    ucts_camera_timestamp = Field(None, "UCTS camera timestamp")
+    ucts_trigger_type = Field(None, "UCTS trigger type")
+    ucts_white_rabbit_status = Field(None, "UCTS whiteRabbit status")
+
+    #cdts_data = Field([], "CDTS data array")
+    swat_data = Field([], "SWAT data array")
+
+    pps_counter= Field([], "Dragon pulse per second counter (n_modules)")
+    tenMHz_counter = Field([], "Dragon 10 MHz counter (n_modules)")
+    event_counter = Field([], "Dragon event counter (n_modules)")
+    trigger_counter = Field([], "Dragon trigger counter (n_modules)")
+    local_clock_counter = Field([], "Dragon local 133 MHz counter (n_modules)")
+
+    chips_flags = Field([], "chips flags")
+    first_capacitor_id = Field([], "first capacitor id")
+    drs_tag_status = Field([], "DRS tag status")
+    drs_tag = Field([], "DRS tag")
+
+
+class LSTCameraContainer(Container):
+    """
+    Container for Fields that are specific to each LST camera
+    """
+    evt = Field(LSTEventContainer(), "LST specific event Information")
+    svc = Field(LSTServiceContainer(), "LST specific camera_config Information")
+
+
+class LSTContainer(Container):
+    """
+    Storage for the LSTCameraContainer for each telescope
+    """
+    tels_with_data = Field([], "list of telescopes with data")
+
+    # create the camera container
+    tel = Field(
+        Map(LSTCameraContainer),
+        "map of tel_id to LSTTelContainer")
+
+
+class LSTDataContainer(DataContainer):
+    """
+    Data container including LST and monitoring information
+    """
+    lst = Field(LSTContainer(), "LST specific Information")
+    mon = Field(MonitoringDataContainer(), "container for MON data")
