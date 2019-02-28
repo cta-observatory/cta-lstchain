835bde43b27a15d20161708f689e7675e924b51f
Franca Cassol <cassol@cppm.in2p3.fr>
2019-02-28 10:15:11 +0100

    start changes for ctapipe master

--- lstchain/calib/camera/tools/calc_flatfield.py
+++ lstchain/calib/camera/tools/calc_flatfield.py
@@ -2,20 +2,20 @@
 Extract flat field coefficients from flasher data files.
 """
 from traitlets import Dict, List, Unicode
-
+import ctapipe.utils.tools as tool_utils
 from ctapipe.core import Provenance
 from ctapipe.io import HDF5TableWriter
 from ctapipe.core import Tool
-from ctapipe.io import EventSourceFactory
+from ctapipe.io import event_source, EventSource
 
-from ctapipe.image import ChargeExtractorFactory, WaveformCleanerFactory
+from ctapipe.image import ChargeExtractor, WaveformCleaner
 
 # to be changed for ctapipe
-from lstchain.calib.camera import FlatFieldFactory
+from lstchain.calib.camera import FlatFieldCalculator
 from lstchain.io.containers import FlatFieldContainer
 
 
-class PedestalsHDF5Writer(Tool):
+class FlatFieldHDF5Writer(Tool):
     name = "FlatFieldHDF5Writer"
     description = "Generate a HDF5 file with flat field coefficients"
 
@@ -25,33 +25,34 @@ class PedestalsHDF5Writer(Tool):
     ).tag(config=True)
 
     aliases = Dict(dict(
-        input_file='EventSourceFactory.input_url',
-        max_events='EventSourceFactory.max_events',
-        allowed_tels='EventSourceFactory.allowed_tels',
-        charge_extractor='ChargeExtractorFactory.product',
-        window_width='ChargeExtractorFactory.window_width',
-        t0='ChargeExtractorFactory.t0',
-        window_shift='ChargeExtractorFactory.window_shift',
-        sig_amp_cut_HG='ChargeExtractorFactory.sig_amp_cut_HG',
-        sig_amp_cut_LG='ChargeExtractorFactory.sig_amp_cut_LG',
-        lwt='ChargeExtractorFactory.lwt',
-        cleaner='WaveformCleanerFactory.product',
-        baseline_start='WaveformCleanerFactory.baseline_start',
-        baseline_end='WaveformCleanerFactory.baseline_end',
-        generator='FlatFieldFactory.product',
-        tel_id='FlatFieldFactory.tel_id',
-        sample_duration='FlatFieldFactory.sample_duration',
-        sample_size='FlatFieldFactory.sample_size',
-        n_channels='FlatFieldFactory.n_channels',
+        input_file='EventSource.input_url',
+        max_events='EventSource.max_events',
+        charge_extractor='ChargeExtractor.product',
+        window_width='WindowIntegrator.window_width',
+        window_shift='WindowIntegrator.window_shift',
+        sig_amp_cut_HG='PeakFindingIntegrator.sig_amp_cut_HG',
+        sig_amp_cut_LG='PeakFindingIntegrator.sig_amp_cut_LG',
+        t0='SimpleIntegrator.t0',
+        lwt='NeighbourPeakIntegrator.lwt',
+        cleaner='WaveformCleaner.product',
+        baseline_start='WaveformCleaner.baseline_start',
+        baseline_end='WaveformCleaner.baseline_end',
+        generator='FlatFieldCalculator.product',
+        tel_id='FlatFieldCalculator.tel_id',
+        sample_duration='FlatFieldCalculator.sample_duration',
+        sample_size='FlatFieldCalculator.sample_size',
+        n_channels='FlatFieldCalculator.n_channels',
     ))
 
-    classes = List([EventSourceFactory,
-                    ChargeExtractorFactory,
-                    WaveformCleanerFactory,
-                    FlatFieldFactory,
+    classes = List([EventSource,
+                    ChargeExtractor,
+                    WaveformCleaner,
+                    FlatFieldCalculator,
                     FlatFieldContainer,
                     HDF5TableWriter
-                    ])
+                    ]+ tool_utils.classes_with_traits(WaveformCleaner)
+	                 + tool_utils.classes_with_traits(ChargeExtractor)
+                   )
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -59,11 +60,17 @@ class PedestalsHDF5Writer(Tool):
         self.flatfield = None
         self.container = None
         self.writer = None
+        self.cleaner = None
 
     def setup(self):
         kwargs = dict(config=self.config, tool=self)
-        self.eventsource = EventSourceFactory.produce(**kwargs)
-        self.flatfield = FlatFieldFactory.produce(**kwargs)
+        self.eventsource = EventSource.from_config(**kwargs)
+        self.flatfield = FlatFieldCalculator(**kwargs)
+
+        self.cleaner = WaveformCleaner.from_name(
+            self.cleaner_product,
+            **kwargs
+        )
 
         #self.container = FlatFieldContainer()
         self.writer = HDF5TableWriter(
@@ -94,7 +101,7 @@ class PedestalsHDF5Writer(Tool):
 
 
 def main():
-    exe = PedestalsHDF5Writer()
+    exe = FlatFieldHDF5Writer()
     exe.run()
 
 
