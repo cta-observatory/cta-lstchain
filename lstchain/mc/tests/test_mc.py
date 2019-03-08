9eef29ff6adeac0e55accab7291faf4af641315b
rlopezcoto <rlopezcoto@gmail.com>
2019-03-08 20:01:06 +0000

    correct assert in the mc testing unit

--- lstchain/mc/tests/test_mc.py
+++ lstchain/mc/tests/test_mc.py
@@ -1,7 +1,6 @@
-import astropy.units as u 
 import numpy as np
+from numpy.testing import assert_allclose
 from lstchain.mc import power_law_integrated_distribution
-
 
 def test_integrated_distribution():
 
@@ -14,4 +13,4 @@ def test_integrated_distribution():
     b ,y = power_law_integrated_distribution(
         emin, emax, Nevents, spectral_index, bins)
 
-    assert(Nevents == np.sum(y))
+    assert_allclose(Nevents,np.sum(y),rtol=1.e-2)
