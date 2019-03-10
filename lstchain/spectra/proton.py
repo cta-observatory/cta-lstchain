24fc283ad908258fb3057399e7a75dfa1b5ce2bf
rlopezcoto <rlopezcoto@gmail.com>
2019-03-10 18:55:42 +0000

    Add reference BESS spectrum

--- lstchain/spectra/proton.py
+++ lstchain/spectra/proton.py
@@ -2,7 +2,7 @@ import astropy.units as u
 import numpy as np
 
 def Proton_BESS(E):
-    """ From http://adsabs.harvard.edu/abs/2015JHEAp...5...30A                                                                                                                         
+    """ From http://adsabs.harvard.edu/abs/2000ApJ...545.1135S
     For each energy point, return the Proton flux
 
     Parameters
@@ -11,12 +11,12 @@ def Proton_BESS(E):
 
     Returns
     -------
-    dFdE: differential energy spectrum. 
-          astropy units: 1/u.TeV / u.cm**2 / u.s
+    dFdEdO: differential energy spectrum. 
+          astropy units: 1 / u.TeV / u.cm**2 / u.s / u.sr
     """
 
     BESS_par=[9.6e-9 / u.GeV / u.cm**2 / u.s / u.sr, -2.70]
     E0 = 1 * u.TeV
-    dFdE = BESS_par[0]*np.power(E/E0,BESS_par[1])
+    dFdEdO = BESS_par[0]*np.power(E/E0,BESS_par[1])
 
-    return dFdE.to(1 / u.TeV / u.cm**2 / u.s / u.sr) 
+    return dFdEdO.to(1 / u.TeV / u.cm**2 / u.s / u.sr) 
