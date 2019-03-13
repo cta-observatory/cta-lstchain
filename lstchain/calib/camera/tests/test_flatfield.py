b1d7f2b4189d9bebaa83e5ff582a7cee566c8ea1
Franca Cassol <cassol@cppm.in2p3.fr>
2019-03-13 09:56:31 +0100

    Suspend the test till we get a test file in lstchain_extra

--- lstchain/calib/camera/tests/test_flatfield.py
+++ lstchain/calib/camera/tests/test_flatfield.py
@@ -3,8 +3,10 @@ from ctapipe.utils import get_dataset_path
 from ctapipe.io import event_source
 from .. import FlasherFlatFieldCalculator
 
+"""
 #example_file_path = get_dataset_path("/ctadata/franca/LST/LST-1.4.Run00167.0001.fits.fz")
 input_reader = event_source("/ctadata/franca/LST/LST-1.4.Run00167.0001.fits.fz", max_events=10)
+
 
 def test_flasherflatfieldcalculator():
 
@@ -16,3 +18,4 @@ def test_flasherflatfieldcalculator():
 
         if ff_calculator.num_events_seen == ff_calculator.sample_size:
             assert ff_data
+"""
