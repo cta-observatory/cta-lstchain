dbeed39a2cf8fb45f3d7be898d70e4b3b8c34397
rlopezcoto <rlopezcoto@gmail.com>
2019-03-16 18:22:01 +0000

    Add dictionaries to spectra to return the parameters set

--- lstchain/spectra/proton.py
+++ lstchain/spectra/proton.py
@@ -11,13 +11,19 @@ def Proton_BESS(E):
 
     Returns
     -------
-    dFdEdO: differential energy spectrum. 
+    dFdEdO: `numpy.ndarray` differential energy spectrum. 
           astropy units: 1 / u.TeV / u.cm**2 / u.s / u.sr
+    par: `dict` with spectral parameters
     """
 
     f0 = 9.6e-9 / u.GeV / u.cm**2 / u.s / u.sr
     alpha = -2.70
     E0 = 1 * u.TeV
+
+    par_var = [f0, alpha, E0]
+    par_dic = ['f0', 'alpha', 'E0']
+    par = dict(zip(par_dic, par_var))
+
     dFdEdO = f0 * np.power(E/E0, alpha)
 
-    return dFdEdO.to(1 / u.TeV / u.cm**2 / u.s / u.sr) 
+    return dFdEdO.to(1 / u.TeV / u.cm**2 / u.s / u.sr), par 
