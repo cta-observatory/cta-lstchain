Correct bugs for the sensitivity calculation

lstchain/mc/mc.py
@@ -26,8 +26,8 @@ def power_law_integrated_distribution(emin, emax, nev, spectral_index, bin_numbe
         y0 = nev / np.log(emax / emin)
         y = y0 * np.log(bins[1:] / bins[:-1])
     else:
-        y0 = nev / (emax ** (spectral_index + 1) - emin ** (spectral_index + 1)) / (spectral_index + 1)
-        y = y0 * (bins[1:] ** (spectral_index + 1) - bins[:-1] ** (spectral_index + 1)) / (spectral_index + 1)
+        y0 = nev / (emax**(spectral_index + 1) - emin**(spectral_index + 1)) / (spectral_index + 1)
+        y = y0 * (bins[1:]**(spectral_index + 1) - bins[:-1]**(spectral_index + 1)) / (spectral_index + 1)
     return bins, y
 
 def int_diff_sp(emin, emax, sp_idx, e0):
@@ -37,10 +37,10 @@ def int_diff_sp(emin, emax, sp_idx, e0):
     """
 
     if spectral_index == -1:
-        integral_E = np.log(emax / emin) / e0 ** sp_idx
+        integral_E = np.log(emax / emin) / e0**sp_idx
     else:
-        integral_E = (emax ** (spectral_index + 1) - emin ** (spectral_index + 1)) \
-            / (spectral_index + 1) / e0 ** sp_idx
+        integral_E = (emax**(spectral_index + 1) - emin**(spectral_index + 1)) \
+            / (spectral_index + 1) / e0**sp_idx
 
     return integral_E
 
@@ -48,13 +48,14 @@ def rate(emin, emax, sp_idx, cone, area, norm, e0):
     """
     Calculates the rate of events for a power-law distribution, 
     in a given energy range, collection area and solid angle
+
     Parameters      
     ----------
     emin:  `float`  minimum energy
     emax:  `float`  maximum energy
     sp_idx:`float`  spectral index of the power-law distribution
-    cone:  `float`  angle in deg for the solid angle cone
-    area:  `float`  collection area 
+    cone:  `float`  angle [deg] for the solid angle cone
+    area:  `float`  collection area [cm**2]
     norm:  `float`  normalization of the differential energy spectrum at e0 
     e0:    `float`  normalization energy 
 