Correct bugs for the sensitivity calculation

lstchain/mc/plot_utils.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from lstchain.spectra.crab import crab_HEGRA
 
-def fill_bin_content(ax,energy_bin):
+def fill_bin_content(ax, energy_bin, gb, tb):
     """
 
     Parameters
@@ -10,8 +10,8 @@ def fill_bin_content(ax,energy_bin):
     Returns
     --------
     """
-    for i in range(0,gammaness_bins):
-        for j in range(0,theta2_bins):
+    for i in range(0,gb):
+        for j in range(0,tb):
             text = ax.text((j+0.5)*0.05, (i+0.5)*0.1, "%.2f %%" % sens[energy_bin][i][j],
                        ha="center", va="center", color="w")
     return ax
@@ -28,8 +28,8 @@ def format_axes(ax,pl):
     """
     ax.set_aspect(0.5)
 
-    ax.set_ylabel(r'Gammaness',fontsize=15)
-    ax.set_xlabel(r'$\theta^2$ (deg$^2$)',fontsize=15)
+    ax.set_ylabel(r'Gammaness', fontsize=15)
+    ax.set_xlabel(r'$\theta^2$ (deg$^2$)', fontsize=15)
     
     starty, endy = ax.get_ylim()
     ax.yaxis.set_ticks(np.arange(endy, starty, 0.1)[::-1])
@@ -37,8 +37,8 @@ def format_axes(ax,pl):
     ax.xaxis.set_ticks(np.arange(startx, endx, 0.1))
 
     cbaxes = fig.add_axes([0.9, 0.125, 0.03, 0.755])
-    cbar = fig.colorbar(pl,cax=cbaxes)
-    cbar.set_label('Sensitivity (% Crab)',fontsize=15)
+    cbar = fig.colorbar(pl, cax=cbaxes)
+    cbar.set_label('Sensitivity (% Crab)', fontsize=15)
 
 
 def format_axes_array(ax, arr_i,arr_j, plot):
@@ -52,9 +52,9 @@ def format_axes_array(ax, arr_i,arr_j, plot):
     """
     ax.set_aspect(0.5)
     if ((arr_i == 0) and (arr_j == 0)):
-        ax.set_ylabel(r'Gammaness',fontsize=15)
+        ax.set_ylabel(r'Gammaness', fontsize=15)
     if ((arr_i == 3) and (arr_j == 2)):
-        ax.set_xlabel(r'$\theta^2$ (deg$^2$)',fontsize=15)
+        ax.set_xlabel(r'$\theta^2$ (deg$^2$)', fontsize=15)
 
     starty, endy = ax.get_ylim()
     ax.yaxis.set_ticks(np.arange(endy, starty, 0.1)[::-1])
@@ -62,8 +62,8 @@ def format_axes_array(ax, arr_i,arr_j, plot):
     ax.xaxis.set_ticks(np.arange(startx, endx, 0.1))
     
     cbaxes = fig.add_axes([0.91, 0.125, 0.03, 0.755])
-    cbar = fig.colorbar(plot,cax=cbaxes)
-    cbar.set_label('Sensitivity (% Crab)',fontsize=15) 
+    cbar = fig.colorbar(plot, cax=cbaxes)
+    cbar.set_label('Sensitivity (% Crab)', fontsize=15) 
 
 def format_axes_sensitivity(ax):
     """
@@ -77,11 +77,11 @@ def format_axes_sensitivity(ax):
 
     ax.set_xscale("log", nonposx='clip')
     ax.set_yscale("log", nonposy='clip')
-    ax.set_xlim(5e1,9.e4)
-    ax.set_ylim(1.e-14,5.e-10)
+    ax.set_xlim(5e1, 9.e4)
+    ax.set_ylim(1.e-14, 5.e-10)
     ax.set_xlabel("Energy [GeV]")
     ax.set_ylabel(r'E$^2$ $\frac{\mathrm{dN}}{\mathrm{dE}}$ [TeV cm$^{-2}$ s$^{-1}$]')
-    ax.grid(ls='--',alpha=.5)
+    ax.grid(ls='--', alpha=.5)
 
 def plot_Crab_SED(ax, emin, emax, percentage=100, **kwargs):
     """
@@ -95,11 +95,11 @@ def plot_Crab_SED(ax, emin, emax, percentage=100, **kwargs):
     En = np.logspace(np.log10(emin.to_value()), np.log10(emax.to_value()), 40) * u.GeV  
 
     dFdE = percentage / 100. * crab_HEGRA(En)
-    ax.loglog(En,dFdE * En/1.e3 * En/1.e3, color='gray', **kwargs)
+    ax.loglog(En, dFdE * En * En, color='gray', **kwargs)
     
     return ax
 
-def plot_sensitivity(ax, e):
+def plot_sensitivity(ax, e, sensitivity):
     """
 
     Parameters
@@ -108,27 +108,28 @@ def plot_sensitivity(ax, e):
     Returns
     --------
     """
-    dFdE = crab_HEGRA(e)
-    ax.loglog(e, sensitivity / 100 * dFdE * Emed/1.e3 * Emed/1.e3, \
+    emed = np.sqrt(e[1:] * e[:-1])
+    dFdE = crab_HEGRA(emed)
+    ax.loglog(e, sensitivity / 100 * dFdE * emed * emed, \
               label = 'Sensitivity')
     
 
-def sensitivity_plots(ebins):
+def sens_minimization_plot(eb, gb, tb, e, sens):
     """
-
+    TODO: Save plots!
     Parameters
     --------
 
     Returns
     --------
     """
-    if (ebins == 12):
+    if (eb == 12):
         figarr, axarr = plt.subplots(4,3, sharex=True, sharey=True, figsize=(13.2,18))
 
-    indices=[]
-    sensitivity = np.ndarray(shape=ebins)
+    # The minimum sensitivity per energy bin
+    sensitivity = np.ndarray(shape=eb)
     
-    for ebin in range(0,ebins):
+    for ebin in range(0,eb):
         if (figarr):
             arr_i = int(ebin/3)
             arr_j = ebin-int(ebin/3)*3
@@ -139,24 +140,21 @@ def sensitivity_plots(ebins):
         fig, ax = plt.subplots(figsize=(8,8))
 
         pl = ax.imshow(sens[ebin], cmap='viridis', extent=[0., 0.5, 1., 0.])
-        fill_bin_content(ax, ebin)
+        fill_bin_content(ax, ebin, gb, tb)
         format_axes(ax, pl)
-
-        # Calculate what is the minimum sensitivity per energy bin
-        ind = np.unravel_index(np.argmin(sens[ebin], axis=None), sens[ebin].shape)
-        indices.append(ind)
-        sensitivity[ebin] = sens[ebin][ind]
 
     if (figarr):
         figarr.subplots_adjust(hspace = 0, wspace = 0)
         format_axes_array(axarr[arr_i,arr_j],arr_i,arr_j, plot)
 
-    # Final sensitivity plot
-    fig, ax = plt.subplots()
-    plot_sensitivity(ax)
 
-    plot_Crab(ax, 100, 100 * u.GeV, 30 * u.TeV, label=r'Crab')
-    plot_Crab(ax, 1, 100 * u.GeV, 30 * u.TeV, ls='dotted',label='1% Crab')
+def sens_plot(eb, e, sensitivity)
+    # Final sensitivity plot
+    fig_sens, ax = plt.subplots()
+    plot_sensitivity(ax, e, sensitivity)
+
+    plot_Crab_SED(ax, 100, 100 * u.GeV, 30 * u.TeV, label=r'Crab')
+    plot_Crab_SED(ax, 1, 100 * u.GeV, 30 * u.TeV, ls='dotted',label='1% Crab')
 
     format_axes_sensitivity(ax)
     ax.legend(numpoints=1,prop={'size':9},ncol=2,loc='upper right')

lstchain/mc/sensitivity.py
@@ -1,10 +1,13 @@
 import numpy as np
 import pandas as pd
 import astropy.units as u
-from .plot_utils import sensitivity_plots
-from .mc import rate, weight
 
 from eventio.simtel.simtelfile import SimTelFile
+
+from .plot_utils import sensitivity_plots
+from .mc import rate, weight
+from lstchain.spectra.crab import crab_HEGRA, proton_BESS
+
 
 def read_sim_par(source):
     """
@@ -167,8 +170,14 @@ def sensitivity(emin_sens, emax_sens, eb, gb, tb, noff, obstime = 50 * 3600 * u.
             
                 final_gamma[i][g][t] = eg_w_sum * obstime
                 final_hadrons[i][g][t] = ep_w_sum * obstime
-
                     
     sens = calculate_sensititity(final_gamma, final_hadrons, 1/noff)
-    sensitivity_plots(eb)
     
+    # Calculate the minimum sensitivity per energy bin
+    sensitivity = np.ndarray(shape=eb)
+    for i in range(0,eb):
+        ind = np.unravel_index(np.argmin(sens[i], axis=None), sens[i].shape)
+        sensitivity[i] = sens[i][ind]
+
+    sens_minimization_plot(eb, gb, tb, E, sens)
+    sens_plot(eb, E, sensitivity)