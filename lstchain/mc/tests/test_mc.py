c27621bba58e493fc0267c8ce24207b4daa0096b
rlopezcoto <rlopezcoto@gmail.com>
2019-03-18 10:03:37 +0100

    Change spectral index, allasert call and relative tolerance

--- lstchain/mc/tests/test_mc.py
+++ lstchain/mc/tests/test_mc.py
@@ -1,5 +1,4 @@
 import numpy as np
-from numpy.testing import assert_allclose
 from lstchain.mc import power_law_integrated_distribution
 
 def test_integrated_distribution():
@@ -7,10 +6,10 @@ def test_integrated_distribution():
     emin = 50.     # u.GeV
     emax = 500.e3  # u.GeV
     Nevents = 1e6
-    spectral_index = -2
+    spectral_index = -2.5
     bins = 30
 
     b ,y = power_law_integrated_distribution(
         emin, emax, Nevents, spectral_index, bins)
 
-    assert_allclose(Nevents,np.sum(y),rtol=1.e-2)
+    np.testing.assert_allclose(Nevents,np.sum(y),rtol=1.e-10)
