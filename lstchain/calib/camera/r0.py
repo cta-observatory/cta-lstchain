76e6f53485a614d3de4f216f36626d34c256298c
Franca Cassol <cassol@cppm.in2p3.fr>
2019-04-25 10:21:26 +0200

    Minor changes

--- lstchain/calib/camera/r0.py
+++ lstchain/calib/camera/r0.py
@@ -401,7 +401,7 @@ class NullR0Calibrator(CameraR0Calibrator):
 
     def calibrate(self, event):
         for telid in event.r0.tels_with_data:
-            event.r1.tel[self.tel_id].trigger_type = event.r0.tel[self.tel_id].trigger_type
-            event.r1.tel[self.tel_id].trigger_time = event.r1.tel[self.tel_id].trigger_time
+            event.r1.tel[telid].trigger_type = event.r0.tel[telid].trigger_type
+            event.r1.tel[telid].trigger_time = event.r1.tel[telid].trigger_time
             samples = event.r0.tel[telid].waveform[:,:,self.r1_sample_start:self.r1_sample_end]
             event.r1.tel[telid].waveform = samples.astype('float32')
