correct units

lstchain/mc/tests/test_senstivity.py
@@ -18,13 +18,12 @@ def test_read_sim_par():
 
     par = read_sim_par(source)
 
-    assert np.isclose(par['emin'], 0.003)
-    assert np.isclose(par['emax'], 330.0)
+    assert np.isclose(par['emin'].to_value(), 0.003)
+    assert np.isclose(par['emax'].to_value(), 330.0)
     assert np.isclose(par['spectral_idx'], -2.0)
-    assert np.isclose(par['n_showers'], 100000)
-    assert np.isclose(par['n_use'], 10)
-    assert np.isclose(par['max_impact'], 2500.0)
-    assert np.isclose(par['cone'], 0.0)
+    assert np.isclose(par['sim_ev'], 100000)
+    assert np.isclose(par['area_sim'].to_value(), 19634954.08)
+    assert np.isclose(par['cone'].to_value(), 0.0)
 
 
 #def test_process_mc():