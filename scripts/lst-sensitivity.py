5b3fb581c162359fc9eb83a8cf230461ecfe0090
misabelber <m.isabel.bernardos@gmail.com>
2019-06-04 17:04:27 +0100

    towards sensitivity calculation...

--- scripts/lst-sensitivity.py
+++ scripts/lst-sensitivity.py
@@ -2,6 +2,7 @@ from lstchain.mc import sensitivity
 from lstchain.mc import plot_utils
 import matplotlib.pyplot as plt
 import astropy.units as u
+from astropy.table import Table
 import argparse
 
 parser = argparse.ArgumentParser(description="Compute Sensitivity Curve.")
@@ -21,20 +22,28 @@ parser.add_argument('--protondl2', '-pd2', type=str,
 
 args = parser.parse_args()
 
-nfiles_gammas = 100*0.2 #Pointlike gammas
-nfiles_protons = 5000*0.2
+nfiles_gammas = 100 #Pointlike gammas
+nfiles_protons = 5000*0.8
 
-eb = 12 # Number of energy bins
-gb = 10 #Number of gammaness bins
+eb = 20 # Number of energy bins
+gb = 11 #Number of gammaness bins
 tb = 10 #Number of theta2 bins
-obstime = 50 * 3600 *u.s
+obstime = 50 * 3600 * u.s
 noff = 5
 
-E, best_sens = sensitivity.sens(args.simtelfile_gammas, args.simtelfile_protons,
+E, best_sens, result, units, rate_g, rate_p, w_g, w_p = sensitivity.sens(args.simtelfile_gammas, args.simtelfile_protons,
                  args.dl2_file_g, args.dl2_file_p,
                  nfiles_gammas, nfiles_protons,
                  eb, gb, tb, noff,
                  obstime)
-
+#plt.show()
 plot_utils.sens_plot(eb, E, best_sens)
 plt.show()
+
+tab = Table.from_pandas(result)
+
+for i, key in enumerate(tab.columns.keys()):
+    tab[key].unit = units[i]
+    if key=='sensitivity':
+        continue
+    tab[key].format = '8f'
