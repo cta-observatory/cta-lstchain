b2d21b2b40ea8baaa79facfcbe9238f6546404cb
vuillaut <thomas.vuillaume@gmail.com>
2019-06-18 22:25:06 +0200

    correct test for replace_config

--- lstchain/io/tests/test_config.py
+++ lstchain/io/tests/test_config.py
@@ -8,6 +8,6 @@ def test_replace_config():
     a = dict(toto=1, tata=2)
     b = dict(tata=3, tutu=4)
     c = config.replace_config(a, b)
-    c["toto"] == 1
-    c["tata"] == 3
-    c["tutu"] == 4
+    assert c["toto"] == 1
+    assert c["tata"] == 3
+    assert c["tutu"] == 4
