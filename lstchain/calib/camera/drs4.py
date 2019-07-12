bb0e81f72bebeaa3a02aa0e8a3b9b183f57bf5ff
Paweł Gliwny <p.gliwny@gmail.com>
2019-07-12 16:13:10 +0200

    improved code to create pedestal file

--- lstchain/calib/camera/drs4.py
+++ lstchain/calib/camera/drs4.py
@@ -6,66 +6,66 @@ __all__ = ['DragonPedestal']
 
 
 class DragonPedestal:
-    n_pixels = 7
-    roisize = 40
-    size4drs = 4*1024
-    high_gain = 0
-    low_gain = 1
+    size4drs = 4096
 
-    def __init__(self, telid=0):
-        self.telid = telid
-        self.first_capacitor = np.zeros((2, 8))
+    def __init__(self, tel_id, n_module):
+        self.tel_id = tel_id
+        self.n_module = n_module
+        self.n_pixels = n_module*7
         self.meanped = np.zeros((2, self.n_pixels, self.size4drs))
         self.numped = np.zeros((2, self.n_pixels, self.size4drs))
+        self.first_cap_array = np.zeros((self.n_module, 2, 7))
 
-    def fill_pedestal_event(self, event, nr):
-        first_cap = event.lst.tel[0].evt.first_capacitor_id[nr * 8:(nr + 1) * 8]
+    def fill_pedestal_event(self, event):
+        expected_pixel_id = event.lst.tel[self.tel_id].svc.pixel_ids
+        waveform = event.r0.tel[self.tel_id].waveform
+        for nr_module in prange(0, self.n_module):
+            self.first_cap_array[nr_module, :, :] = self.get_first_capacitor(event, nr_module)
 
-        for i, j in zip([0, 1, 2, 3, 4, 5, 6], [0, 0, 1, 1, 2, 2, 3]):
-            self.first_capacitor[self.high_gain, i] = first_cap[j]
-
-        for i, j in zip([0, 1, 2, 3, 4, 5, 6], [4, 4, 5, 5, 6, 6, 7]):
-            self.first_capacitor[self.low_gain, i] = first_cap[j]
-
-        waveform = event.r0.tel[0].waveform[:, :, :]
-        expected_pixel_id = event.lst.tel[self.telid].svc.pixel_ids
-        self._fill_pedestal_event_jit(nr, waveform, expected_pixel_id, self.first_capacitor, self.meanped, self.numped)
+        self._fill_pedestal_event_jit(waveform,
+                                      expected_pixel_id,
+                                      self.first_cap_array,
+                                      self.meanped,
+                                      self.numped)
 
     @staticmethod
     @jit(parallel=True)
-    def _fill_pedestal_event_jit(nr, waveform, expected_pixel_id, first_cap, meanped, numped):
+    def _fill_pedestal_event_jit(waveform, expected_pixel_id, first_cap_array,
+                                 meanped, numped):
         size4drs = 4096
         roisize = 40
-        for i in prange(0, 2):
-            for j in prange(0, 7):
-                fc = int(first_cap[i, j])
-                pixel =  expected_pixel_id[nr*7 + j]
-                posads0 = int((2+fc)%size4drs)
-                if posads0 + 40 < 4096:
-                    meanped[i, j, posads0:(posads0+36)] += waveform[i, pixel, 2:38]
-                    numped[i, j, posads0:(posads0 + 36)] += 1
-                else:
-                    for k in prange(2, roisize-2):
-                        posads = int((k+fc)%size4drs)
-                        val = waveform[i, pixel, k]
-                        meanped[i, j, posads] += val
-                        numped[i, j, posads] += 1
+        for nr_module in prange(0, 265):
+            first_cap = first_cap_array[nr_module, :, :]
+            for gain in prange(0, 2):
+                for pix in prange(0, 7):
+                    fc = first_cap[gain, pix]
+                    pixel = expected_pixel_id[nr_module * 7 + pix]
+
+                    posads0 = int((2 + fc) % size4drs)
+                    if posads0 + 40 < 4096:
+                        meanped[gain, pixel, posads0:(posads0 + 36)] += waveform[gain, pixel, 2:38]
+                        numped[gain, pixel, posads0:(posads0 + 36)] += 1
+
+                    else:
+                        for k in prange(2, roisize - 2):
+                            posads = int((k + fc) % size4drs)
+                            val = waveform[gain, pixel, k]
+                            meanped[gain, pixel, posads] += val
+                            numped[gain, pixel, posads] += 1
 
     def finalize_pedestal(self):
-        #Print error message when not every capacitor in pixel is coverage
-        with np.errstate(all='raise'):
-            try:
-                self.meanped = self.meanped / self.numped
-            except FloatingPointError:
-                print("Not enough events to coverage all capacitor."
-                      " Please use more events to create pedestal file.")
+        try:
+            self.meanped = self.meanped / self.numped
+        except Exception as err:
+             print("Not enough events to coverage all capacitor. Please use more events to create pedestal file.")
+             print(err)
 
     def get_first_capacitor(self, event, nr):
-        fc = np.zeros((2, 8))
-        first_cap = event.lst.tel[self.telid].evt.first_capacitor_id[nr * 8:(nr + 1) * 8]
+        fc = np.zeros((2, 7))
+        first_cap = event.lst.tel[self.tel_id].evt.first_capacitor_id[nr * 8:(nr + 1) * 8]
         # First capacitor order according Dragon v5 board data format
         for i, j in zip([0, 1, 2, 3, 4, 5, 6], [0, 0, 1, 1, 2, 2, 3]):
-            fc[self.high_gain, i] = first_cap[j]
+            fc[0, i] = first_cap[j] # high gain
         for i, j in zip([0, 1, 2, 3, 4, 5, 6], [4, 4, 5, 5, 6, 6, 7]):
-            fc[self.low_gain, i] = first_cap[j]
+            fc[1, i] = first_cap[j] # low gain
         return fc
