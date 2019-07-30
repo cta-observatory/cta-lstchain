41ca9c5de4c2ef266a58c46c6e7228c60fc63e8b
rlopezcoto <rlopezcoto@gmail.com>
2019-07-30 13:52:20 -0500

    Cleaning up the sensitivity script

--- scripts/lst-sensitivity.py
+++ scripts/lst-sensitivity.py
@@ -13,58 +13,47 @@ from lstchain.io import read_simu_info_merged_hdf5
 
 parser = argparse.ArgumentParser(description="Compute Sensitivity Curve.")
 
-parser.add_argument('--gammadl1', '-gd1', type=str,
-                    dest='dl1file_gammas',
-                    help='path to gammas simtelfile')
-parser.add_argument('--protondl1', '-pd1', type=str,
-                    dest='dl1file_protons',
-                    help='path to protons simtelfile')
-parser.add_argument('--gammadl2-cuts', '-gd2-cuts', type=str,
-                    dest='dl2_file_g_cuts',
-                    help='path to reconstructed gammas dl2 file')
-parser.add_argument('--protondl2-cuts', '-pd2-cuts', type=str,
-                    dest='dl2_file_p_cuts',
-                    help='path to reconstructed protons dl2 file')
-parser.add_argument('--gammadl2-sens', '-gd2-sens', type=str,
-                    dest='dl2_file_g_sens',
-                    help='path to reconstructed gammas dl2 file')
-parser.add_argument('--protondl2-sens', '-pd2-sens', type=str,
-                    dest='dl2_file_p_sens',
-                    help='path to reconstructed protons dl2 file')
+parser.add_argument('--gammadl1', '-gd1', type = str,
+                    dest = 'dl1file_gammas',
+                    help = 'path to gammas simtelfile')
+parser.add_argument('--protondl1', '-pd1', type = str,
+                    dest = 'dl1file_protons',
+                    help = 'path to protons simtelfile')
+parser.add_argument('--gammadl2-cuts', '-gd2-cuts', type = str,
+                    dest = 'dl2_file_g_cuts',
+                    help = 'path to reconstructed gammas dl2 file')
+parser.add_argument('--protondl2-cuts', '-pd2-cuts', type = str,
+                    dest = 'dl2_file_p_cuts',
+                    help = 'path to reconstructed protons dl2 file')
+parser.add_argument('--gammadl2-sens', '-gd2-sens', type = str,
+                    dest = 'dl2_file_g_sens',
+                    help = 'path to reconstructed gammas dl2 file')
+parser.add_argument('--protondl2-sens', '-pd2-sens', type = str,
+                    dest = 'dl2_file_p_sens',
+                    help = 'path to reconstructed protons dl2 file')
 
 args = parser.parse_args()
 
-#nfiles_gammas = 0.5#100*0.5 #Pointlike gammas
-#nfiles_protons = 0.5#5000*0.8*0.5
-#nfiles_gammas = 4#100*0.5 #Pointlike gammas
-#nfiles_protons = 4#5000*0.8*0.5
-nfiles_gammas = 4#100*0.5 #Pointlike gammas
-nfiles_protons = 4#5000*0.8*0.5
 
+ntelescopes_gamma = 4
+ntelescopes_protons = 4
 
-
-eb = 20 # Number of energy bins
-gb = 11 #Number of gammaness bins
-tb = 10 #Number of theta2 bins
+eb = 20  #  Number of energy bins
+gb = 11  #  Number of gammaness bins
+tb = 10  #  Number of theta2 bins
 obstime = 50 * 3600 * u.s
 noff = 5
 
-print("nfiles_gammas, nfiles_protons ", nfiles_gammas, nfiles_protons)
 E, best_sens, result, units, gcut, tcut = sensitivity.find_best_cuts_sens(args.dl1file_gammas,
-                                                args.dl1file_protons,
+                                               args.dl1file_protons,
                                                args.dl2_file_g_cuts, args.dl2_file_p_cuts,
-                                               nfiles_gammas, nfiles_protons,
+                                               ntelescopes_gammas, ntelescopes_protons,
                                                eb, gb, tb, noff,
                                                obstime)
 
-print("result1 ", result)
-
-print('gcut before', gcut)
-print('tcut before', tcut)
-#gcut = np.ones(eb) * 0.8
-#tcut = np.ones(eb) * 0.01
-print('gcut after', gcut)
-print('tcut after', tcut)
+# For testing using fixed cuts
+# gcut = np.ones(eb) * 0.8
+# tcut = np.ones(eb) * 0.01
 
 E, best_sens, result, units, dl2 = sensitivity.sens(args.dl1file_gammas,
                                                args.dl1file_protons,
@@ -72,9 +61,6 @@ E, best_sens, result, units, dl2 = sensitivity.sens(args.dl1file_gammas,
                                                nfiles_gammas, nfiles_protons,
                                                eb, gcut, tcut * (u.deg**2), noff,
                                                obstime)
-#plt.show()
-
-print("result2 ", result)
 
 tab = Table.from_pandas(result)
 
@@ -84,7 +70,6 @@ for i, key in enumerate(tab.columns.keys()):
         continue
     tab[key].format = '8f'
 
-print(tab)
 
 #tab.to_csv("table.txt")
 
@@ -93,16 +78,16 @@ plt.show()
 
 emed = np.sqrt(E[1:] * E[:-1])
 
-plt.plot(emed[:-1], tab['hadron_rate'], label='Hadron rate', marker='o')
-plt.plot(emed[:-1], tab['gamma_rate'], label='Gamma rate', marker='o')
+plt.plot(emed[:-1], tab['hadron_rate'], label = 'Hadron rate', marker = 'o')
+plt.plot(emed[:-1], tab['gamma_rate'], label = 'Gamma rate', marker = 'o')
 plt.legend()
 plt.xscale('log')
 plt.xlabel('Energy (GeV)')
 plt.ylabel('events / min')
 #plt.show()
 
-gammas_mc = dl2[dl2.mc_type==0]
-protons_mc = dl2[dl2.mc_type==101]
+gammas_mc = dl2[dl2.mc_type == 0]
+protons_mc = dl2[dl2.mc_type == 101]
 good_gammas = dl2
 
 sns.distplot(gammas_mc.gammaness, label='gammas');
@@ -120,11 +105,11 @@ sns.distplot(protons_mc.reco_energy, label='protons');
 plt.legend();
 plt.tight_layout()
 #plt.show()
-ctaplot.plot_theta2(gammas_mc.reco_alt, gammas_mc.reco_az, gammas_mc.mc_alt, gammas_mc.mc_az, range=(0,1), bins=100)
+ctaplot.plot_theta2(gammas_mc.reco_alt, gammas_mc.reco_az, gammas_mc.mc_alt, gammas_mc.mc_az, range=(0, 1), bins=100)
 #plt.show()
-plt.figure(figsize=(12,8))
+plt.figure(figsize=(12, 8))
 ctaplot.plot_angular_res_per_energy(gammas_mc.reco_alt, gammas_mc.reco_az, gammas_mc.mc_alt, gammas_mc.mc_az,
-                                    10**(gammas_mc.reco_energy-3),
+                                    10**(gammas_mc.reco_energy - 3),
                                    );
 
 ctaplot.plot_angular_res_cta_requirements('north', color='black')
@@ -132,8 +117,8 @@ ctaplot.plot_angular_res_cta_requirements('north', color='black')
 plt.legend()
 plt.tight_layout()
 #plt.show()
-plt.figure(figsize=(12,8))
-ctaplot.plot_energy_resolution(10**(gammas_mc.mc_energy-3), 10**(gammas_mc.reco_energy-3));
+plt.figure(figsize=(12, 8))
+ctaplot.plot_energy_resolution(10**(gammas_mc.mc_energy - 3), 10**(gammas_mc.reco_energy - 3));
 
 ctaplot.plot_energy_resolution_cta_requirements('north', color='black')
 
@@ -141,7 +126,7 @@ plt.legend()
 plt.tight_layout()
 #plt.show()
 
-ctaplot.plot_energy_bias(10**(gammas_mc.mc_energy-3), 10**(gammas_mc.reco_energy-3))
+ctaplot.plot_energy_bias(10**(gammas_mc.mc_energy - 3), 10**(gammas_mc.reco_energy - 3))
 #plt.show()
 
 gamma_ps_simu_info = read_simu_info_merged_hdf5(args.dl1file_gammas)
