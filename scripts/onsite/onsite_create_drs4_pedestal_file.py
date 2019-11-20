d84bf03f2836682b8c14d9d1543de71bb7fe663c
Franca Cassol <cassol@cppm.in2p3.fr>
2019-11-20 12:50:24 +0100

    Add  base_dir argument with default value valid for la Palma and corret new script name

--- scripts/onsite/onsite_create_drs4_pedestal_file.py
+++ scripts/onsite/onsite_create_drs4_pedestal_file.py
@@ -12,10 +12,6 @@ from pathlib import Path
 from lstchain.io.data_management import *
 import lstchain.visualization.plot_drs4 as drs4
 
-# to be changed with the right one
-base_dir = '/ctadata/franca/fefs/aswg/real'
-#base_dir = '/fefs/aswg/data/real'
-
 # parse arguments
 parser = argparse.ArgumentParser(description='Create DRS4 pedestal file',
                                  formatter_class = argparse.ArgumentDefaultsHelpFormatter)
@@ -28,11 +24,14 @@ optional.add_argument('-v', '--prod_version', help="Version of the production",
                       type=int, default=0)
 optional.add_argument('-m', '--max_events', help="Number of events to be processed",
                       type=int, default=8000)
+optional.add_argument('-b','--base_dir', help="Base dir for the output directory tree",
+                      type=str, default='/fefs/aswg/data/real')
 
 args = parser.parse_args()
 run = args.run_number
 prod_id = 'v%02d'%args.prod_version
 max_events = args.max_events
+base_dir = args.base_dir
 
 
 def main():
