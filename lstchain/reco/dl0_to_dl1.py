505358921a72eff3f794b28b0f2e1b533816fc25
Daniel Morcuende <dmorcuen@ucm.es>
2019-11-21 14:14:51 +0100

    Fixed indentation

--- lstchain/reco/dl0_to_dl1.py
+++ lstchain/reco/dl0_to_dl1.py
@@ -318,15 +318,15 @@ def r0_to_dl1(input_filename=get_dataset_path('gamma_test_large.simtel.gz'),
                         gps_time = utc_time.gps
                         dl1_container.gps_time = gps_time
 
-                    if pointing_file_path:
-                        pointing_file = pointing_file_path
-                        pointings = PointingPosition()
-                        pointings.drive_path = pointing_file
-                        azimuth, altitude = pointings.cal_pointingposition(utc_time.unix)
-                        event.pointing[telescope_id].azimuth = azimuth
-                        event.pointing[telescope_id].altitude = altitude
-                        dl1_container.az_tel = azimuth
-                        dl1_container.alt_tel = altitude
+                        if pointing_file_path:
+                            pointing_file = pointing_file_path
+                            pointings = PointingPosition()
+                            pointings.drive_path = pointing_file
+                            azimuth, altitude = pointings.cal_pointingposition(utc_time.unix)
+                            event.pointing[telescope_id].azimuth = azimuth
+                            event.pointing[telescope_id].altitude = altitude
+                            dl1_container.az_tel = azimuth
+                            dl1_container.alt_tel = altitude
 
                     foclen = (
                         event.inst.subarray.tel[telescope_id]
