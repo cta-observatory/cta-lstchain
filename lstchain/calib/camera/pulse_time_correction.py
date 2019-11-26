506f3d79a505fa73434e8f63e7ee7c4c73d29091
Paweł Gliwny <p.gliwny@gmail.com>
2019-11-26 21:47:38 +0100

    added jit function to speed up code

--- lstchain/calib/camera/pulse_time_correction.py
+++ lstchain/calib/camera/pulse_time_correction.py
@@ -20,6 +20,8 @@ class PulseTimeCorrection:
         self.fan_array = None
         self.fbn_array = None
 
+        self.first_cap_array = np.zeros((265, 2, 7))
+
         self.load_calib_file()
 
     def load_calib_file(self):
@@ -37,8 +39,33 @@ class PulseTimeCorrection:
             for pix in range(0, 7):
                 fc = self.get_first_capacitor(event, nr)[gain, pix]
                 pixel = pixel_ids[nr * 7 + pix]
-                pulse_corr[pixel] = pulse[pixel] - get_corr_time(fc%1024, self.fan_array[gain, pixel], self.fbn_array[gain, pixel], self.fNumHarmonics)
+                pulse_corr[pixel] = pulse[pixel] - get_corr_time(fc%1024,
+                                                                 self.fan_array[gain, pixel],
+                                                                 self.fbn_array[gain, pixel],
+                                                                 self.fNumHarmonics)
         return pulse_corr
+
+    def call_corr_pulse_jit(self, event, pulse):
+        gain = 0
+        pixel_ids = event.lst.tel[self.tel_id].svc.pixel_ids
+        pulse_corr = np.zeros(1855)
+        for nr in prange(0, 265):
+            self.first_cap_array[nr, :, :] = self.get_first_capacitor(event, nr)
+        self.get_corr_pulse_jit(pulse, pulse_corr, pixel_ids, self.first_cap_array, self.fan_array, self.fbn_array, self.fNumHarmonics)
+        return pulse_corr
+
+    @staticmethod
+    @njit(parallel=True)
+    def get_corr_pulse_jit(pulse, pulse_corr, pixel_ids, first_capacitor, fan_array, fbn_array, fNumHarmonics):
+        gain = 0
+        for nr in prange(0, 265):
+            for pix in prange(0, 7):
+                fc = first_capacitor[nr, 0, pix]
+                pixel = pixel_ids[nr * 7 + pix]
+                pulse_corr[pixel] = pulse[pixel] - get_corr_time_jit(fc % 1024,
+                                                               fan_array[gain, pixel],
+                                                               fbn_array[gain, pixel],
+                                                               fNumHarmonics)
 
     def get_first_capacitor(self, event, nr):
         fc = np.zeros((2, 7))
@@ -54,6 +81,15 @@ class PulseTimeCorrection:
 def get_corr_time(first_cap, fan, fbn, fNumHarmonics, fNumCap=1024):
     time = fan[0] / 2.
     for n in range(1, fNumHarmonics):
+        time += fan[n] * np.cos((first_cap * n * 2 * np.pi) / fNumCap)
+        time += fbn[n] * np.sin((first_cap * n * 2 * np.pi) / fNumCap)
+    return time
+
+
+@njit()
+def get_corr_time_jit(first_cap, fan, fbn, fNumHarmonics, fNumCap=1024):
+    time = fan[0] / 2.
+    for n in prange(1, fNumHarmonics):
         time += fan[n] * np.cos((first_cap * n * 2 * np.pi) / fNumCap)
         time += fbn[n] * np.sin((first_cap * n * 2 * np.pi) / fNumCap)
     return time
