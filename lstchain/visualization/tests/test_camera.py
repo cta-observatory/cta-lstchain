559c5a3b06fdc60fce9b5ca681049ae4498f101a
vuillaut <thomas.vuillaume@gmail.com>
2019-12-30 14:13:37 +0100

    adding a simple display for DL1 image and pulse time

--- lstchain/visualization/tests/test_camera.py
+++ lstchain/visualization/tests/test_camera.py
@@ -1,5 +1,5 @@
 import numpy as np
-from lstchain.visualization.camera import overlay_source, overlay_disp_vector
+from lstchain.visualization.camera import overlay_source, overlay_disp_vector, display_dl1_event
 from lstchain.reco.disp import disp_parameters_event
 from ctapipe.visualization import CameraDisplay
 from ctapipe.instrument import CameraGeometry
@@ -22,3 +22,15 @@ def test_overlay_source():
     image = np.random.rand(geom.n_pixels)
     display = CameraDisplay(geom, image)
     overlay_source(display, 0.1, 0.3)
+
+
+def test_display_dl1_event():
+    from ctapipe.io import event_source, EventSeeker
+    from ...tests.test_lstchain import mc_gamma_testfile
+    from ctapipe.calib import CameraCalibrator
+    source = event_source(mc_gamma_testfile, back_seekable=True)
+    seeker = EventSeeker(source)
+    event = seeker[11]  # event 11 has telescopes 1 and 4 with data
+    CameraCalibrator()(event)
+    display_dl1_event(event, tel_id=1)
+    display_dl1_event(event, tel_id=4)
