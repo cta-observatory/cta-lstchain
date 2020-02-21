d8b8f2d11d0717a9debd230bef27a2190d5948ad
vuillaut <thomas.vuillaume@gmail.com>
2020-02-21 17:32:32 +0100

    nan as default pointing values for events without timing

--- lstchain/reco/dl0_to_dl1.py
+++ lstchain/reco/dl0_to_dl1.py
@@ -351,6 +351,9 @@ def r0_to_dl1(input_filename=get_dataset_path('gamma_test_large.simtel.gz'),
                             event.pointing[telescope_id].altitude = altitude
                             dl1_container.az_tel = azimuth
                             dl1_container.alt_tel = altitude
+                        else:
+                            dl1_container.az_tel = np.nan
+                            dl1_container.alt_tel = np.nan
 
                     foclen = event.inst.subarray.tel[telescope_id].optics.equivalent_focal_length
                     width = np.rad2deg(np.arctan2(dl1_container.width, foclen))
