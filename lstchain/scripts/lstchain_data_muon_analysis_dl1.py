ba8e2d9d4ee5a274806a251a76bc3b826894e22e
Abelardo Moralejo Olaizola <moralejo@ifae.es>
2020-03-18 15:34:16 +0900

    Added possibility to use wildcards for input files - but quotes must be used, to avoid the system to expand the regular expression.

--- lstchain/scripts/lstchain_data_muon_analysis_dl1.py
+++ lstchain/scripts/lstchain_data_muon_analysis_dl1.py
@@ -1,5 +1,6 @@
 import argparse
 import sys, os
+import glob
 import numpy as np
 from ctapipe.image.muon.features import ring_containment 
 from ctapipe.image.muon.features import ring_completeness
@@ -55,7 +56,7 @@ args = parser.parse_args()
 
 def main():
 
-    print("input file: {}".format(args.input_file))
+    print("input files: {}".format(args.input_file))
     print("calib file: {}".format(args.calib_file))
     print("output file: {}".format(args.output_file))
     
@@ -90,82 +91,91 @@ def main():
     print(f"Found a total of {np.sum(bad_pixels)} bad pixels.")
 
     # image = pd.read_hdf(args.input_file, key = dl1_image_lstcam_key)
-    # For some reason the call above is not working, this is a fix using
-    # tables for the time being
-    file = tables.open_file(args.input_file).root 
-    group = file.dl1.event.telescope.image.LST_LSTCam  
-    images = [x['image'] for x in group.iterrows()]
-    
-    parameters = pd.read_hdf(args.input_file, key = dl1_params_lstcam_key)
-    telescope_description = pd.read_hdf(args.input_file, 
-                                        key='instrument/telescope/optics')
-    equivalent_focal_length = telescope_description['equivalent_focal_length'].values * u.m
-    mirror_area = telescope_description['mirror_area'].values * pow(u.m,2)
+    # The call above does not work, because of the file's vector columns (pixel-wise charges & times)
+    # So we use tables for the time being.
 
-    # File open
-    num_muons = 0
-    for full_image, event_id in zip(images, parameters['event_id']):
-        image = full_image*(~bad_pixels)
-        #print("Event {}. Number of pixels above 10 phe: {}".format(event_id,
-        #                                                          np.size(image[image > 10.])))
-        #if((np.size(image[image > 10.]) > 300) or (np.size(image[image > 10.]) < 50)):
-        #    continue
-        if not tag_pix_thr(image): #default skips pedestal and calibration events
-            continue
+    print(glob.glob(args.input_file))
 
-        #if not muon_filter(image) #default values apply no filtering. This filter is rather useless for biased extractors anyway
-        #    continue
+    filenames = glob.glob(args.input_file)
+    filenames.sort()
+    for filename in filenames:
+        print('Opening file', filename)
 
-        muonintensityparam, size_outside_ring, muonringparam, good_ring, \
-        radial_distribution, mean_pixel_charge_around_ring = analyze_muon_event(event_id, image, geom, equivalent_focal_length, 
-                                                                                mirror_area, args.plot_rings, args.plots_path)
-
-        #if not (good_ring):
-        #    continue
-
-        if good_ring:
-            num_muons = num_muons + 1
-            print("Number of good muon rings found {}, EventID {}".format(num_muons, event_id))
-
-        output_parameters['event_id'].append(
-        event_id)
-        output_parameters['ring_size'].append(
-        muonintensityparam.ring_size)
-        output_parameters['size_outside'].append(
-        size_outside_ring)
-        output_parameters['ring_radius'].append(
-        muonringparam.ring_radius.value)
-        output_parameters['ring_width'].append(
-        muonintensityparam.ring_width.value)
-        output_parameters['good_ring'].append(
-        good_ring)
-        output_parameters['muon_efficiency'].append(
-        muonintensityparam.optical_efficiency_muon)
-        output_parameters['ring_containment'].append(
-        muonringparam.ring_containment)
-        output_parameters['ring_completeness'].append(
-        muonintensityparam.ring_completeness)
-        output_parameters['ring_pixel_completeness'].append(
-        muonintensityparam.ring_pix_completeness)
-        output_parameters['impact_parameter'].append(
-        muonintensityparam.impact_parameter.value)
-        output_parameters['impact_x_array'].append(
-        muonintensityparam.impact_parameter_pos_x.value)
-        output_parameters['impact_y_array'].append(
-        muonintensityparam.impact_parameter_pos_y.value)
-        output_parameters['radial_stdev'].append(
-        radial_distribution['standard_dev'].value)
-        output_parameters['radial_skewness'].append(
-        radial_distribution['skewness'])
-        output_parameters['radial_excess_kurtosis'].append(
-        radial_distribution['excess_kurtosis'])
-        output_parameters['num_pixels_in_ring'].append(
-        np.sum(muonintensityparam.mask))
-        output_parameters['mean_pixel_charge_around_ring'].append(
-        mean_pixel_charge_around_ring)
         
-        if num_muons == max_muons:
-            break
+        with tables.open_file(filename) as file:
+            
+            # unfortunately pandas.read_hdf does not seem compatible with "with... as..." statements
+            parameters = pd.read_hdf(filename, key = dl1_params_lstcam_key)
+            telescope_description = pd.read_hdf(filename, key='instrument/telescope/optics')
+
+            group = file.root.dl1.event.telescope.image.LST_LSTCam  
+            images = [x['image'] for x in group.iterrows()]
+        
+            equivalent_focal_length = telescope_description['equivalent_focal_length'].values * u.m
+            mirror_area = telescope_description['mirror_area'].values * pow(u.m,2)
+            
+            # File open
+            num_muons = 0
+            for full_image, event_id in zip(images, parameters['event_id']):
+                image = full_image*(~bad_pixels)
+                #print("Event {}. Number of pixels above 10 phe: {}".format(event_id,
+                #                                                          np.size(image[image > 10.])))
+                #if((np.size(image[image > 10.]) > 300) or (np.size(image[image > 10.]) < 50)):
+                #    continue
+                if not tag_pix_thr(image): #default skips pedestal and calibration events
+                    continue
+                
+                #if not muon_filter(image) #default values apply no filtering. This filter is rather useless for biased extractors anyway
+                #    continue
+            
+                muonintensityparam, size_outside_ring, muonringparam, good_ring, \
+                    radial_distribution, mean_pixel_charge_around_ring = analyze_muon_event(event_id, image, geom, equivalent_focal_length, 
+                                                                                            mirror_area, args.plot_rings, args.plots_path)
+        
+                if good_ring:
+                    num_muons = num_muons + 1
+                    print("Number of good muon rings found {}, EventID {}".format(num_muons, event_id))
+
+                # write ring data, including also "not-so-good" rings, in case we want to reconsider ring selections!:
+                output_parameters['event_id'].append(
+                    event_id)
+                output_parameters['ring_size'].append(
+                    muonintensityparam.ring_size)
+                output_parameters['size_outside'].append(
+                    size_outside_ring)
+                output_parameters['ring_radius'].append(
+                    muonringparam.ring_radius.value)
+                output_parameters['ring_width'].append(
+                    muonintensityparam.ring_width.value)
+                output_parameters['good_ring'].append(
+                    good_ring)
+                output_parameters['muon_efficiency'].append(
+                    muonintensityparam.optical_efficiency_muon)
+                output_parameters['ring_containment'].append(
+                    muonringparam.ring_containment)
+                output_parameters['ring_completeness'].append(
+                    muonintensityparam.ring_completeness)
+                output_parameters['ring_pixel_completeness'].append(
+                    muonintensityparam.ring_pix_completeness)
+                output_parameters['impact_parameter'].append(
+                    muonintensityparam.impact_parameter.value)
+                output_parameters['impact_x_array'].append(
+                    muonintensityparam.impact_parameter_pos_x.value)
+                output_parameters['impact_y_array'].append(
+                    muonintensityparam.impact_parameter_pos_y.value)
+                output_parameters['radial_stdev'].append(
+                    radial_distribution['standard_dev'].value)
+                output_parameters['radial_skewness'].append(
+                    radial_distribution['skewness'])
+                output_parameters['radial_excess_kurtosis'].append(
+                    radial_distribution['excess_kurtosis'])
+                output_parameters['num_pixels_in_ring'].append(
+                    np.sum(muonintensityparam.mask))
+                output_parameters['mean_pixel_charge_around_ring'].append(
+                    mean_pixel_charge_around_ring)
+                    
+                if num_muons == max_muons:
+                    break
         
     table = Table(output_parameters)
     if os.path.exists(args.output_file):
