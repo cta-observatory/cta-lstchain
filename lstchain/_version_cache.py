f3379debf142b695b886cc23230b3d18dd8ffe7f
misabelber <m.isabel.bernardos@gmail.com>
2020-03-25 11:23:02 +0000

    save

--- lstchain/_version_cache.py
+++ lstchain/_version_cache.py
@@ -1,13 +1,1 @@
-<<<<<<< HEAD
-version='0.3.0.post129+git2faa64b'
-=======
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-version='0.3.0.post233+git0ec4864'
-=======
-version='0.3.0.post218+gitf935945'
->>>>>>> Stashed changes
-=======
-version='0.3.0.post218+gitf935945'
->>>>>>> Stashed changes
->>>>>>> master
+version='0.3.0.post280+gitd560b4b'
