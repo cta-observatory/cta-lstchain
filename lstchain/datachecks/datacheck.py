0d08161e4458cd60370fcef2805431cd2b53c7af
Daniel Morcuende <dmorcuen@ucm.es>
2020-04-10 14:55:34 +0100

    calling plot_dl1b from datacheck

--- lstchain/datachecks/datacheck.py
+++ lstchain/datachecks/datacheck.py
@@ -1,9 +1,21 @@
-#from lstchain.datachecks import plot_dl1b
+import argparse
+from lstchain.datachecks import plot_dl1b
 
 
 def main():
-    print("Hello World!")
+    plot_dl1b.plot_dl1_params(args.infile)
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Data-check plots")
+
+    requiredNamed = parser.add_argument_group('required arguments')
+
+    requiredNamed.add_argument("--infile", '-f', type=str,
+                               dest='infile',
+                               help="Path to the DL1 input file",
+                               required=True)
+
+    args = parser.parse_args()
+
     main()
