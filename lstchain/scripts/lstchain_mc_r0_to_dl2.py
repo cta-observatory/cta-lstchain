44dcaac150bea3be20221ad449d1e76b8673bed2
vuillaut <thomas.vuillaume@gmail.com>
2020-04-10 11:33:00 +0200

    fix main

--- lstchain/scripts/lstchain_mc_r0_to_dl2.py
+++ lstchain/scripts/lstchain_mc_r0_to_dl2.py
@@ -14,50 +14,49 @@ import argparse
 import os
 from distutils.util import strtobool
 
-parser = argparse.ArgumentParser(description="Reconstruct events")
+def main():
 
-# Required arguments
-parser.add_argument('--datafile', '-f', type=str,
-                    dest='datafile',
-                    help='path to the file with simtelarray events',
-                    default=get_dataset_path('gamma_test_large.simtel.gz'))
+    parser = argparse.ArgumentParser(description="Reconstruct events")
 
-parser.add_argument('--pathmodels', '-p', action='store', type=str,
-                     dest='path_models',
-                     help='Path where to find the trained RF',
-                     default='./trained_models')
+    # Required arguments
+    parser.add_argument('--datafile', '-f', type=str,
+                        dest='datafile',
+                        help='path to the file with simtelarray events',
+                        default=get_dataset_path('gamma_test_large.simtel.gz'))
 
-# Optional argument
-parser.add_argument('--store_dl1', '-s1', action='store', type=lambda x: bool(strtobool(x)),
-                    dest='store_dl1',
-                    help='Boolean. True for storing DL1 file'
-                    'Default=False, use True otherwise',
-                    default=True)
+    parser.add_argument('--pathmodels', '-p', action='store', type=str,
+                        dest='path_models',
+                        help='Path where to find the trained RF',
+                        default='./trained_models')
 
-parser.add_argument('--outdir', '-o', action='store', type=str,
-                     dest='outdir',
-                     help='Path where to store the reco dl2 events',
-                     default='./dl2_data')
+    # Optional argument
+    parser.add_argument('--store_dl1', '-s1', action='store', type=lambda x: bool(strtobool(x)),
+                        dest='store_dl1',
+                        help='Boolean. True for storing DL1 file'
+                             'Default=False, use True otherwise',
+                        default=True)
 
+    parser.add_argument('--outdir', '-o', action='store', type=str,
+                        dest='outdir',
+                        help='Path where to store the reco dl2 events',
+                        default='./dl2_data')
 
-parser.add_argument('--config_file', '-conf', action='store', type=str,
-                    dest='config_file',
-                    help='Path to a configuration file. If none is given, a standard configuration is applied',
-                    default=None
-                    )
+    parser.add_argument('--config_file', '-conf', action='store', type=str,
+                        dest='config_file',
+                        help='Path to a configuration file. If none is given, a standard configuration is applied',
+                        default=None
+                        )
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
+    outdir = os.path.abspath(args.outdir)
+    dl1_file = os.path.join(outdir, 'dl1_' + os.path.basename(args.datafile).rsplit('.', 1)[0] + '.h5')
 
-if __name__ == '__main__':
-
-    dl1_file = args.outdir + '/dl1_' + os.path.basename(args.datafile).rsplit('.', 1)[0] + '.h5'
-
-    cmd_r0_to_dl1 = f'lstchain_mc_r0_to_dl1.py -f {args.datafile} -o {args.outdir}'
+    cmd_r0_to_dl1 = f'lstchain_mc_r0_to_dl1 -f {args.datafile} -o {outdir}'
     if args.config_file is not None:
         cmd_r0_to_dl1 = cmd_r0_to_dl1 + f' -conf {args.config_file}'
 
-    cmd_dl1_to_dl2 = f'lstchain_dl1_to_dl2.py -f {dl1_file} -p {args.path_models} -o {args.outdir}'
+    cmd_dl1_to_dl2 = f'lstchain_dl1_to_dl2 -f {dl1_file} -p {args.path_models} -o {outdir}'
     if args.config_file is not None:
         cmd_dl1_to_dl2 = cmd_dl1_to_dl2 + f' -conf {args.config_file}'
 
