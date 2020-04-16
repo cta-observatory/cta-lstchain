d30218fbd3a5ffbc3197e8658141803872ad40a3
rlopezcoto <rlopezcoto@gmail.com>
2020-04-16 17:20:36 +0200

    fix __init__

--- lstchain/reco/__init__.py
+++ lstchain/reco/__init__.py
@@ -1,5 +1,6 @@
 from .utils import *
-from .r0_to_dl1 import *
+from .r0_to_dl1 import get_dl1, add_disp_to_parameters_table
 from .dl1_to_dl2 import *
 from .volume_reducer import *
 from .disp import *
+
