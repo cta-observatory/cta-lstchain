fd5ea37c0f0830a60889cc7913a11d8fc663fda4
rlopezcoto <rlopezcoto@gmail.com>
2020-04-27 20:36:16 +0200

    reverse test to its original order

--- lstchain/scripts/tests/test_lstchain_scripts.py
+++ lstchain/scripts/tests/test_lstchain_scripts.py
@@ -43,6 +43,9 @@ def test_lstchain_mc_r0_to_dl1():
 @pytest.mark.run(after='test_lstchain_mc_r0_to_dl1')
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> a010dd5... reverse test to its original order
 def test_add_source_dependent_parameters():
     run_program('lstchain_add_source_dependent_parameters', '-f', dl1_file)
     dl1_params_src_dep = pd.read_hdf(dl1_file, key=dl1_params_src_dep_lstcam_key)
@@ -50,10 +53,13 @@ def test_add_source_dependent_parameters():
 
 
 @pytest.mark.run(after='test_lstchain_mc_r0_to_dl1')
+<<<<<<< HEAD
 def test_lstchain_mc_trainpipe():
 =======
 =======
 >>>>>>> 6585052... move src_dependent test to the endof the file
+=======
+>>>>>>> a010dd5... reverse test to its original order
 def test_lstchain_trainpipe():
 >>>>>>> 6585052... move src_dependent test to the endof the file
     gamma_file = dl1_file
@@ -99,12 +105,6 @@ def test_lstchain_dl1_to_dl2():
     )
     assert os.path.exists(dl2_file)
 
-
-@pytest.mark.run(after='test_lstchain_dl1_to_dl2')
-def test_add_source_dependent_parameters():
-    run_program('lstchain_add_source_dependent_parameters', '-f', dl1_file)
-    dl1_params_src_dep = pd.read_hdf(dl1_file, key=dl1_params_src_dep_lstcam_key)
-    assert 'alpha' in dl1_params_src_dep.columns
 
 @pytest.mark.run(after='test_lstchain_mc_r0_to_dl1')
 def test_mc_dl1ab():
