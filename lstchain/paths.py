571ade43e07fd3597236b14dd88f7b550a194b21
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2020-05-01 10:32:15 +0200

    Add muon filename, rename functions

--- lstchain/paths.py
+++ lstchain/paths.py
@@ -3,7 +3,11 @@ from collections import namedtuple
 import os
 
 
-Run = namedtuple('R0Path', 'tel_id stream run subrun')
+Run = namedtuple('R0Path', 'tel_id run subrun stream')
+# set the default of stream to None,
+# should be replaced by using the `defaults` argument to namedtuple (new in 3.7)
+# when support for python 3.6 is dropped
+Run.__new__.__defaults__ = (None, )
 R0_RE = re.compile(r'LST-(\d+).(\d+).Run(\d+).(\d+).fits.fz')
 
 DL1_RE = re.compile(
@@ -11,7 +15,7 @@ DL1_RE = re.compile(
     r'(?:.(\d+))?'       # stream is optional
     r'.Run(\d+)'         # run number
     r'.(\d+)'            # subrun number
-    r'(?:.fits)?'        # stream is optional
+    r'(?:.fits)?'        # fits extension is optional (old files had it)
     r'.(?:h5|hdf5|hdf)'  # usual extensions for hdf5 files
 )
 
@@ -23,30 +27,89 @@ def parse_int(string):
 
 
 def parse_r0_filename(filename):
-    '''Parse an raw data file name and return a nametuple of it's components'''
+    '''
+    Parse a canonical r0 file name
+    and return a ``Run`` namedtuple of it's components
+
+    Parameters
+    ----------
+    filename: str or pathlib.Path
+        the filename to parse
+
+    Returns
+    -------
+    run: Run
+        namedtuple with fields tel_id, run, subrun and stream
+
+    Raises
+    ------
+    ValueError: when the filename does not match the expected pattern
+    '''
     m = R0_RE.match(os.path.basename(filename))
 
     if m is None:
         raise ValueError(f'Filename {filename} does not match pattern {R0_RE}')
 
-    return Run(*map(parse_int, m.groups()))
+    values = [parse_int(v) for v in m.groups()]
+    return Run(tel_id=values[0], run=values[2], subrun=values[3], stream=values[1])
 
 
 def parse_dl1_filename(filename):
-    '''Parse an raw data file name and return a nametuple of it's components'''
+    '''
+    Parse canonical dl1 file name and return a ``Run`` namedtuple of it's components
+
+    Parameters
+    ----------
+    filename: str or pathlib.Path
+        the filename to parse
+
+    Returns
+    -------
+    run: Run
+        namedtuple with fields tel_id, run, subrun and stream
+
+    Raises
+    ------
+    ValueError: when the filename does not match the expected pattern
+    '''
     m = DL1_RE.match(os.path.basename(filename))
 
     if m is None:
         raise ValueError(f'Filename {filename} does not match pattern {R0_RE}')
 
-    return Run(*map(parse_int, m.groups()))
+    values = [parse_int(v) for v in m.groups()]
+    return Run(tel_id=values[0], run=values[2], subrun=values[3], stream=values[1])
 
 
-def r0_run_to_filename(run):
-    return f'LST-{run.tel_id}.{run.stream}.Run{run.run:05d}.{run.subrun:04d}.fits.fz'
+def run_to_r0_filename(tel_id, run, subrun, stream=None):
+    '''
+    Create the filename for an r0 file from telescope / run info.
+    If you have a `Run` tuple, use like this: ``r0_run_to_filename(*run)``
+    '''
+    return f'LST-{tel_id}.{stream}.Run{run:05d}.{subrun:04d}.fits.fz'
 
 
-def dl1_run_to_filename(run):
-    if run.stream is None:
-        return f'dl1_LST-{run.tel_id}.Run{run.run:05d}.{run.subrun:04d}.h5'
-    return f'dl1_LST-{run.tel_id}.{run.stream}.Run{run.run:05d}.{run.subrun:04d}.h5'
+def run_to_dl1_filename(tel_id, run, subrun, stream=None):
+    '''
+    Create the filename for a dl1 file from telescope / run info.
+    If you have a `Run` tuple, use like this: ``r0_run_to_filename(*run)``
+    '''
+    if stream is None:
+        return f'dl1_LST-{tel_id}.Run{run:05d}.{subrun:04d}.h5'
+    return f'dl1_LST-{tel_id}.{stream}.Run{run:05d}.{subrun:04d}.h5'
+
+
+def run_to_muon_filename(tel_id, run, subrun, stream=None, gzip=True):
+    '''
+    Create the filename for a muon output file from telescope / run info.
+    If you have a `Run` tuple, use like this: ``r0_run_to_filename(*run)``
+    '''
+    if stream is None:
+        name = f'muon_LST-{tel_id}.Run{run:05d}.{subrun:04d}.fits'
+    else:
+        name = f'muon_LST-{tel_id}.{stream}.Run{run:05d}.{subrun:04d}.fits'
+
+    if gzip:
+        name += '.gz'
+
+    return name
--- lstchain/tests/test_paths.py
+++ lstchain/tests/test_paths.py
@@ -6,37 +6,44 @@ def test_parse_dl1():
 
     run = parse_dl1_filename('dl1_LST-1.1.Run01920.0000.fits.h5')
     assert run.tel_id == 1
-    assert run.stream == 1
     assert run.run == 1920
     assert run.subrun == 0
+    assert run.stream == 1
 
     run = parse_dl1_filename('dl1_LST-1.Run01920.0000.fits.h5')
     assert run.tel_id == 1
-    assert run.stream is None
     assert run.run == 1920
     assert run.subrun == 0
+    assert run.stream is None
 
     run = parse_dl1_filename('dl1_LST-1.Run01920.0000.h5')
     assert run.tel_id == 1
-    assert run.stream is None
     assert run.run == 1920
     assert run.subrun == 0
+    assert run.stream is None
 
     run = parse_dl1_filename('dl1_LST-1.Run01920.0000.fits.hdf5')
     assert run.tel_id == 1
-    assert run.stream is None
     assert run.run == 1920
     assert run.subrun == 0
+    assert run.stream is None
 
     with pytest.raises(ValueError):
         run = parse_dl1_filename('foo.fits.fz')
 
 
 def test_dl1_to_filename():
-    from lstchain.paths import Run, dl1_run_to_filename
+    from lstchain.paths import run_to_dl1_filename, Run
 
-    assert dl1_run_to_filename(Run(1, None, 1920, 2)) == 'dl1_LST-1.Run01920.0002.h5'
-    assert dl1_run_to_filename(Run(1, 2, 1920, 3)) == 'dl1_LST-1.2.Run01920.0003.h5'
+    assert run_to_dl1_filename(
+        tel_id=1, run=1920, subrun=2
+    ) == 'dl1_LST-1.Run01920.0002.h5'
+    assert run_to_dl1_filename(
+        tel_id=1, run=1920, subrun=3, stream=2
+    ) == 'dl1_LST-1.2.Run01920.0003.h5'
+
+    run = Run(tel_id=2, run=5, subrun=1)
+    assert run_to_dl1_filename(*run) == 'dl1_LST-2.Run00005.0001.h5'
 
 
 def test_parse_r0():
@@ -53,6 +60,24 @@ def test_parse_r0():
 
 
 def test_r0_to_filename():
-    from lstchain.paths import Run, r0_run_to_filename
+    from lstchain.paths import run_to_r0_filename
 
-    assert r0_run_to_filename(Run(1, 2, 1920, 3)) == 'LST-1.2.Run01920.0003.fits.fz'
+    assert run_to_r0_filename(
+        tel_id=1, run=1920, subrun=3, stream=2
+    ) == 'LST-1.2.Run01920.0003.fits.fz'
+
+
+def test_muon_filename():
+    from lstchain.paths import run_to_muon_filename
+
+    assert run_to_muon_filename(
+        tel_id=1, run=2, subrun=3
+    ) == 'muon_LST-1.Run00002.0003.fits.gz'
+
+    assert run_to_muon_filename(
+        tel_id=1, run=2, subrun=3, gzip=False
+    ) == 'muon_LST-1.Run00002.0003.fits'
+
+    assert run_to_muon_filename(
+        tel_id=1, run=2, subrun=3, stream=4
+    ) == 'muon_LST-1.4.Run00002.0003.fits.gz'
