9a46bbd2ea5bd3bd19a087a45db50a4d1127dd58
vuillaut <thomas.vuillaume@gmail.com>
2020-06-11 12:37:05 +0200

    fix bug in plot feature importance when figure already exists

--- lstchain/visualization/plot_dl2.py
+++ lstchain/visualization/plot_dl2.py
@@ -484,6 +484,8 @@ def plot_models_features_importances(path_models, config_file=None, axes=None, *
 
     if axes is None:
         fig, axes = plt.subplots(1, 3, figsize=(20, 5))
+    else:
+        fig = axes[0].get_figure()
 
     fig.suptitle('Features importances')
 
