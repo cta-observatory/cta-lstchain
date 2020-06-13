716a147804719b8b635ac3ac370a37bf7c53ce7c
Paweł Gliwny <p.gliwny@gmail.com>
2020-06-13 13:43:24 +0200

    clean up cleaning.py file

--- lstchain/image/cleaning/cleaning.py
+++ lstchain/image/cleaning/cleaning.py
@@ -1,7 +1,17 @@
+import h5py
+import tables
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+from ctapipe.instrument import CameraGeometry
+from ctapipe.visualization import CameraDisplay
+
+
 def tailcuts_clean_with_pedestal_threshold(
     geom,
     image,
-    interleaved_pedestal_threshold,
+    interleaved_pedestal_thresh,
     picture_thresh=7,
     boundary_thresh=5,
     keep_isolated_pixels=False,
@@ -9,7 +19,7 @@ def tailcuts_clean_with_pedestal_threshold(
 ):
 
     pixels_above_picture = np.logical_and(image>= picture_thresh,
-                                          image >= interleaved_pedestal_threshold)
+                                          image >= interleaved_pedestal_thresh)
 
     if keep_isolated_pixels or min_number_picture_neighbors == 0:
         pixels_in_picture = pixels_above_picture
@@ -39,3 +49,24 @@ def tailcuts_clean_with_pedestal_threshold(
         return (pixels_above_boundary & pixels_with_picture_neighbors) | (
             pixels_in_picture & pixels_with_boundary_neighbors
 )
+
+
+
+def get_bias_and_rms(dl1_file):
+    "return bias (mean) and rms from pedestal events in pe"
+
+    f = tables.open_file(dl1_file)
+    ped = f.root['/dl1/event/telescope/monitoring/pedestal']
+    print("n events = {}".format(np.array(ped.cols.n_events)))
+    ped_charge_mean = np.array(ped.cols.charge_mean)
+    ped_charge_rms = np.array(ped.cols.charge_std)
+    calib = f.root['/dl1/event/telescope/monitoring/calibration']
+    dc_to_pe = np.array(calib.cols.dc_to_pe)
+    ped_charge_mean_pe = ped_charge_mean*dc_to_pe
+    ped_charge_rms_pe = ped_charge_rms*dc_to_pe
+    f.close()
+    return ped_charge_mean_pe, ped_charge_rms_pe
+
+def get_threshold(ped_mean_pe, ped_rms_pe, sigma_clean):
+    threshold_clean_pe = ped_mean_pe + sigma_clean*ped_rms_pe
+    return threshold_clean_pe
