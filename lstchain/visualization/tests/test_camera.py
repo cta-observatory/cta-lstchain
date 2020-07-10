016f843763daa391ff1797f5f9e2fb54a4bb0f5a
rlopezcoto <rlopezcoto@gmail.com>
2020-07-10 17:39:14 +0200

    put back the source overlay

--- lstchain/visualization/tests/test_camera.py
+++ lstchain/visualization/tests/test_camera.py
@@ -1,15 +1,17 @@
-import numpy as np
-from lstchain.visualization.camera import overlay_source, overlay_disp_vector, display_dl1_event
-from lstchain.reco.disp import disp_parameters_event
-from ctapipe.visualization import CameraDisplay
-from ctapipe.instrument import CameraGeometry
 import astropy.units as u
+import numpy as np
+from ctapipe.instrument import CameraGeometry
+from ctapipe.visualization import CameraDisplay
+
+from lstchain.reco.disp import disp_parameters_event
+from lstchain.visualization.camera import overlay_source, overlay_disp_vector, display_dl1_event
+
 
 def test_overlay_source():
     geom = CameraGeometry.from_name('LSTCam')
     image = np.random.rand(geom.n_pixels)
     display = CameraDisplay(geom, image)
-    #overlay_source(display, 0.1 * u.m, 0.3 * u.m)
+    overlay_source(display, 0.1 * u.m, 0.3 * u.m)
 
 
 def test_overlay_disp_vector():
