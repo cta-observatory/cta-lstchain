a166f70f78c9f68dca9e34b7f892c950993118b7
Abelardo Moralejo Olaizola <moralejo@ifae.es>
2020-07-12 01:30:36 +0200

    Removed the duplicated masks for the DL1 parameters and DL1 images tables, since they have one-to-one corresponding rows. Besides, code did not work because images table now lacks event_id

--- lstchain/datachecks/dl1_checker.py
+++ lstchain/datachecks/dl1_checker.py
@@ -262,25 +262,16 @@ def process_dl1_file(filename, bins, trigger_source='trigger_type'):
                            ff_min_pixel_charge_median) &
                           (np.std(image, axis=1) <
                            ff_max_pixel_charge_stddev))
-        # obtain the corresponding mask for the parameters table:
-        ff_indices = image_table.col('event_id')[flatfield_mask]
-        params_flatfield_mask = np.array(
-                [(True if evtid in ff_indices else False) for evtid in
-                 parameters['event_id']])
+        # The same mask should be valid for image_table, since the entry in
+        # the two tables correspond one to one.
 
         # then use trigger_source (name of one of the trigger tags in the DL1
         # file) to try to identify pedestals on the parameters table (but we
         # trust better the above empirical identification of flatfield events):
-        params_pedestal_mask = (parameters[trigger_source] == 32) & \
-            ~params_flatfield_mask
-        # obtain the corresponding pedestal mask for the images table:
-        ped_indices = np.array(parameters['event_id'][params_pedestal_mask])
-        pedestal_mask = np.array([(True if evtid in ped_indices else False)
-                                  for evtid in image_table.col('event_id')])
+        pedestal_mask = (parameters[trigger_source] == 32) & ~flatfield_mask
 
         # Now obtain by exclusion the masks for cosmics:
         cosmics_mask = ~(pedestal_mask | flatfield_mask)
-        params_cosmics_mask = ~(params_pedestal_mask | params_flatfield_mask)
 
         logger.info(f'   pedestals: {np.sum(pedestal_mask)}, '
                     f' flatfield: {np.sum(flatfield_mask)}, '
@@ -288,19 +279,19 @@ def process_dl1_file(filename, bins, trigger_source='trigger_type'):
 
         # fill quantities which depend on event-wise (i.e. not
         # pixel-wise) parameters:
-        if params_pedestal_mask.sum() > 1:
+        if pedestal_mask.sum() > 1:
             dl1datacheck_pedestals.fill_event_wise_info(subrun_index,
                                                         parameters,
-                                                        params_pedestal_mask,
+                                                        pedestal_mask,
                                                         geom, bins)
-        if params_flatfield_mask.sum() > 1:
+        if flatfield_mask.sum() > 1:
             dl1datacheck_flatfield.fill_event_wise_info(subrun_index,
                                                         parameters,
-                                                        params_flatfield_mask,
+                                                        flatfield_mask,
                                                         geom, bins)
-        if params_cosmics_mask.sum() > 1:
+        if cosmics_mask.sum() > 1:
             dl1datacheck_cosmics.fill_event_wise_info(subrun_index, parameters,
-                                                      params_cosmics_mask,
+                                                      cosmics_mask,
                                                       geom, bins)
 
         # now fill pixel-wise information:
@@ -319,14 +310,14 @@ def process_dl1_file(filename, bins, trigger_source='trigger_type'):
 
         # Return None for a container that has not been completely filled,
         # otherwise it will give trouble in the plotting stage.
-        if pedestal_mask.sum() == 0 or params_pedestal_mask.sum() == 0:
+        if pedestal_mask.sum() == 0:
             dl1datacheck_pedestals = None
-        if flatfield_mask.sum() == 0 or params_flatfield_mask.sum() == 0:
+        if flatfield_mask.sum() == 0:
             dl1datacheck_flatfield = None
-        if cosmics_mask.sum() == 0 or params_cosmics_mask.sum() == 0:
+        if cosmics_mask.sum() == 0:
             dl1datacheck_cosmics = None
 
-                # in case event sof some type are missing, just issue a warning and
+        # in case events of some type are missing, just issue a warning and
         # retun None for the corresponding container, to avoid catastrophic
         # failure when trying to write it out
 
