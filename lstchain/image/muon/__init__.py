0468a651d258e252c33f178a045817c7c03d8849
Abelardo Moralejo Olaizola <moralejo@ifae.es>
2020-07-17 10:09:19 +0200

    Fixed order of imports

--- lstchain/image/muon/__init__.py
+++ lstchain/image/muon/__init__.py
@@ -1,3 +1,4 @@
+from .intensity_fitter import MuonIntensityFitter
 from .plot_muon import plot_muon_event
 from .muon_analysis import (
     analyze_muon_event,
@@ -6,11 +7,9 @@ from .muon_analysis import (
     fill_muon_event,
     pixel_coords_to_telescope,
 )
-from .intensity_fitter import (
-    MuonIntensityFitter
-)
 
 __all__ = [
+    'MuonIntensityFitter',
     'analyze_muon_event',
     'tag_pix_thr',
     'create_muon_table',
