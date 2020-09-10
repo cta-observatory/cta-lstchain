3508096922ce2d20718f5b49b4cbfd134cf2df99
rlopezcoto <rlopezcoto@gmail.com>
2020-09-10 11:51:18 +0200

    update to astropy v4

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     version=version,
     packages=find_packages(),
     install_requires=[
-        "astropy>=3,<5",
+        "astropy>3,<5",
         'ctapipe~=0.8.0',
         'ctaplot~=0.5.3',
         "eventio>=1.1.1,<2.0.0a0",  # at least 1.1.1, but not 2
