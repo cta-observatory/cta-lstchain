7719916595289b857fcf60fe5f2efce9fe0de6ac
mari-taka <maritaka@icrr.u-tokyo.ac.jp>
2020-09-16 07:48:22 +0000

    modified write_array_info08() like write_array_info().

--- lstchain/io/io.py
+++ lstchain/io/io.py
@@ -363,10 +363,51 @@ def write_array_info_08(subarray, output_filename):
     subarray: `ctapipe.instrument.subarray.SubarrayDescription`
     output_filename: str
     """
-    stage1 = Stage1ProcessorTool()
-    stage1.output_path = output_filename
-    stage1._write_instrument_configuration(subarray)
 
+    serialize_meta = True
+
+    subarray.to_table().write(
+      output_filename,
+      path="/configuration/instrument/subarray/layout",
+      serialize_meta=serialize_meta,
+      append=True,
+    )
+
+    subarray.to_table(kind="optics").write(
+      output_filename,
+      path="/configuration/instrument/telescope/optics",
+      append=True,
+      serialize_meta=serialize_meta,
+    )
+
+    for telescope_type in subarray.telescope_types:
+      ids = set(subarray.get_tel_ids_for_type(telescope_type))
+      if len(ids) > 0: # only write if there is a telescope with this camera
+        tel_id = list(ids)[0]
+        camera = subarray.tel[tel_id].camera
+        camera_name = f'geometry_{camera}'
+        with tables.open_file(output_filename, mode='a') as f:
+          telescope_chidren = f.root['/configuration/instrument/telescope']._v_children.keys()
+          if 'camera' in telescope_chidren:
+            cameras_name = f.root['/configuration/instrument/telescope/camera']._v_children.keys()
+            if camera_name in cameras_name:
+              print(
+                f'WARNING during lstchain.io.write_array_info_08():',
+                f'camera {camera_name} seems to be already present in the h5 file.'
+              )
+              continue
+        camera.geometry.to_table().write(
+          output_filename,
+          path=f"/configuration/instrument/telescope/camera/geometry_{camera}",
+          append=True,
+          serialize_meta=serialize_meta          
+        )
+        camera.readout.to_table().write(
+          output_filename,
+          path=f"/configuration/instrument/telescope/camera/readout_{camera}",
+          append=True,
+          serialize_meta=serialize_meta
+        )
 
 @deprecated('09/07/2020', message='this function will disappear in lstchain v0.7')
 def write_array_info(subarray, output_filename):
