fd1fdab5d1040ee9c72c7fc777f522187d5d1894
Franca Cassol <cassol@cppm.in2p3.fr>
2020-09-23 16:57:13 +0200

    Modify the default excess noise factor

--- lstchain/calib/camera/calibration_calculator.py
+++ lstchain/calib/camera/calibration_calculator.py
@@ -36,7 +36,7 @@ class CalibrationCalculator(Component):
 
     """
     squared_excess_noise_factor = Float(
-        1.2,
+        1.222,
         help='Excess noise factor squared: 1+ Var(gain)/Mean(Gain)**2'
     ).tag(config=True)
 
