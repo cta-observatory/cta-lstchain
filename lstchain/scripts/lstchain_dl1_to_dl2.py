9cd4dd5ea1b12c4376b95d7c0e0d35e156fb3ac6
Enrique <garcia@lapp.in2p3.fr>
2020-11-04 15:30:32 +0100

    simplify output

--- lstchain/scripts/lstchain_dl1_to_dl2.py
+++ lstchain/scripts/lstchain_dl1_to_dl2.py
@@ -74,7 +74,7 @@ def main():
     os.rename(args.input_file, args.input_file.replace('dl1_', 'dl1_aligned_ctapipe08_'))
 
     input_reorganizer = args.input_file.replace('dl1_', 'dl1_aligned_ctapipe08_')
-    output_reorganizer = args.input_file.replace('dl1_aligned_ctapipe08_', 'dl1_')
+    output_reorganizer = args.input_file
 
     hiperta_lstchain_reorganzier(input_reorganizer, output_reorganizer)
     # TODO erase dl1_hiperta ?
