98e07ecb439ecc66a10cf2b7da348015337a90d3
rlopezcoto <rlopezcoto@gmail.com>
2020-11-12 13:01:55 +0100

    print io

--- lstchain/scripts/lstchain_merge_muon_files.py
+++ lstchain/scripts/lstchain_merge_muon_files.py
@@ -4,9 +4,12 @@
 Merge all fits files resulting 
 from parallel muon reconstructions present in a 
 directory. 
+
 - Input: several fits files.
 - Output single fits file.
+
 Usage: 
+
 $> python lstchain_merge_muon_files.py
 --input-dir ./
 --run-number 1881
@@ -14,8 +17,9 @@ $> python lstchain_merge_muon_files.py
 
 import argparse
 import os
-from astropy.table import Table, vstack
 from glob import glob
+from astropy.table import Table, vstack
+
 
 parser = argparse.ArgumentParser(description='Merge fits files')
 
@@ -56,12 +60,23 @@ def main():
         file_list = sorted(glob(os.path.join(args.srcdir, args.pattern)))
 
 
+    if not file_list:
+        raise IOError(f'No muon files in {args.srcdir} with the parameters requested')
+
     tab = Table.read('{}'.format(file_list[0]), format='fits')
     for i in range(1,len(file_list)):
         tab2 = Table.read('{}'.format(file_list[i]), format='fits')
         tab = vstack([tab, tab2])
 
-        
+
+    if os.path.exists(args.outfile):
+        raise IOError(args.outfile + ' exists, exiting.')
+
+    tab.write(args.outfile)
+
 
 if __name__ == '__main__':
     main()
+
+
+    
