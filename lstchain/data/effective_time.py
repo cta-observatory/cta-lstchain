67613793dfa373853631151a4cdcddf9b029d6b3
misabelber <m.isabel.bernardos@gmail.com>
2020-11-17 16:11:11 +0100

    remove prints

--- lstchain/data/effective_time.py
+++ lstchain/data/effective_time.py
@@ -19,14 +19,6 @@ def get_effecive_time(events):
     rate=1/np.mean(deltat)
     dead_time = np.amin(deltat)
     t_elapsed = events.shape[0]/rate * u.s
-    total_dead_time=events.shape[0]*dead_time
     t_eff = t_elapsed/(1+rate*dead_time)
-
-    print("ELAPSED TIME: %.2f s\n" % t_elapsed.to_value(),
-          "EFFECTIVE TIME: %.2f s\n" % t_eff.to_value(),
-          "DEAD TIME: %.2E s\n" % dead_time,
-          "TOTAL DEAD TIME: %.2f s\n" % total_dead_time,
-          "RATE: %.2f 1/s\n" % rate
-    )
 
     return t_eff
