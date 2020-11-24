5831fb42f363d3da38a6632b003d6f20340ccad0
Jose Enrique Ruiz <jer@iaa.es>
2020-11-24 23:05:47 +0100

    use consistent argument names

--- lstchain/tools/lstchain_create_calibration_file.py
+++ lstchain/tools/lstchain_create_calibration_file.py
@@ -18,8 +18,12 @@ class CalibrationHDF5Writer(Tool):
     name = "CalibrationHDF5Writer"
     description = "Generate a HDF5 file with camera calibration coefficients"
 
-    one_event = Bool(False, help="Stop after first calibration event").tag(config=True)
-    output_file = Unicode("calibration.hdf5", help="Name of the output file").tag(config=True)
+    one_event = traits.Bool(False, help="Stop after first calibration event").tag(config=True)
+    output = traits.Path(
+        help="Name of the output file",
+        directory_ok=False,
+        default_value="calibration.hdf5"
+    ).tag(config=True)
     calibration_product = traits.create_class_enum_trait(
         CalibrationCalculator, default_value="LSTCalibrationCalculator"
     )
@@ -28,8 +32,8 @@ class CalibrationHDF5Writer(Tool):
     )
 
     aliases = {
-            "input_file": "EventSource.input_url",
-            "output_file": "CalibrationHDF5Writer.output_file",
+            "input": "EventSource.input_url",
+            "output": "CalibrationHDF5Writer.output",
             "max_events": "EventSource.max_events",
             "calibration_product": "CalibrationHDF5Writer.calibration_product",
             "r0calibrator_product": "CalibrationHDF5Writer.r0calibrator_product",
@@ -61,8 +65,8 @@ class CalibrationHDF5Writer(Tool):
 
     def setup(self):
 
-        self.log.debug(f"Open file")
         self.eventsource = EventSource.from_config(parent=self)
+        self.log.debug(self.eventsource.input_url)
 
         # if data remember how many event in the files
         if "LSTEventSource" in str(type(self.eventsource)):
@@ -81,9 +85,9 @@ class CalibrationHDF5Writer(Tool):
                 self.r0calibrator_product, parent=self
             )
 
-        self.log.debug(f"Open output file {self.output_file}")
+        self.log.debug(f"Open output file {self.output}")
         self.writer = HDF5TableWriter(
-            filename=self.output_file, group_name=f"tel_{self.processor.tel_id}", overwrite=True
+            filename=self.output, group_name=f"tel_{self.processor.tel_id}", overwrite=True
         )
 
     def start(self):
@@ -214,7 +218,7 @@ class CalibrationHDF5Writer(Tool):
 
     def finish(self):
         Provenance().add_output_file(
-            self.output_file,
+            self.output,
             role='mon.tel.calibration'
         )
         self.writer.close()
