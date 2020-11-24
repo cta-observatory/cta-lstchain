d224770e0bf55a46c81c429c66fddbbfd9da1f59
Jose Enrique Ruiz <jer@iaa.es>
2020-11-24 19:46:22 +0100

    add subcomponents

--- lstchain/tools/lstchain_create_time_calibration_file.py
+++ lstchain/tools/lstchain_create_time_calibration_file.py
@@ -17,20 +17,19 @@ class TimeCalibrationHDF5Writer(Tool):
     description = "Generate a HDF5 file with time calibration coefficients"
 
     input_file = traits.Unicode(
-        help="Path to the input file containing events (wildcards allowed)",
+        help="Path to the fits.fz input file containing events (wildcards allowed)",
     ).tag(config=True)
 
     output_file = traits.Unicode(
-        help="Path to the time calibration file",
+        help="Path to the hdf5 time calibration file",
     ).tag(config=True)
 
     pedestal_file = traits.Unicode(
-        help="Path to drs4 pedestal file",
+        help="Path to drs4 fits pedestal file",
     ).tag(config=True)
 
     max_events = traits.Int(
-        help="Maximum numbers of events to read. Default = 20000",
-        default_value=20000
+        help="Maximum numbers of events to read. Default = 20000", default_value=20000
     ).tag(config=True)
 
     aliases = {
@@ -50,7 +49,7 @@ class TimeCalibrationHDF5Writer(Tool):
         For getting help run:
         lstchain_create_time_calibration_file --help
         """
-
+        self.reader = None
         self.timeCorr = None
         self.path_list = None
         self.lst_r0 = None
@@ -58,14 +57,19 @@ class TimeCalibrationHDF5Writer(Tool):
     def setup(self):
 
         self.path_list = sorted(glob.glob(self.input_file))
-        reader = LSTEventSource(input_url=self.path_list[0], max_events=self.max_events)
-        self.lst_r0 = LSTR0Corrections(
-            pedestal_path=self.pedestal_file,
-            config=self.config,
+        self.reader = self.add_component(
+            LSTEventSource(
+                input_url=self.path_list[0],
+                max_events=self.max_events,
+                config=self.config,
+            )
+        )
+        self.lst_r0 = self.add_component(
+            LSTR0Corrections(pedestal_path=self.pedestal_file, parent=self)
         )
         self.timeCorr = TimeCorrectionCalculate(
             calib_file_path=self.output_file,
-            subarray=reader.subarray,
+            subarray=self.reader.subarray,
             config=self.config,
         )
 
@@ -73,10 +77,10 @@ class TimeCalibrationHDF5Writer(Tool):
 
         try:
             for j, path in enumerate(self.path_list):
-                reader = LSTEventSource(input_url=self.path_list[0], max_events=self.max_events)
+                self.reader.input_url = path
                 self.log.info(f"File {j + 1} out of {len(self.path_list)}")
                 self.log.info(f"Processing: {path}")
-                for i, event in enumerate(reader):
+                for i, event in enumerate(self.reader):
                     if i % 5000 == 0:
                         self.log.debug(f"i = {i}, ev id = {event.index.event_id}")
                     self.lst_r0.calibrate(event)
