842b906a22d49b2e54415e8abc331be246d8e27a
Thomas Vuillaume <thomas.vuillaume@gmail.com>
2020-11-24 16:35:23 +0100

    Add ctapipe-extra to setup.py
    ctapipe-extra is actually required when we load telescope or optics info from `ctapipe_resources` such as `OpticsDescription.from_name`
    
    It otherwise induces a bug when lstchain is installed without the conda env.

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
     install_requires=[
         "astropy~=4.0,>=4.0.2",
         'ctapipe~=0.8.0',
+        'ctapipe-extra~=0.3',
         'ctaplot~=0.5.3',
         "eventio>=1.1.1,<2.0.0a0",  # at least 1.1.1, but not 2
         'gammapy>=0.18',
