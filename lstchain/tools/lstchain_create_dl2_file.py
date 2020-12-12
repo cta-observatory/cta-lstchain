4bff53dce2ac99d60a0df2b82293a8b45e802a8b
Jose Enrique Ruiz <jer@iaa.es>
2020-12-12 20:26:54 +0100

    add apply models

--- lstchain/tools/lstchain_create_dl2_file.py
+++ lstchain/tools/lstchain_create_dl2_file.py
@@ -6,19 +6,26 @@ import joblib
 import numpy as np
 import pandas as pd
 from ctapipe.core import Provenance, Tool, traits
-from lstchain.io import (get_dataset_keys, read_configuration_file,
-                         replace_config, standard_config, write_dl2_dataframe)
-from lstchain.io.io import (dl1_images_lstcam_key, dl1_params_lstcam_key,
-                            dl1_params_src_dep_lstcam_key)
+from lstchain.io import (
+    get_dataset_keys,
+    read_configuration_file,
+    replace_config,
+    standard_config,
+    write_dl2_dataframe,
+)
+from lstchain.io.io import (
+    dl1_images_lstcam_key,
+    dl1_params_lstcam_key,
+    dl1_params_src_dep_lstcam_key,
+)
 from lstchain.reco import dl1_to_dl2
 from lstchain.reco.utils import filter_events, impute_pointing
 from tables import open_file
-from tqdm.autonotebook import tqdm
 
 
 class ReconstructionHDF5Writer(Tool):
 
-    name = "CalibrationHDF5Writer"
+    name = "ReconstructionHDF5Writer"
     description = (
         "Generate a HDF5 file with reconstructed energy, disp and hadroness of events"
     )
@@ -34,13 +41,8 @@ class ReconstructionHDF5Writer(Tool):
     ).tag(config=True)
     config_file = traits.Path(
         help="Path to a configuration file. If none is given, a standard configuration is applied",
-        directory_ok=False
+        directory_ok=False,
     ).tag(config=True)
-
-    # progress_bar = traits.Bool(
-    #     help="show progress bar during processing",
-    #     default_value=True,
-    # ).tag(config=True)
 
     aliases = {
         "input": "ReconstructionHDF5Writer.input",
@@ -61,18 +63,58 @@ class ReconstructionHDF5Writer(Tool):
         """
         self.configuration = None
         self.data = None
+        self.reg_energy = None
+        self.reg_disp_vector = None
+        self.cls_gh = None
+        self.dl2 = None
 
     def setup(self):
+
+        self.log.info("Reading configuration")
         self.configuration = standard_config
         if self.config_file:
             self.configuration = read_configuration_file(self.config_file)
+
+        self.log.info("Reading DL1 file")
         self.data = pd.read_hdf(self.input, key=dl1_params_lstcam_key)
-        if self.configuration['source_dependent']:
+        if self.configuration["source_dependent"]:
             data_src_dep = pd.read_hdf(self.input, key=dl1_params_src_dep_lstcam_key)
             self.data = pd.concat([self.data, data_src_dep], axis=1)
 
+        self.log.info("Reading RF models")
+        self.reg_energy = joblib.load(self.path_models / "reg_energy.sav")
+        self.reg_disp_vector = joblib.load(self.path_models / "reg_disp_vector.sav")
+        self.cls_gh = joblib.load(self.path_models / "cls_gh.sav")
+
     def start(self):
-        pass
+
+        # dealing with pointing missing values
+        # this happened when `ucts_time` was invalid
+        if (
+            "alt_tel" in self.data.columns and "az_tel" in self.data.columns
+            and (np.isnan(self.data.alt_tel).any() or np.isnan(self.data.az_tel).any())
+        ):
+            # make sure there is a least one good pointing value to interpolate from
+            if np.isfinite(self.data.alt_tel).any() and np.isfinite(self.data.az_tel).any():
+                self.data = impute_pointing(self.data)
+            else:
+                self.data.alt_tel = -np.pi / 2.0
+                self.data.az_tel = -np.pi / 2.0
+        self.data = filter_events(
+            self.data,
+            filters=self.configuration["events_filters"],
+            finite_params=self.configuration["regression_features"]
+            + self.configuration["classification_features"],
+        )
+
+        self.log.info("Applying models to data")
+        self.dl2 = dl1_to_dl2.apply_models(
+            self.data,
+            self.cls_gh,
+            self.reg_energy,
+            self.reg_disp_vector,
+            custom_config=self.configuration,
+        )
 
     def finish(self):
         pass
