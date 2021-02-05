478a853891cf088e087305ca0c88d4f7150e7f20
Daniel Morcuende <dmorcuen@ucm.es>
2021-02-05 10:46:20 +0100

    update disp parameters only if already present in the file

--- lstchain/scripts/lstchain_dl1ab.py
+++ lstchain/scripts/lstchain_dl1ab.py
@@ -105,11 +105,6 @@ def main():
         'log_intensity',
         'x',
         'y',
-        'disp_dx',
-        'disp_dy',
-        'disp_norm',
-        'disp_angle',
-        'disp_sign',
     ])
 
     nodes_keys = get_dataset_keys(args.input_file)
@@ -120,6 +115,8 @@ def main():
 
     with tables.open_file(args.input_file, mode='r') as input:
         image_table = input.root[dl1_images_lstcam_key]
+        dl1_params_input = input.root[dl1_params_lstcam_key].colnames
+
         with tables.open_file(args.output_file, mode='a') as output:
 
             params = output.root[dl1_params_lstcam_key].read()
@@ -161,16 +158,27 @@ def main():
                     dl1_container.r = np.sqrt(dl1_container.x ** 2 + dl1_container.y ** 2)
                     dl1_container.log_intensity = np.log10(dl1_container.intensity)
 
-                disp_dx, disp_dy, disp_norm, disp_angle, disp_sign = disp(dl1_container['x'].to_value(u.m),
-                                                                          dl1_container['y'].to_value(u.m),
-                                                                          params['src_x'][ii],
-                                                                          params['src_y'][ii],
-                                                                          )
-                dl1_container['disp_dx'] = disp_dx
-                dl1_container['disp_dy'] = disp_dy
-                dl1_container['disp_norm'] = disp_norm
-                dl1_container['disp_angle'] = disp_angle
-                dl1_container['disp_sign'] = disp_sign
+                if ['disp_dx', 'disp_dy', 'disp_norm', 'disp_angle', 'disp_sign'] in dl1_params_input:
+                    parameters_to_update.extend([
+                        'disp_dx',
+                        'disp_dy',
+                        'disp_norm',
+                        'disp_angle',
+                        'disp_sign'
+                    ])
+
+                    disp_dx, disp_dy, disp_norm, disp_angle, disp_sign = disp(
+                        dl1_container['x'].to_value(u.m),
+                        dl1_container['y'].to_value(u.m),
+                        params['src_x'][ii],
+                        params['src_y'][ii]
+                    )
+
+                    dl1_container['disp_dx'] = disp_dx
+                    dl1_container['disp_dy'] = disp_dy
+                    dl1_container['disp_norm'] = disp_norm
+                    dl1_container['disp_angle'] = disp_angle
+                    dl1_container['disp_sign'] = disp_sign
 
                 for p in parameters_to_update:
                     params[ii][p] = u.Quantity(dl1_container[p]).value
