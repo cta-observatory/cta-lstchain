0f247b92aa92612b4d7b7cc6125f772abd59e7da
misabelber <m.isabel.bernardos@gmail.com>
2021-02-25 14:26:42 +0100

    fix spaces

--- lstchain/reco/tests/test_utils.py
+++ lstchain/reco/tests/test_utils.py
@@ -90,8 +90,8 @@ def test_filter_events():
 def test_get_obstime_real():
     t_obs = 600
     rate = 10e3
-    n_events =  np.random.poisson(rate * t_obs)
+    n_events = np.random.poisson(rate * t_obs)
     timestamps = np.sort(np.random.uniform(0, t_obs, n_events))
     events = pd.DataFrame({'dragon_time': timestamps})
-                          
+    
     assert np.isclose(utils.get_effective_time(events)[0].value, t_obs)
