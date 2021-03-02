ade438a7331aecf4ad4051b607054a28ad401ec1
Paweł Gliwny <p.gliwny@gmail.com>
2021-03-02 17:50:30 +0100

    add test to read clean param

--- lstchain/io/tests/test_config.py
+++ lstchain/io/tests/test_config.py
@@ -1,5 +1,5 @@
 from lstchain.io import config
-
+from lstchain.io.config import get_cleaning_parameters
 
 def test_get_standard_config():
     config.get_standard_config()
@@ -11,3 +11,12 @@ def test_replace_config():
     assert c["toto"] == 1
     assert c["tata"] == 3
     assert c["tutu"] == 4
+
+def test_get_cleaning_parameters():
+    std_config = config.get_standard_config()
+    cleaning_params = get_cleaning_parameters(std_config, 'tailcut')
+    picture_th, boundary_th, isolated_pixels, min_n_neighbors = cleaning_params
+    assert std_config['tailcut']['picture_thresh'] == picture_th
+    assert std_config['tailcut']['boundary_thresh'] == boundary_th
+    assert std_config['tailcut']['keep_isolated_pixels'] == isolated_pixels
+    assert std_config['tailcut']['min_number_picture_neighbors'] == min_n_neighbors
