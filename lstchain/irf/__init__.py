dfc140574d9f96c18984370493bf5acec5a238ef
chaimain <cpriyadarshi@ifae.es>
2021-03-03 17:18:36 +0100

    Updated with Black formatting

--- lstchain/irf/__init__.py
+++ lstchain/irf/__init__.py
@@ -1,9 +1,6 @@
 from .hdu_table import (
     create_obs_hdu_index,
     create_event_list,
-    )
+)
 
-__all__ = [
-    'create_obs_hdu_index',
-    'create_event_list'
-    ]
+__all__ = ["create_obs_hdu_index", "create_event_list"]
--- lstchain/scripts/tests/test_lstchain_scripts.py
+++ lstchain/scripts/tests/test_lstchain_scripts.py
@@ -12,7 +12,7 @@ from lstchain.io.io import (
     dl1_params_lstcam_key,
     dl2_params_lstcam_key,
     get_dataset_keys,
-    dl1_params_src_dep_lstcam_key
+    dl1_params_src_dep_lstcam_key,
 )
 from lstchain.tests.test_lstchain import (
     test_drive_report,
@@ -20,8 +20,9 @@ from lstchain.tests.test_lstchain import (
     test_calib_path,
     test_time_calib_path,
     test_r0_path,
-    test_r0_path2
+    test_r0_path2,
 )
+
 
 def find_entry_points(package_name):
     """from: https://stackoverflow.com/a/47383763/3838691"""
@@ -29,7 +30,7 @@ def find_entry_points(package_name):
         ep.name
         for ep in pkg_resources.iter_entry_points("console_scripts")
         if ep.module_name.startswith(package_name)
-    	]
+    ]
     return entrypoints
 
 
@@ -56,20 +57,22 @@ def test_all_help(script):
 def simulated_dl1ab(temp_dir_simulated_files, simulated_dl1_file):
     """Produce a new simulated dl1 file using the dl1ab script."""
     output_file = temp_dir_simulated_files / "dl1ab.h5"
-    run_program(
-        "lstchain_dl1ab",
-        "-f",
-        simulated_dl1_file,
-        "-o",
-        output_file
-    )
+    run_program("lstchain_dl1ab", "-f", simulated_dl1_file, "-o", output_file)
     return output_file
 
+
 def test_add_source_dependent_parameters(simulated_dl1_file):
-    run_program('lstchain_add_source_dependent_parameters', '-f', simulated_dl1_file)
-    dl1_params_src_dep = pd.read_hdf(simulated_dl1_file, key=dl1_params_src_dep_lstcam_key)
-    dl1_params_src_dep.columns = pd.MultiIndex.from_tuples([tuple(col[1:-1].replace('\'', '').replace(' ', '').split(",")) for col in dl1_params_src_dep.columns])
-    assert 'alpha' in dl1_params_src_dep['on'].columns
+    run_program("lstchain_add_source_dependent_parameters", "-f", simulated_dl1_file)
+    dl1_params_src_dep = pd.read_hdf(
+        simulated_dl1_file, key=dl1_params_src_dep_lstcam_key
+    )
+    dl1_params_src_dep.columns = pd.MultiIndex.from_tuples(
+        [
+            tuple(col[1:-1].replace("'", "").replace(" ", "").split(","))
+            for col in dl1_params_src_dep.columns
+        ]
+    )
+    assert "alpha" in dl1_params_src_dep["on"].columns
 
 
 @pytest.fixture(scope="session")
@@ -79,9 +82,12 @@ def merged_simulated_dl1_file(simulated_dl1_file, temp_dir_simulated_files):
     merged_dl1_file = temp_dir_simulated_files / "script_merged_dl1.h5"
     run_program(
         "lstchain_merge_hdf5_files",
-        "-d", temp_dir_simulated_files,
-        "-o", merged_dl1_file,
-        "--no-image", "True"
+        "-d",
+        temp_dir_simulated_files,
+        "-o",
+        merged_dl1_file,
+        "--no-image",
+        "True",
     )
     return merged_dl1_file
 
@@ -100,12 +106,16 @@ def observed_dl1_files(temp_dir_observed_files):
     #  muons and datacheck files should be coherent
 
     # First set of files to be produced
-    dl1_output_path1 = temp_dir_observed_files / ("dl1_" + test_r0_path.with_suffix('').stem + ".h5")
+    dl1_output_path1 = temp_dir_observed_files / (
+        "dl1_" + test_r0_path.with_suffix("").stem + ".h5"
+    )
     muons_file1 = temp_dir_observed_files / "muons_LST-1.Run02008.0000_first50.fits"
     datacheck_file1 = temp_dir_observed_files / "datacheck_dl1_LST-1.Run02008.0000.h5"
 
     # Second set of files
-    dl1_output_path2 = temp_dir_observed_files / ("dl1_" + test_r0_path2.with_suffix('').stem + ".h5")
+    dl1_output_path2 = temp_dir_observed_files / (
+        "dl1_" + test_r0_path2.with_suffix("").stem + ".h5"
+    )
     muons_file2 = temp_dir_observed_files / "muons_LST-1.Run02008.0100_first50.fits"
     datacheck_file2 = temp_dir_observed_files / "datacheck_dl1_LST-1.Run02008.0100.h5"
 
@@ -130,7 +140,7 @@ def observed_dl1_files(temp_dir_observed_files):
         "--ucts-t0-tib",
         "1582059789516351903",
         "--tib-counter0",
-        "2516351200"
+        "2516351200",
     )
 
     run_program(
@@ -146,16 +156,16 @@ def observed_dl1_files(temp_dir_observed_files):
         "--time-calibration-file",
         test_time_calib_path,
         "--pointing-file",
-        test_drive_report
+        test_drive_report,
     )
 
     return {
-        'dl1_file1': dl1_output_path1,
-        'muons1': muons_file1,
-        'datacheck1': datacheck_file1,
-        'dl1_file2': dl1_output_path2,
-        'muons2': muons_file2,
-        'datacheck2': datacheck_file2
+        "dl1_file1": dl1_output_path1,
+        "muons1": muons_file1,
+        "datacheck1": datacheck_file1,
+        "dl1_file2": dl1_output_path2,
+        "muons2": muons_file2,
+        "datacheck2": datacheck_file2,
     }
 
 
@@ -183,10 +193,10 @@ def test_observed_dl1_validity(observed_dl1_files):
 
     dl1_tables = get_dataset_keys(observed_dl1_files["dl1_file1"])
 
-    assert 'dl1/event/telescope/monitoring/calibration' in dl1_tables
-    assert 'dl1/event/telescope/monitoring/flatfield' in dl1_tables
-    assert 'dl1/event/telescope/monitoring/pedestal' in dl1_tables
-    assert 'dl1/event/telescope/image/LST_LSTCam' in dl1_tables
+    assert "dl1/event/telescope/monitoring/calibration" in dl1_tables
+    assert "dl1/event/telescope/monitoring/flatfield" in dl1_tables
+    assert "dl1/event/telescope/monitoring/pedestal" in dl1_tables
+    assert "dl1/event/telescope/image/LST_LSTCam" in dl1_tables
 
     assert "alt_tel" in dl1_df.columns
     assert "az_tel" in dl1_df.columns
@@ -197,9 +207,12 @@ def test_observed_dl1_validity(observed_dl1_files):
     assert "tib_time" in dl1_df.columns
     assert "ucts_time" in dl1_df.columns
     assert np.isclose(
-        (Time(first_event_timestamp, format='unix') -
-         Time(first_timestamp_nightsummary / 1e9, format='unix_tai')
-         ).to_value(u.s), 0)
+        (
+            Time(first_event_timestamp, format="unix")
+            - Time(first_timestamp_nightsummary / 1e9, format="unix_tai")
+        ).to_value(u.s),
+        0,
+    )
     np.testing.assert_allclose(dl1_df["dragon_time"], dl1_df["trigger_time"])
 
 
@@ -241,24 +254,36 @@ def test_lstchain_merge_dl1_hdf5_files(merged_simulated_dl1_file):
 
 
 @pytest.mark.private_data
-def test_lstchain_merge_dl1_hdf5_observed_files(temp_dir_observed_files, observed_dl1_files):
+def test_lstchain_merge_dl1_hdf5_observed_files(
+    temp_dir_observed_files, observed_dl1_files
+):
     merged_dl1_observed_file = temp_dir_observed_files / "dl1_LST-1.Run02008_merged.h5"
     run_program(
         "lstchain_merge_hdf5_files",
-        "-d", temp_dir_observed_files,
-        "-o", merged_dl1_observed_file,
-        "--no-image", "False",
-        "--smart", "False",
-        "--run-number", "2008",
-        "--pattern", "dl1_*.h5"
+        "-d",
+        temp_dir_observed_files,
+        "-o",
+        merged_dl1_observed_file,
+        "--no-image",
+        "False",
+        "--smart",
+        "False",
+        "--run-number",
+        "2008",
+        "--pattern",
+        "dl1_*.h5",
     )
     dl1a_df = pd.read_hdf(observed_dl1_files["dl1_file1"], key=dl1_params_lstcam_key)
     dl1b_df = pd.read_hdf(observed_dl1_files["dl1_file1"], key=dl1_params_lstcam_key)
     merged_dl1_df = pd.read_hdf(merged_dl1_observed_file, key=dl1_params_lstcam_key)
     assert merged_dl1_observed_file.is_file()
     assert len(dl1a_df) + len(dl1b_df) == len(merged_dl1_df)
-    assert 'dl1/event/telescope/image/LST_LSTCam' in get_dataset_keys(merged_dl1_observed_file)
-    assert 'dl1/event/telescope/parameters/LST_LSTCam' in get_dataset_keys(merged_dl1_observed_file)
+    assert "dl1/event/telescope/image/LST_LSTCam" in get_dataset_keys(
+        merged_dl1_observed_file
+    )
+    assert "dl1/event/telescope/parameters/LST_LSTCam" in get_dataset_keys(
+        merged_dl1_observed_file
+    )
 
 
 @pytest.mark.private_data
@@ -266,15 +291,20 @@ def test_merge_datacheck_files(temp_dir_observed_files):
     run_program(
         "lstchain_check_dl1",
         "--batch",
-        "--input-file", temp_dir_observed_files / "datacheck_dl1_LST-1.Run02008.*.h5",
-        "--output-dir", temp_dir_observed_files
+        "--input-file",
+        temp_dir_observed_files / "datacheck_dl1_LST-1.Run02008.*.h5",
+        "--output-dir",
+        temp_dir_observed_files,
     )
     assert (temp_dir_observed_files / "datacheck_dl1_LST-1.Run02008.h5").is_file()
     assert (temp_dir_observed_files / "datacheck_dl1_LST-1.Run02008.pdf").is_file()
 
-def test_lstchain_merged_dl1_to_dl2(temp_dir_simulated_files, merged_simulated_dl1_file, rf_models):
+
+def test_lstchain_merged_dl1_to_dl2(
+    temp_dir_simulated_files, merged_simulated_dl1_file, rf_models
+):
     output_file = merged_simulated_dl1_file.with_name(
-        merged_simulated_dl1_file.name.replace('dl1', 'dl2')
+        merged_simulated_dl1_file.name.replace("dl1", "dl2")
     )
     run_program(
         "lstchain_dl1_to_dl2",
@@ -283,7 +313,7 @@ def test_lstchain_merged_dl1_to_dl2(temp_dir_simulated_files, merged_simulated_d
         "-p",
         rf_models["path"],
         "--output-dir",
-        temp_dir_simulated_files
+        temp_dir_simulated_files,
     )
     assert output_file.is_file()
 
@@ -301,9 +331,12 @@ def test_lstchain_dl1_to_dl2(simulated_dl2_file):
 
 
 @pytest.mark.private_data
-def test_lstchain_observed_dl1_to_dl2(temp_dir_observed_files, observed_dl1_files,  rf_models):
+def test_lstchain_observed_dl1_to_dl2(
+    temp_dir_observed_files, observed_dl1_files, rf_models
+):
     real_data_dl2_file = temp_dir_observed_files / (
-        "dl2_" + test_r0_path.with_suffix('').stem + ".h5")
+        "dl2_" + test_r0_path.with_suffix("").stem + ".h5"
+    )
     run_program(
         "lstchain_dl1_to_dl2",
         "--input-file",
@@ -311,7 +344,7 @@ def test_lstchain_observed_dl1_to_dl2(temp_dir_observed_files, observed_dl1_file
         "--path-models",
         rf_models["path"],
         "--output-dir",
-        temp_dir_observed_files
+        temp_dir_observed_files,
     )
     assert real_data_dl2_file.is_file()
     dl2_df = pd.read_hdf(real_data_dl2_file, key=dl2_params_lstcam_key)
@@ -333,7 +366,9 @@ def test_dl1ab(simulated_dl1ab):
 @pytest.mark.private_data
 def test_observed_dl1ab(tmp_path, observed_dl1_files):
     output_dl1ab = tmp_path / "dl1ab.h5"
-    run_program("lstchain_dl1ab", "-f", observed_dl1_files["dl1_file1"], "-o", output_dl1ab)
+    run_program(
+        "lstchain_dl1ab", "-f", observed_dl1_files["dl1_file1"], "-o", output_dl1ab
+    )
     assert output_dl1ab.is_file()
     dl1ab = pd.read_hdf(output_dl1ab, key=dl1_params_lstcam_key)
     dl1 = pd.read_hdf(observed_dl1_files["dl1_file1"], key=dl1_params_lstcam_key)
@@ -358,24 +393,25 @@ def test_mc_r0_to_dl2(tmp_path, rf_models, mc_gamma_testfile):
         "--store-dl1",
         "False",
         "--output-dir",
-        tmp_path
+        tmp_path,
     )
     assert dl2_file.is_file()
 
 
 def test_read_mc_dl2_to_pyirf(simulated_dl2_file):
-	from lstchain.io.io import read_mc_dl2_to_pyirf
-	import astropy.units as u
+    from lstchain.io.io import read_mc_dl2_to_pyirf
+    import astropy.units as u
 
-	events, sim_info = read_mc_dl2_to_pyirf(simulated_dl2_file)
-	assert 'true_energy' in events.colnames
-	assert sim_info.energy_max == 330 * u.TeV
+    events, sim_info = read_mc_dl2_to_pyirf(simulated_dl2_file)
+    assert "true_energy" in events.colnames
+    assert sim_info.energy_max == 330 * u.TeV
 
 
 def test_read_data_dl2_to_QTable(temp_dir_observed_files):
     from lstchain.io.io import read_data_dl2_to_QTable
 
     real_data_dl2_file = temp_dir_observed_files / (
-        "dl2_" + test_r0_path.with_suffix('').stem + ".h5")
+        "dl2_" + test_r0_path.with_suffix("").stem + ".h5"
+    )
     events = read_data_dl2_to_QTable(real_data_dl2_file)
-    assert 'gh_score' in events.colnames
+    assert "gh_score" in events.colnames
