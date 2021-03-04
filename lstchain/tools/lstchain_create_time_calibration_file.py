3833f4e35a28a4a2aa00e35521d3987b9a197545
Jose Enrique Ruiz <jer@iaa.es>
2021-03-04 20:03:32 +0100

    adapt tool to last modifs done in data_create_time_calibration_file script

--- lstchain/tools/lstchain_create_time_calibration_file.py
+++ lstchain/tools/lstchain_create_time_calibration_file.py
@@ -2,17 +2,17 @@
 Create drs4 time correction coefficients.
 """
 import glob
+from traitlets.config.loader import Config
 
-import numpy as np
 from ctapipe.core import Provenance, Tool, traits
-from ctapipe_io_lst import LSTEventSource
+from ctapipe.io import EventSource
+from ctapipe_io_lst.event_time import EventTimeCalculator
 from ctapipe_io_lst.calibration import LSTR0Corrections
 from lstchain.calib.camera.time_correction_calculate import TimeCorrectionCalculate
 from tqdm.autonotebook import tqdm
 
 
 class TimeCalibrationHDF5Writer(Tool):
-
     name = "TimeCalibrationHDF5Writer"
     description = "Generate a HDF5 file with time calibration coefficients"
 
@@ -25,10 +25,6 @@ class TimeCalibrationHDF5Writer(Tool):
         default_value="*"
     ).tag(config=True)
 
-    max_events = traits.Int(
-        help="Maximum numbers of events to read. Default = 20000", default_value=20000
-    ).tag(config=True)
-
     progress_bar = traits.Bool(
         help="show progress bar during processing",
         default_value=True,
@@ -38,13 +34,16 @@ class TimeCalibrationHDF5Writer(Tool):
         ("i", "input"): "TimeCalibrationHDF5Writer.input",
         ("o", "output"): "TimeCorrectionCalculate.calib_file_path",
         "glob": "TimeCalibrationHDF5Writer.glob",
+        "max-events": "EventSource.max_events",
         "pedestal": "LSTR0Corrections.drs4_pedestal_path",
-        "max_events": "TimeCalibrationHDF5Writer.max_events",
+        # "dragon-reference-time": "EventTimeCalculator.ucts_t0_dragon",
+        # "dragon-reference-counter": "EventTimeCalculator.dragon_counter0",
     }
 
-    classes = [LSTEventSource, LSTR0Corrections, TimeCorrectionCalculate]
+    classes = [EventSource, LSTR0Corrections, TimeCorrectionCalculate, EventTimeCalculator]
 
     def __init__(self, **kwargs):
+
         super().__init__(**kwargs)
         """
         Tool that generates a HDF5 file with time calibration coefficients.
@@ -53,9 +52,8 @@ class TimeCalibrationHDF5Writer(Tool):
         lstchain_create_time_calibration_file --help
         """
         self.eventsource = None
-        self.timeCorr = None
         self.path_list = None
-        self.lst_r0 = None
+        self.timeCorr = None
 
     def setup(self):
 
@@ -63,27 +61,18 @@ class TimeCalibrationHDF5Writer(Tool):
         if self.input.is_dir():
             self.path_list = sorted(glob.glob(str(self.input/self.glob)))
 
-        self.eventsource = self.add_component(
-            LSTEventSource(
-                input_url=self.path_list[0],
-                max_events=self.max_events,
-                parent=self,
-            )
-        )
-        self.lst_r0 = self.add_component(
-            LSTR0Corrections(parent=self)
-        )
-        self.timeCorr = TimeCorrectionCalculate(
-            subarray=self.eventsource.subarray,
-            config=self.config,
-        )
-
     def start(self):
 
         for j, path in enumerate(self.path_list):
-            self.eventsource.input_url = path
+            self.eventsource = EventSource(input_url=path, config=self.config)
             self.log.info(f"File {j + 1} out of {len(self.path_list)}")
             self.log.info(f"Processing: {path}")
+
+            self.timeCorr = TimeCorrectionCalculate(
+                subarray=self.eventsource.subarray,
+                config=self.config,
+            )
+
             for event in tqdm(
                     self.eventsource,
                     desc=self.eventsource.__class__.__name__,
@@ -91,12 +80,7 @@ class TimeCalibrationHDF5Writer(Tool):
                     unit="ev",
                     disable=not self.progress_bar,
             ):
-                self.lst_r0.calibrate(event)
-                # cut in signal to avoid cosmic events
-                if event.r1.tel[self.timeCorr.tel_id].trigger_type == 4 or (
-                    np.median(np.sum(event.r1.tel[self.timeCorr.tel_id].waveform[0], axis=1)) > 300
-                ):
-                    self.timeCorr.calibrate_peak_time(event)
+                self.timeCorr.calibrate_peak_time(event)
 
     def finish(self):
 
