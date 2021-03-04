c9fe23f32d55df368936d428b3ef46401bc34ba5
Daniel Morcuende <dmorcuen@ucm.es>
2021-03-04 17:36:54 +0000

    use astropy Table, get subrun number differently

--- lstchain/scripts/lstchain_create_run_summary.py
+++ lstchain/scripts/lstchain_create_run_summary.py
@@ -10,9 +10,10 @@ from pathlib import Path
 
 import numpy as np
 import protozfits
+from astropy.table import Table
+from astropy.time import Time
 from ctapipe.containers import EventType
 from ctapipe.io import EventSource
-from ctapipe_io_lst.event_time import CENTRAL_MODULE
 from lstchain.paths import parse_r0_filename
 
 # FIXME: take it from somewhere else
@@ -66,26 +67,19 @@ def get_list_of_runs(list_of_files):
     return sorted(parse_r0_filename(file) for file in list_of_files)
 
 
-def get_run_numbers(list_of_runs):
-    """Get the list of run numbers of a given date."""
-    return np.unique([run_info.run for run_info in list_of_runs])
-
-
-def get_number_of_subruns(run_number, list_of_run_objects):
-    """Obtain the number of sequential files (subruns) of a given run."""
-    filtered_run = filter(lambda x: x.run == run_number, list_of_run_objects)
-    last_subrun = max(list(filtered_run), key=lambda x: x.subrun)
-    # Since subruns are counted from 0, the number of files is increased by 1.
-    return last_subrun.subrun + 1
-
-
-def start_of_run_datetime(run_number):
+def get_runs_and_subruns(list_of_run_objects):
     """
-    Get datetime of the start of the run in ISOT format and UTC scale.
+    Get the list of run numbers and the number of sequenctial files (subruns)
+    of each run for a given date assuming 4 ZFW streams.
     """
-    filename = args.R0_PATH / args.date / f"LST-1.1.Run{run_number:05d}.0000.fits.fz"
-    file = protozfits.File(str(filename))
-    return file.CameraConfig.header["DATE"]
+    NUMBER_OF_STREAMS = 4
+
+    run, number_of_files = np.unique(
+        list(map(lambda x: x.run, list_of_run_objects)), return_counts=True
+    )
+
+    n_subruns = number_of_files / NUMBER_OF_STREAMS
+    return run, n_subruns.astype(int)
 
 
 def type_of_run(run_number, n_events=500):
@@ -100,19 +94,17 @@ def type_of_run(run_number, n_events=500):
     filename = args.R0_PATH / args.date / f"LST-1.1.Run{run_number:05d}.0000.fits.fz"
 
     with EventSource(input_url=filename, max_events=n_events) as source:
-        n_pedestal = sum(
+        n_pedestal_events = sum(
             1 for event in source if event.trigger.event_type == EventType.SKY_PEDESTAL
         )
-        n_sky = sum(
-            1 for event in source if event.trigger.event_type == EventType.SUBARRAY
-        )
+        n_sky_events = sum(1 for event in source if event.trigger.event_type == EventType.SUBARRAY)
 
     # FIXME: Do this classification in some other way?
-    if n_sky / n_events > 0.999:
+    if n_sky_events / n_events > 0.999:
         run_type = "DRS4"
-    elif n_pedestal / n_events > 0.1:
+    elif n_pedestal_events / n_events > 0.1:
         run_type = "CALI"
-    elif n_pedestal / n_events < 0.1:
+    elif n_pedestal_events / n_events < 0.1:
         run_type = "DATA"
     else:
         run_type = "UNKW"
@@ -120,10 +112,11 @@ def type_of_run(run_number, n_events=500):
     return run_type
 
 
-def get_initial_timestamps(run_number):
+def initial_timestamps(run_number):
     """
-    Get initial valid timestamps. Write down the Dragon module used.
-    First subrun needs to be open. Taken from ctapipe_io_lst.event_time
+    Get initial valid timestamps from the first subrun.
+    Write down the Dragon module used and reference event_id.
+    Taken from ctapipe_io_lst.event_time.
     """
     filename = args.R0_PATH / args.date / f"LST-1.1.Run{run_number:05d}.0000.fits.fz"
 
@@ -131,8 +124,10 @@ def get_initial_timestamps(run_number):
 
         for event in source:
             lst = event.lst.tel[tel_id]
-            central_module_index = np.where(lst.svc.module_ids == CENTRAL_MODULE)[0][0]
-            module_index = central_module_index
+            # Start fo the run timestamps (nanoseconds) in UNIX TAI
+            run_date = int(round(Time(lst.svc.date, format="unix").unix_tai)) * int(1e9)
+            # Get the first available module
+            module_index = np.where(lst.evt.module_status != 0)[0][0]
             ucts_available = lst.evt.extdevices_presence & 2
 
             # Look for the first ucts timestamp available
@@ -143,74 +138,55 @@ def get_initial_timestamps(run_number):
                     int(1e9) * lst.evt.pps_counter[module_index]
                     + 100 * lst.evt.tenMHz_counter[module_index]
                 )
-
-                return (
-                    dragon_reference_source,
-                    dragon_reference_timestamp,
-                    initial_dragon_counter,
-                    module_index,
-                )
+                break
 
             else:
                 # No UCTS available in the first subrun
                 dragon_reference_source = "run_date"
-                dragon_reference_timestamp = lst.svc.date
+                dragon_reference_timestamp = -1
                 initial_dragon_counter = (
                     int(1e9) * lst.evt.pps_counter[module_index]
                     + 100 * lst.evt.tenMHz_counter[module_index]
                 )
+                break
 
-                return (
-                    dragon_reference_source,
-                    dragon_reference_timestamp,
-                    initial_dragon_counter,
-                    module_index,
-                )
+    return {
+        "run": run_number,
+        "start_of_the_run": run_date,
+        "dragon_reference_source": dragon_reference_source,
+        "event_id": event.index.event_id,
+        "dragon_reference_timestamp": dragon_reference_timestamp,
+        "initial_dragon_counter": initial_dragon_counter,
+        "module_id": module_index,
+    }
 
 
-def write_run_summary_to_file(date, run_numbers, list_of_run_objects):
+def main(date):
     """
-    Write to a .txt file the following information per run:
+    Write run summary to a file the following information per run:
     Run number
-    Number of subruns starting from 0
+    Number of subruns
     Type of run
     Start_of_the_run
+    Event ID used to take time reference
     Reference_source ("ucts" or "run_date")
     Reference_timestamp
     Initial dragon counter
     Dragon module ID used to take the counter values
     """
     # TODO: Be able to create file incrementally run-by-run
-    with open(args.output_dir / f"RunSummary_{date}.txt", "a+") as summary_file:
-        for run in run_numbers:
-            n_subruns = get_number_of_subruns(run, list_of_run_objects)
 
-            (
-                dragon_reference_source,
-                dragon_reference_timestamp,
-                initial_dragon_counter,
-                module_index,
-            ) = get_initial_timestamps(run)
-
-            run_information = (
-                f"{run:05d},"
-                f"{n_subruns},"
-                f"{type_of_run(run)},"
-                f"{start_of_run_datetime(run)},"
-                f"{dragon_reference_source},"
-                f"{dragon_reference_timestamp},"
-                f"{initial_dragon_counter},"
-                f"{module_index}\n"
-            )
-            summary_file.write(run_information)
-
-
-def main():
-    list_of_files = get_list_of_files(args.date)
+    list_of_files = get_list_of_files(date)
     list_of_run_objects = get_list_of_runs(list_of_files)
-    run_numbers = get_run_numbers(list_of_run_objects)
-    write_run_summary_to_file(args.date, run_numbers, list_of_run_objects)
+    run_numbers, n_subruns = get_runs_and_subruns(list_of_run_objects)
+    list_type_of_runs = [type_of_run(run) for run in run_numbers]
+    dict_run_timestamps = [initial_timestamps(run) for run in run_numbers]
+
+    run_summary = Table(dict_run_timestamps)
+    run_summary.add_column(n_subruns, name="n_subruns", index=1)
+    run_summary.add_column(list_type_of_runs, name="type_of_run", index=2)
+    run_summary.write(args.output_dir / f"RunSummary_{date}.txt", format="ascii.csv")
 
 
 if __name__ == "__main__":
-    main()
+    main(args.date)
