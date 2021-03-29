3e7daef4e6361c03c3320630825939321ef2140c
vuillaut <thomas.vuillaume@gmail.com>
2021-03-29 15:18:06 +0200

    and tel_id option for src_dependent script as well

--- lstchain/scripts/lstchain_add_source_dependent_parameters.py
+++ lstchain/scripts/lstchain_add_source_dependent_parameters.py
@@ -41,6 +41,11 @@ parser.add_argument('--config', '-c', action='store', type=str,
                     default=None
                     )
 
+parser.add_argument('--tel-id', '-tid', action='store', type=str,
+                    dest='tel_id',
+                    help='Telescope id - used to read Telescope or Optics info such as the focal length',
+                    default=1)
+
 args = parser.parse_args()
 
 def main():
@@ -64,4 +69,4 @@ def main():
 
 if __name__ == '__main__':
     main()
- 
+
