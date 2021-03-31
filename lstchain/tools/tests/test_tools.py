9d2492151657bb536250cc270a5eada3bcb7e50b
chaimain <cpriyadarshi@ifae.es>
2021-03-31 17:25:42 +0200

    Corrected the example configs to be used for IRF and DL3 Tools

--- lstchain/tools/tests/test_tools.py
+++ lstchain/tools/tests/test_tools.py
@@ -1,5 +1,6 @@
 import pytest
 from ctapipe.core import run_tool
+import os
 
 
 def test_create_irf(temp_dir_observed_files, simulated_dl2_file):
@@ -9,6 +10,7 @@ def test_create_irf(temp_dir_observed_files, simulated_dl2_file):
     from lstchain.tools.lstchain_create_irf_files import IRFFITSWriter
 
     irf_file = temp_dir_observed_files / "irf.fits.gz"
+    config_file = os.path.join(os.getcwd(), "./docs/examples/irf_tool_config.json")
 
     assert (
         run_tool(
@@ -18,6 +20,22 @@ def test_create_irf(temp_dir_observed_files, simulated_dl2_file):
                 f"--input-proton-dl2={simulated_dl2_file}",
                 f"--input-electron-dl2={simulated_dl2_file}",
                 f"--output-irf-file={irf_file}",
+                "--overwrite",
+            ],
+            cwd=temp_dir_observed_files,
+        )
+        == 0
+    )
+
+    assert (
+        run_tool(
+            IRFFITSWriter(),
+            argv=[
+                f"--input-gamma-dl2={simulated_dl2_file}",
+                f"--input-proton-dl2={simulated_dl2_file}",
+                f"--input-electron-dl2={simulated_dl2_file}",
+                f"--output-irf-file={irf_file}",
+                f"--config={config_file}",
                 "--overwrite",
             ],
             cwd=temp_dir_observed_files,
@@ -35,6 +53,7 @@ def test_create_dl3(temp_dir_observed_files, observed_dl2_file):
     from lstchain.tools.lstchain_create_dl3_file import DataReductionFITSWriter
 
     irf_file = temp_dir_observed_files / "irf.fits.gz"
+    config_file = os.path.join(os.getcwd(), "docs/examples/dl3_tool_config.json")
 
     assert (
         run_tool(
@@ -46,6 +65,24 @@ def test_create_dl3(temp_dir_observed_files, observed_dl2_file):
                 "--source-name=Crab",
                 "--source-ra=83.633deg",
                 "--source-dec=22.01deg",
+                "--overwrite",
+            ],
+            cwd=temp_dir_observed_files,
+        )
+        == 0
+    )
+
+    assert (
+        run_tool(
+            DataReductionFITSWriter(),
+            argv=[
+                f"--input-dl2={observed_dl2_file}",
+                f"--output-dl3-path={temp_dir_observed_files}",
+                f"--input-irf={irf_file}",
+                "--source-name=Crab",
+                "--source-ra=83.633deg",
+                "--source-dec=22.01deg",
+                f"--config={config_file}",
                 "--overwrite",
             ],
             cwd=temp_dir_observed_files,
