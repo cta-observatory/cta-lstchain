ed75d2877defb1355c33b9a3e56ce017a99fd358
SeiyaNozaki <nozaki.seiya.1211@gmail.com>
2021-04-12 07:54:27 +0000

    add a tool to create sampling interval coefficients

---
+++ lstchain/calib/camera/sampling_interval_coefficient_calculate.py
@@ -0,0 +1,170 @@
+import numpy as np
+from astropy.io import fits
+from numba import jit, prange
+
+from ctapipe.core import Component
+
+
+__all__ = ['SamplingIntervalCalculate']
+
+N_PIXELS = 1855
+N_CAPACITORS_CHANNEL = 1024
+N_SAMPLES = 40
+
+class SamplingIntervalCalculate(Component):
+    """
+        The SamplingIntervalCalculate class to create a sampling interval coefficient table for LST readout system using chip DRS4.
+    """
+    def __init__(self):
+        self.peak_count = np.zeros([N_PIXELS, N_CAPACITORS_CHANNEL])
+        self.fc_count = np.zeros([N_PIXELS, N_CAPACITORS_CHANNEL])
+
+        self.peak_count_stuck ={}
+        self.fc_count_stuck = {}
+
+        self.sampling_interval_coefficient = {}
+        self.charge_array_after_corr ={}
+        self.charge_reso_array_after_corr ={}
+
+        self.charge_reso_final = np.zeros(N_PIXELS)
+        self.used_run = np.zeros(N_PIXELS)
+        self.sampling_interval_coefficient_final = np.zeros([N_PIXELS, N_CAPACITORS_CHANNEL])
+
+    def increment_peak_count(self, event, tel_id, gain, r0_r1_calibrator):
+
+        waveform = event.r1.tel[tel_id].waveform
+        
+        first_capacitors = r0_r1_calibrator.first_cap
+        r1_sample_start = r0_r1_calibrator.r1_sample_start.tel[tel_id]
+
+        # Check pulse
+        pulse_event_flag = np.sum(np.max(waveform[gain], axis=1) > 100) > 1800
+    
+        if pulse_event_flag:
+
+            # find pulse peak position
+            pulse_peak = np.argmax(waveform[gain], axis=1)
+            fc = first_capacitors[tel_id][gain]
+            pulse_peak_abs_pos = (fc + r1_sample_start + pulse_peak) % N_CAPACITORS_CHANNEL
+            
+            # increment peak count array
+            self.peak_count[np.arange(N_PIXELS), pulse_peak_abs_pos] += 1
+            self.fc_count[np.arange(N_PIXELS), fc % N_CAPACITORS_CHANNEL] += 1
+            
+
+    def stuck_single_sampling_interval(self, file_list, gain):
+
+        for single_file in file_list:
+
+            run_id, gain_in_file, peak_count, fc_count = load_single_fits_sampling_interval(single_file)
+            
+            if gain_in_file == gain:
+                if not run_id in self.peak_count_stuck.keys():
+                    self.peak_count_stuck[run_id] = np.zeros([N_PIXELS, N_CAPACITORS_CHANNEL])
+                    self.fc_count_stuck[run_id] = np.zeros([N_PIXELS, N_CAPACITORS_CHANNEL])
+                
+                self.peak_count_stuck[run_id] += peak_count
+                self.fc_count_stuck[run_id] += fc_count
+
+
+    def convert_to_samp_interval_coefficient(self, gain):
+        # convert peak counts to sampling interval coefficient
+
+        for run_id in self.peak_count_stuck.keys():
+            self.sampling_interval_coefficient[run_id] = np.zeros([N_PIXELS, N_CAPACITORS_CHANNEL + N_SAMPLES])
+
+            for pixel in range(N_PIXELS):
+                self.sampling_interval_coefficient[run_id][pixel, :N_CAPACITORS_CHANNEL] = \
+                    self.peak_count_stuck[run_id][pixel] / np.sum(self.peak_count_stuck[run_id][pixel]) * N_CAPACITORS_CHANNEL
+                
+                self.sampling_interval_coefficient[run_id][pixel, N_CAPACITORS_CHANNEL:] = \
+                    self.sampling_interval_coefficient[run_id][pixel, :N_SAMPLES]
+
+                
+    def set_charge_array(self, gain):
+        self.charge_array_before_corr = np.zeros([N_PIXELS, 60000])
+        self.charge_reso_array_before_corr = np.zeros(N_PIXELS)
+
+        for run_id in self.peak_count_stuck.keys():
+            self.charge_array_after_corr[run_id] = np.zeros([N_PIXELS, 60000])
+            self.charge_reso_array_after_corr[run_id] = np.zeros(N_PIXELS)
+            
+
+    def calc_charge(self, count, event, tel_id, gain, r0_r1_calibrator):
+
+        waveform = event.r1.tel[tel_id].waveform
+        
+        first_capacitors = r0_r1_calibrator.first_cap
+        r1_sample_start = r0_r1_calibrator.r1_sample_start.tel[tel_id]
+        r1_sample_end = r0_r1_calibrator.r1_sample_end.tel[tel_id]
+
+        # Check pulse
+        pulse_event_flag = np.sum(np.max(waveform[gain], axis=1) > 100) > 1800
+
+        if pulse_event_flag:
+
+            # find pulse peak position
+            pulse_peak = np.argmax(waveform[gain], axis=1)
+            fc = first_capacitors[tel_id][gain]
+            pulse_peak_abs_pos = (fc + r1_sample_start + pulse_peak) % N_CAPACITORS_CHANNEL
+            integ_start = pulse_peak - 2
+            integ_last = integ_start + 5
+
+            integ_abs_start = (pulse_peak_abs_pos -2 ) % N_CAPACITORS_CHANNEL
+            integ_abs_last = integ_abs_start + 5
+
+            for pixel in range(N_PIXELS):
+
+                if integ_start[pixel] < 0 or integ_last[pixel] > (r1_sample_end - r1_sample_start):
+                    continue
+
+                self.charge_array_before_corr[pixel][count] = np.sum(waveform[gain, pixel,integ_start[pixel]:integ_last[pixel]])
+
+                for run_id in self.peak_count_stuck.keys():
+                    samp_interval_coefficient = self.sampling_interval_coefficient[run_id]
+                    
+                    self.charge_array_after_corr[run_id][pixel][count] = \
+                        np.sum(waveform[gain,pixel,integ_start[pixel]:integ_last[pixel]] \
+                               * samp_interval_coefficient[pixel, integ_abs_start[pixel]:integ_abs_last[pixel]])
+
+    def calc_charge_reso(self, gain):
+        
+        # Before correction
+        for pixel in range(N_PIXELS):
+            charge_array_before_corr_final = self.charge_array_before_corr[pixel]
+            charge_array_before_corr_final = charge_array_before_corr_final[charge_array_before_corr_final!=0]
+            self.charge_reso_array_before_corr[pixel] = np.std(charge_array_before_corr_final)/np.mean(charge_array_before_corr_final)
+
+            for run_id in self.peak_count_stuck.keys():
+                charge_array_after_corr_final = self.charge_array_after_corr[run_id][pixel]   
+                charge_array_after_corr_final = charge_array_after_corr_final[charge_array_after_corr_final!=0] 
+                self.charge_reso_array_after_corr[run_id][pixel] = np.std(charge_array_after_corr_final)/np.mean(charge_array_after_corr_final)
+
+    def verify(self):
+        n_keys=len(self.charge_reso_array_after_corr.keys())
+        run_id_array = np.zeros(n_keys)
+        charge_reso_array_after_corr_all = np.zeros([n_keys, 1855])
+        
+        for i, ikey in enumerate(self.charge_reso_array_after_corr.keys()):
+            run_id_array[i] = ikey
+            charge_reso_array_after_corr_all[i] = self.charge_reso_array_after_corr[ikey]
+    
+        for ipix in range(N_PIXELS):
+            min_charge_reso_arg = np.argmin(charge_reso_array_after_corr_all.T[ipix])
+            self.charge_reso_final[ipix] = charge_reso_array_after_corr_all[min_charge_reso_arg, ipix]
+            self.used_run[ipix] = int(run_id_array[min_charge_reso_arg])
+            self.sampling_interval_coefficient_final[ipix] = self.sampling_interval_coefficient[self.used_run[ipix]][ipix, :N_CAPACITORS_CHANNEL]
+
+
+
+def load_single_fits_sampling_interval(input_file):
+        
+    hdulist = fits.open(input_file)
+        
+    run_id = hdulist[0].header['run_id']
+    gain = hdulist[0].header['gain']
+    peak_count = hdulist[1].data
+    fc_count = hdulist[2].data
+    
+    return run_id, gain, peak_count, fc_count
+
---
+++ lstchain/tools/lstchain_create_sampling_interval_coefficient_file.py
@@ -0,0 +1,248 @@
+import numpy as np
+from astropy.io import fits
+import h5py
+
+from ctapipe.core import Provenance, traits
+from ctapipe.core import Tool
+from ctapipe_io_lst import LSTEventSource
+from ctapipe_io_lst.calibration import LSTR0Corrections
+from lstchain.calib.camera.sampling_interval_coefficient_calculate import SamplingIntervalCalculate
+from lstchain.paths import run_info_from_filename
+
+class SamplingIntervalCoefficientHDFWriter(Tool):
+    """
+    Tool that generates a sampling interval coefficient hdf file.
+    For getting help run:
+    lstchain_create_sampling_interval_coefficient_file --help
+    """
+
+    name = "SamplingIntervalCoefficientHDFWriter"
+    description = "Generate a sampling interval coefficient table"
+
+    input_fits = traits.Path(
+        help="Path to the generated peak count fits files",
+        directory_ok=True,
+    ).tag(config=True)
+
+    input_fits_hg = traits.Path(
+        help="Path to the generated fits sampling interval coefficient file for high gain",
+        directory_ok=False,
+    ).tag(config=True)
+
+    input_fits_lg = traits.Path(
+        help="Path to the generated fits sampling interval coefficient file for low gain",
+        directory_ok=False,
+    ).tag(config=True)
+
+    output = traits.Path(
+        help="Path to the output fits file",
+        directory_ok=False,
+    ).tag(config=True)
+
+    glob = traits.Unicode(
+        help="Filename pattern to glob files in the directory",
+        default_value="*.fits"
+    ).tag(config=True)
+
+    gain = traits.Int(
+        help="Select gain channel (0: High Gain, 1: Low Gain)",
+    ).tag(config=True)
+
+    count_flag = traits.Bool(
+        default_value=False,
+        help="First step: count peak on each capacitor"
+    ).tag(config=True)
+
+    stuck_verify_flag = traits.Bool(
+        default_value=False,
+        help="Second step: merge peak count files and verify with charge resolution"
+    ).tag(config=True)
+
+    merge_flag = traits.Bool(
+        default_value=False,
+        help="Third step: Merge sampling coefficient fits files for both gains."
+    ).tag(config=True)
+
+    progress_bar = traits.Bool(
+        help="show progress bar during processing",
+        default_value=True,
+    ).tag(config=True)
+
+    aliases = {
+        ("i", "input"): "EventSource.input_url",
+        ("o", "output"): "SamplingIntervalCoefficientHDFWriter.output",
+        ("g", "gain"): "SamplingIntervalCoefficientHDFWriter.gain",
+        "input_fits": "SamplingIntervalCoefficientHDFWriter.input_fits",
+        "input_fits_hg": "SamplingIntervalCoefficientHDFWriter.input_fits_hg",
+        "input_fits_lg": "SamplingIntervalCoefficientHDFWriter.input_fits_lg",
+        "pedestal": "LSTR0Corrections.drs4_pedestal_path",        
+        "max-events": "EventSource.max_events",
+        "r1_sample_start": "LSTR0Corrections.r1_sample_start",
+        "r1_sample_end": "LSTR0Corrections.r1_sample_end",
+    }
+
+    classes = [LSTEventSource, LSTR0Corrections, SamplingIntervalCalculate]
+
+    flags = {
+        'count_flag': (
+            {'SamplingIntervalCoefficientHDFWriter': {'count_flag': True}},
+            'First step: count peak on each capacitor',
+        ),
+        'stuck_verify_flag': (
+            {'SamplingIntervalCoefficientHDFWriter': {'stuck_verify_flag': True}},
+            'Second step: merge peak count files and verify with charge resolution',
+        ),
+        'merge_flag': (
+            {'SamplingIntervalCoefficientHDFWriter': {'merge_flag': True}},
+            'Third step: Merge sampling coefficient fits files for both gains',
+        )
+    }
+
+    def setup(self):
+        
+        self.sampling_interval_calculate = SamplingIntervalCalculate()
+
+        if self.count_flag:
+            self.setup_count()
+    
+        if self.stuck_verify_flag:
+            self.setup_stuck_verify()
+
+        if self.merge_flag:
+            self.setup_merge()
+
+
+    def setup_count(self):
+        self.log.debug('First step: count peak on each capacitor')
+        self.eventsource = LSTEventSource(parent = self)
+        self.run_id = self.eventsource.obs_ids[0]
+
+    def setup_stuck_verify(self):
+        self.log.debug('Second step: stuck and verify with charge resolution')
+        self.path_list = [str(self.input_fits)]
+        if self.input_fits.is_dir():
+            self.path_list = sorted(self.input_fits.glob(self.glob))
+        self.eventsource = LSTEventSource(parent=self)
+
+    def setup_merge(self):
+        self.log.debug('Third step: Merge sampling coefficients')
+
+
+
+    def start(self):
+        
+        if self.count_flag:
+            self.start_count()
+
+        if self.stuck_verify_flag:
+            self.start_stuck_verify()
+
+        if self.merge_flag:
+            self.start_merge()
+
+    def start_count(self):
+        self.log.debug('start peak counting')
+
+        for i, event in enumerate(self.eventsource):
+            if event.index.event_id % 500 == 0:
+                self.log.debug(f'event id = {event.index.event_id}')
+
+            # skip the first 1000 events which would not be calibrated correctly
+            if i < 1000:
+                continue
+
+            #first_capacitor = self.eventsource.r0_r1_calibrator.first_cap
+            self.sampling_interval_calculate.increment_peak_count(event, tel_id = self.eventsource.tel_id, \
+                                                                  gain = self.gain, r0_r1_calibrator = self.eventsource.r0_r1_calibrator)
+
+    def start_stuck_verify(self):
+        self.log.debug('stuck peak count tables')
+        self.sampling_interval_calculate.stuck_single_sampling_interval(self.path_list, self.gain)
+
+        self.log.debug('convert peak counts into sampling interval coefficients') 
+        self.sampling_interval_calculate.convert_to_samp_interval_coefficient(gain = self.gain)
+        self.sampling_interval_calculate.set_charge_array(gain = self.gain)
+
+        for i, event in enumerate(self.eventsource):
+            if event.index.event_id % 500 == 0:
+                self.log.debug(f'event id = {event.index.event_id}')
+
+            # skip the first 1000 events which would not be calibrated correctly
+            if i < 1000:
+                continue
+
+            #first_capacitor = self.eventsource.r0_r1_calibrator.first_cap
+            self.sampling_interval_calculate.calc_charge(i, event, tel_id = self.eventsource.tel_id, \
+                                                         gain = self.gain, r0_r1_calibrator = self.eventsource.r0_r1_calibrator)
+
+        self.log.debug('calculate charge resolution using the sampling coefficients')
+        self.sampling_interval_calculate.calc_charge_reso(gain = self.gain)
+        self.sampling_interval_calculate.verify()
+        
+
+    def start_merge(self):
+        # High Gain
+        hdulist = fits.open(self.input_fits_hg)
+        hdu = hdulist[0]
+        self.sampling_interval_coefficient_hg = hdulist[1].data
+        self.used_run_hg = hdulist[2].data
+        self.charge_reso_hg = hdulist[3].data
+
+        # Low Gain
+        hdulist = fits.open(self.input_fits_lg)
+        hdu = hdulist[0]
+        self.sampling_interval_coefficient_lg = hdulist[1].data
+        self.used_run_lg = hdulist[2].data
+        self.charge_reso_lg = hdulist[3].data
+        
+        self.sampling_interval_coefficient_merge = np.array([self.sampling_interval_coefficient_hg, self.sampling_interval_coefficient_lg])
+        self.used_run_merge = np.array([self.used_run_hg, self.used_run_lg])
+        self.charge_reso_merge = np.array([self.charge_reso_hg, self.charge_reso_lg])
+        
+
+    def finish(self):
+        
+        if self.count_flag:
+            self.finish_count()
+        
+        if self.stuck_verify_flag:
+            self.finish_stuck_verify()
+
+        if self.merge_flag:
+            self.finish_merge()
+            
+    def finish_count(self):
+        hdu_peak = fits.ImageHDU(self.sampling_interval_calculate.peak_count)
+        hdu_fc = fits.ImageHDU(self.sampling_interval_calculate.fc_count)
+        hdr = fits.Header()
+        hdr['run_id'] = self.run_id
+        hdr['gain'] = self.gain
+        primary_hdu = fits.PrimaryHDU(header=hdr)
+        hdul = fits.HDUList([primary_hdu, hdu_peak, hdu_fc])
+        hdul.writeto(self.output)
+
+    def finish_stuck_verify(self):
+        hdu_sampling_interval_coefficient = fits.ImageHDU(self.sampling_interval_calculate.sampling_interval_coefficient_final)
+        hdu_used_run = fits.ImageHDU(self.sampling_interval_calculate.used_run)
+        hdu_charge_reso_final = fits.ImageHDU(self.sampling_interval_calculate.charge_reso_final)
+
+        hdr = fits.Header()
+        hdr['gain'] = self.gain
+        primary_hdu = fits.PrimaryHDU(header=hdr)
+        hdul = fits.HDUList([primary_hdu, hdu_sampling_interval_coefficient, hdu_used_run, hdu_charge_reso_final])
+        hdul.writeto(self.output)
+
+    def finish_merge(self):
+
+        with h5py.File(self.output, 'w') as hf:
+            hf.create_dataset('sampling_interval_coefficient', data = self.sampling_interval_coefficient_merge)
+            hf.create_dataset('used_run', data = self.used_run_merge)
+            hf.create_dataset('charge_reso', data = self.charge_reso_merge)
+
+def main():
+    exe = SamplingIntervalCoefficientHDFWriter()
+    exe.run()
+
+
+if __name__ == "__main__":
+    main()
