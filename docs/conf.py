23b5bc91bbf0088824dc7cdfa507084f8583fa4f
Daniel Morcuende <dmorcuen@ucm.es>
2021-11-08 20:35:28 +0100

    missing import and small fixes

--- docs/conf.py
+++ docs/conf.py
@@ -19,13 +19,13 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
+import lstchain
 
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
- needs_sphinx = '1.5'
+needs_sphinx = '1.5'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -63,7 +63,7 @@ release = lstchain.__version__
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
