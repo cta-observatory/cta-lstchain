5129840bc7e890b9741f2c7b54b460e62a3a970c
Daniel Morcuende <dmorcuen@ucm.es>
2021-11-10 20:09:16 +0100

    bump sphinx version

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ setup_cfg = dict(conf.items("metadata"))
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '1.5'
+needs_sphinx = '4.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -92,7 +92,9 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -161,8 +163,3 @@ texinfo_documents = [
      author, 'lstchain', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-import sphinx_rtd_theme
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
