17682647dd8d4ab76ee1abd1d8c5cfbea809fa25
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2022-01-27 15:13:46 +0100

    Fix pedestal and flatfield calculator not taking extractor configuration, fixes #890

--- lstchain/calib/camera/flatfield.py
+++ lstchain/calib/camera/flatfield.py
@@ -7,6 +7,7 @@ from astropy import units as u
 from ctapipe.calib.camera.flatfield import FlatFieldCalculator
 from ctapipe.core.traits import  List, Path
 from lstchain.calib.camera.time_sampling_correction import TimeSamplingCorrection
+from ctapipe.image.extractor import ImageExtractor
 
 __all__ = [
     'FlasherFlatFieldCalculator'
@@ -85,6 +86,11 @@ class FlasherFlatFieldCalculator(FlatFieldCalculator):
             )
         else:
             self.time_sampling_corrector = None
+
+        # fix for broken extractor setup in ctapipe baseclass
+        self.extractor = ImageExtractor.from_name(
+            self.charge_product, parent=self, subarray=subarray
+        )
 
 
     def _extract_charge(self, event):
