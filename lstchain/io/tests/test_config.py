7ceebd7d98cea1156a9d482fcf509f5182ca4125
SeiyaNozaki <nozaki.seiya.1211@gmail.com>
2022-01-31 15:14:39 +0000

    add test_get_srcdep_config

--- lstchain/io/tests/test_config.py
+++ lstchain/io/tests/test_config.py
@@ -4,6 +4,9 @@ from lstchain.io.config import get_cleaning_parameters
 def test_get_standard_config():
     config.get_standard_config()
 
+def test_get_srcdep_config():
+    config.get_srcdep_config()
+
 def test_replace_config():
     a = dict(toto=1, tata=2)
     b = dict(tata=3, tutu=4)
