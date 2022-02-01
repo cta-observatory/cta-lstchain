7d24fcd1ffa5d4c960c34270921609a12bb344fb
Ruben Lopez Coto <rlopezcoto@gmail.com>
2022-02-01 09:43:39 +0100

    mv files

--- lstchain/high_level/__init__.py
+++ lstchain/high_level/__init__.py
@@ -1,3 +1,9 @@
+from .post_dl2 import (
+    analyze_on_off,
+    analyze_wobble,
+    setup_logging,
+)
+
 from .hdu_table import (
     add_icrs_position_params,
     create_event_list,
@@ -9,9 +15,12 @@ from .hdu_table import (
 
 __all__ = [
     "add_icrs_position_params",
+    'analyze_on_off',
+    'analyze_wobble',
     "create_event_list",
     "create_hdu_index_hdu",
     "create_obs_index_hdu",
     "get_pointing_params",
     "get_timing_params",
+    'setup_logging',
 ]
