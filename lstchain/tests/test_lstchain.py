413c78d01d2f70bccf87b79622dffbf2a115ef03
SeiyaNozaki <nozaki.seiya.1211@gmail.com>
2022-02-01 05:09:00 +0000

    add test_get_source_dependent_parameters_observed

--- lstchain/tests/test_lstchain.py
+++ lstchain/tests/test_lstchain.py
@@ -90,7 +90,7 @@ def test_content_dl1(simulated_dl1_file):
         assert 'obs_id' in params_table.colnames
 
 
-def test_get_source_dependent_parameters(simulated_dl1_file):
+def test_get_source_dependent_parameters_mc(simulated_dl1_file):
     from lstchain.reco.dl1_to_dl2 import get_source_dependent_parameters
 
     # for gamma MC
@@ -109,11 +109,46 @@ def test_get_source_dependent_parameters(simulated_dl1_file):
     assert (src_dep_df_gamma['on']['expected_src_y'] == dl1_params['src_y']).all()
 
     np.testing.assert_allclose(
-        src_dep_df_proton['on']['expected_src_x'], 0.195, rtol=1e-2
+        src_dep_df_proton['on']['expected_src_x'], 0.195, atol=1e-2
     )
     np.testing.assert_allclose(
         src_dep_df_proton['on']['expected_src_y'], 0., atol=1e-2
     )
+
+@pytest.mark.private_data
+def test_get_source_dependent_parameters_observed(observed_dl1_files):
+    from lstchain.reco.dl1_to_dl2 import get_source_dependent_parameters
+
+    # on observation data
+    srcdep_config['observation_mode']='on'
+    dl1_params = pd.read_hdf(observed_dl1_files["dl1_file1"], key=dl1_params_lstcam_key)
+    src_dep_df_on = get_source_dependent_parameters(dl1_params, srcdep_config)
+
+    # wobble observation data
+    srcdep_config['observation_mode']='wobble'
+    dl1_params['alt_tel'] += np.deg2rad(0.4) 
+    src_dep_df_wobble = get_source_dependent_parameters(dl1_params, srcdep_config)
+
+    assert 'alpha' in src_dep_df_on['on'].columns
+    assert 'dist' in src_dep_df_on['on'].columns
+    assert 'time_gradient_from_source' in src_dep_df_on['on'].columns
+    assert 'skewness_from_source' in src_dep_df_on['on'].columns
+    assert (src_dep_df_on['on']['expected_src_x'] == 0).all()
+    assert (src_dep_df_on['on']['expected_src_y'] == 0).all()
+
+    np.testing.assert_allclose(
+        src_dep_df_wobble['on']['expected_src_x'], -0.195, atol=1e-2
+    )
+    np.testing.assert_allclose(
+        src_dep_df_wobble['on']['expected_src_y'], 0., atol=1e-2
+    )
+    np.testing.assert_allclose(
+        src_dep_df_wobble['off_180']['expected_src_x'], 0.195, atol=1e-2
+    )
+    np.testing.assert_allclose(
+        src_dep_df_wobble['off_180']['expected_src_y'], 0., atol=1e-2
+    )
+
 
 def test_build_models(simulated_dl1_file, rf_models):
     from lstchain.reco.dl1_to_dl2 import build_models
