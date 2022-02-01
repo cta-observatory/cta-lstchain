c746bf320e48ce3acabef3f545af120e8b993c11
Ruben Lopez Coto <rlopezcoto@gmail.com>
2022-02-01 10:07:31 +0100

    forgotten commit

--- lstchain/data/__init__.py
+++ lstchain/data/__init__.py
@@ -1,14 +1,5 @@
-from .post_dl2 import (
-    analyze_on_off,
-    analyze_wobble,
-    setup_logging,
-)
-
 from .normalised_pulse_template import NormalizedPulseTemplate
 
 __all__ = [
     'NormalizedPulseTemplate',
-    'analyze_on_off',
-    'analyze_wobble',
-    'setup_logging',
 ]
