128df7ba92dd42ffa35ba35e6faee09045edffef
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2022-03-24 09:47:46 +0100

    Use flatfield heuristic for test dl1 files

--- lstchain/conftest.py
+++ lstchain/conftest.py
@@ -135,6 +135,7 @@ def observed_dl1_files(temp_dir_observed_files, run_summary_path):
         "2516351600",
         "--dragon-module-id",
         "132",
+        "--flatfield-heuristic",
     )
 
     run_program(
@@ -163,6 +164,7 @@ def observed_dl1_files(temp_dir_observed_files, run_summary_path):
         test_drive_report,
         '--run-summary-path',
         run_summary_path,
+        "--flatfield-heuristic",
     )
 
     run_program(
