bea16c60e2f83b64f4636ec82b18447eb227a694
chaimain <cpriyadarshi@ifae.es>
2022-04-25 11:23:13 +0200

    Support for pyirf v0.7

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
         'numba',
         'numpy<1.22.0a0',
         'pandas',
-        'pyirf~=0.6.0',
+        'pyirf~=0.7.0',
         'scipy',
         'seaborn',
         'scikit-learn~=1.0',
