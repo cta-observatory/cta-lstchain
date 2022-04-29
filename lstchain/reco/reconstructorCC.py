4de845cbfc5001b10c2103695c83f876b0ad14b1
Gabriel Emery <gabriel-emery@hotmail.fr>
2022-04-29 10:05:14 +0200

    Improving documentation and comments

--- lstchain/reco/reconstructorCC.py
+++ lstchain/reco/reconstructorCC.py
@@ -24,27 +24,33 @@ def compile_reconstructor_cc():
         """
         Performs the sum log likelihood for low luminosity pixels in TimeWaveformFitter.log_pdf
         The log likelihood is sum(pixels) sum(times) of the log single sample likelihood.
-        The single sample likelihood is the sum(possible number of pe) of a Poisson term times a gaussian term.
+        The single sample likelihood is the sum(possible number of pe) of a generalised
+        Poisson term times a Gaussian term.
 
         """
         n_pixels, n_samples = waveform.shape
         n_k = kmax - kmin
-
         sum = 0.0
         for i in range(n_pixels):
             for j in range(n_samples):
                 sum_k = 0.0
                 for k in range(n_k):
+                    # Generalised Poisson term
                     poisson = (mu[i] * pow(mu[i] + (kmin + k) * crosstalk[i], (kmin + k - 1)) / factorial[k]
                                * np.exp(-mu[i] - (kmin + k) * crosstalk[i]))
+                    # Gaussian term
                     mean = (kmin + k) * templates[i, j]
                     sigma = (kmin + k) * ((sig_s[i] * templates[i, j]) ** 2)
                     sigma = np.sqrt(error[i] * error[i] + sigma)
                     gauss = 1 / (np.sqrt(2 * np.pi) * sigma) * np.exp(
                         -(waveform[i, j] - mean) * (waveform[i, j] - mean) / 2.0 / sigma / sigma)
+                    # Add the contribution for the k+kmin number of p.e. to the single sample likelihood
                     sum_k += poisson * gauss
+                # Security to deal with negatively rounded values
                 if sum_k <= 0:
                     return -np.inf
+                # Add the log single sample likelihood to the full log likelihood
+                # An optional weight increasing high signal ample importance is supported
                 sum += weight[i, j] * np.log(sum_k)
         return sum
 
@@ -53,19 +59,22 @@ def compile_reconstructor_cc():
         """
         Performs the sum log likelihood for high luminosity pixels in TimeWaveformFitter.log_pdf
         The log likelihood is sum(pixels) sum(times) of the log single sample likelihood.
-        The single sample likelihood is a gaussian term.
+        The single sample likelihood is a Gaussian term.
 
         """
         n_pixels, n_samples = waveform.shape
         sum = 0
         for i in range(n_pixels):
             for j in range(n_samples):
+                # Log Gaussian term
                 mean = mu[i] / (1 - crosstalk[i]) * templates[i, j]
                 sigma = (mu[i] / (1 - crosstalk[i]) / (1 - crosstalk[i]) / (1 - crosstalk[i])
                          * templates[i, j] * templates[i, j])
                 sigma = np.sqrt((error[i] ** 2) + sigma)
                 log_gauss = (-(waveform[i, j] - mean) * (waveform[i, j] - mean) / 2.0 / sigma / sigma
                              - np.log(np.sqrt(2 * np.pi) * sigma))
+                # Add the log single sample likelihood to the full log likelihood
+                # An optional weight increasing high signal ample importance is supported
                 sum += weight[i, j] * log_gauss
         return sum
 
@@ -73,13 +82,13 @@ def compile_reconstructor_cc():
     def asygaussian2d(size, x, y, x_cm, y_cm, width, length, psi, rl):
         """
         Evaluate the bi-dimensional gaussian law with asymmetry along the
-        main axis
+        main axis.
 
         Parameters
         ----------
         size: float
             Integral of the 2D Gaussian
-        x, y: float or array-like
+        x, y: array-like
             Position at which the log gaussian is evaluated
         x_cm, y_cm: float
             Center of the 2D Gaussian
