6a6f09b05fd4026799ab68577a1cf5409c94f517
SeiyaNozaki <nozaki.seiya.1211@gmail.com>
2022-05-03 09:52:47 +0000

    add an option if duplicated events are removed or not

--- lstchain/tools/lstchain_create_dl3_file.py
+++ lstchain/tools/lstchain_create_dl3_file.py
@@ -141,6 +141,11 @@ class DataReductionFITSWriter(Tool):
         default_value=False,
     ).tag(config=True)
 
+    rm_duplicated_events = traits.Bool(
+        help="If True, duplicated events after alpha and gammaness cut are removed.",
+        default_value=True,
+    ).tag(config=True)
+
     classes = [EventSelector, DL3Cuts]
 
     aliases = {
@@ -161,6 +166,10 @@ class DataReductionFITSWriter(Tool):
         "source-dep": (
             {"DataReductionFITSWriter": {"source_dep": True}},
             "source-dependent analysis if True",
+        ),
+        "rm-duplicated-events": (
+            {"DataReductionFITSWriter": {"rm_duplicated_events": True}},
+            "duplicated events are removed if True",
         ),
     }
 
@@ -264,13 +273,18 @@ class DataReductionFITSWriter(Tool):
             else:
                 self.data = vstack([self.data, data_temp])
          
-        data_without_duplicated_events = remove_duplicated_events(self.data)
-        duplicated_events_ratio = (
-            len(self.data) - len(data_without_duplicated_events))/len(data_without_duplicated_events)
-        self.log.info(
-            f"{duplicated_events_ratio}% of events are duplicated after alpha and gammaness cut"
-        )
-        self.data = data_without_duplicated_events
+        if self.rm_duplicated_events:
+            if len(srcdep_assumed_positions) > 2:
+                self.log.warning(
+                    "If multiple off positions are assumed, the process to remove duplicated events can introduce a bias"
+                )
+            data_without_duplicated_events = remove_duplicated_events(self.data)
+            duplicated_events_ratio = (
+                len(self.data) - len(data_without_duplicated_events))/len(data_without_duplicated_events)
+            self.log.info(
+                f"Remove duplicated events: a ratio of duplicated events is {duplicated_events_ratio} %}"
+            )
+            self.data = data_without_duplicated_events
 
     def start(self):
 
