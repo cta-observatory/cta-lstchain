8437bc296689da5b26eee10895ccacdd439976d3
Gabriel Emery <gabriel-emery@hotmail.fr>
2022-05-31 13:03:00 +0200

    Handle both angle and distance units for the geometry used during the fit. Save uncertainties of fitted parameters in the likelihood parameter container.

--- lstchain/reco/reconstructor.py
+++ lstchain/reco/reconstructor.py
@@ -105,11 +105,20 @@ class TimeWaveformFitter(TelescopeComponent):
         """
 
         geometry = self.subarray.tel[telescope_id].camera.geometry
-        pix_x = geometry.pix_x.to_value(u.m)
-        pix_y = geometry.pix_y.to_value(u.m)
-        r_max = geometry.guess_radius().to_value(u.m)
-        pix_radius = np.sqrt(geometry.pix_area[0].to_value(u.m ** 2)/np.pi)  # find linear size of a pixel
+
+        # setup angle to distance conversion on the camera plane
         focal_length = self.subarray.tel[telescope_id].optics.equivalent_focal_length
+        angle_dist_eq = [(u.rad, u.m, lambda x: np.tan(x) * focal_length.to_value(u.m),
+                          lambda x: np.arctan(x / focal_length.to_value(u.m))),
+                         (u.rad**2, u.m**2, lambda x: (np.tan(np.sqrt(x)) * focal_length.to_value(u.m))**2,
+                          lambda x: (np.arctan(np.sqrt(x) / focal_length.to_value(u.m)))**2)]
+        u.set_enabled_equivalencies(angle_dist_eq)
+
+        unit = geometry.pix_x.unit
+        pix_x = geometry.pix_x.to_value(unit)
+        pix_y = geometry.pix_y.to_value(unit)
+        r_max = geometry.guess_radius().to_value(unit)
+        pix_radius = np.sqrt(geometry.pix_area[0].to_value(unit ** 2)/np.pi)  # find linear size of a pixel
         readout = self.subarray.tel[telescope_id].camera.readout
         sampling_rate = readout.sampling_rate.to_value(u.GHz)
         dt = (1.0 / sampling_rate)
@@ -118,6 +127,7 @@ class TimeWaveformFitter(TelescopeComponent):
         hillas_signal_pixels = event.dl1.tel[telescope_id].image_mask
         start_x_cm, start_y_cm = init_centroid(dl1_container,
                                                geometry[hillas_signal_pixels],
+                                               unit,
                                                image[hillas_signal_pixels],
                                                self.no_asymmetry
                                                )
@@ -135,6 +145,7 @@ class TimeWaveformFitter(TelescopeComponent):
         selected_gains = event.r1.tel[telescope_id].selected_gain_channel
         is_high_gain = (selected_gains == 0)
 
+        # We assume that the time gradient is given in unit of 'geometry spatial unit'/ns
         v = dl1_container.time_gradient
         psi = dl1_container.psi.to_value(u.rad)
         # We use only positive time gradients and psi is projected in [-pi,pi] from [-pi/2,pi/2]
@@ -144,13 +155,13 @@ class TimeWaveformFitter(TelescopeComponent):
             else:
                 psi = psi + np.pi
 
-        start_length = max(np.tan(dl1_container.length.to_value(u.rad)) * focal_length.to_value(u.m), 0.02)
+        start_length = max(dl1_container.length.to_value(unit), pix_radius)
         # With current likelihood computation, order and type of the parameters are important
         start_parameters = {'charge': dl1_container.intensity,
                             't_cm': dl1_container.intercept
                             - self.template_time_of_max_dict[telescope_id],
-                            'x_cm': start_x_cm.to_value(u.m),
-                            'y_cm': start_y_cm.to_value(u.m),
+                            'x_cm': start_x_cm.to_value(unit),
+                            'y_cm': start_y_cm.to_value(unit),
                             'length': start_length,
                             'wl': max(dl1_container.wl, 0.01),
                             'psi': psi,
@@ -172,13 +183,13 @@ class TimeWaveformFitter(TelescopeComponent):
         bound_parameters = {'charge': (dl1_container.intensity * 0.25,
                                        dl1_container.intensity * 4.0),
                             't_cm': (-10, t_max + 10),
-                            'x_cm': (start_x_cm.to_value(u.m)
+                            'x_cm': (start_x_cm.to_value(unit)
                                      - start_length,
-                                     start_x_cm.to_value(u.m)
+                                     start_x_cm.to_value(unit)
                                      + start_length),
-                            'y_cm': (start_y_cm.to_value(u.m)
+                            'y_cm': (start_y_cm.to_value(unit)
                                      - start_length,
-                                     start_y_cm.to_value(u.m)
+                                     start_y_cm.to_value(unit)
                                      + start_length),
                             'length': (pix_radius,
                                        min(2 * start_length, r_max)),
@@ -200,7 +211,7 @@ class TimeWaveformFitter(TelescopeComponent):
 
         p_x = pix_x[mask_pixel]
         p_y = pix_y[mask_pixel]
-        pix_area = geometry.pix_area[mask_pixel].to_value(u.m ** 2)
+        pix_area = geometry.pix_area[mask_pixel].to_value(unit ** 2)
 
         data = waveform
         error = None  # TODO include option to use calibration data
@@ -230,17 +241,17 @@ class TimeWaveformFitter(TelescopeComponent):
         self.names_parameters = start_parameters.keys()
         self.bound_parameters = bound_parameters
 
-        return focal_length, fit_params
+        return unit, fit_params
 
     def __call__(self, event, telescope_id, dl1_container):
         self.start_parameters = None
         self.names_parameters = None
-        focal_length, fit_params = self.call_setup(event, telescope_id, dl1_container)
+        unit_cam, fit_params = self.call_setup(event, telescope_id, dl1_container)
         self.end_parameters = None
         self.error_parameters = None
         self.fcn = None
 
-        return self.predict(focal_length, fit_params)
+        return self.predict(unit_cam, fit_params)
 
     def clean_data(self, pix_x, pix_y, pix_radius, times, start_parameters, telescope_id):
         """
@@ -352,19 +363,16 @@ class TimeWaveformFitter(TelescopeComponent):
         m.simplex().migrad()
         self.end_parameters = m.values.to_dict()
         self.fcn = m.fval
-        try:
-            self.error_parameters = m.errors.to_dict()
-        except (KeyError, AttributeError, RuntimeError):
-            self.error_parameters = {key: np.nan for key in self.names_parameters}
+        self.error_parameters = m.errors.to_dict()
 
-    def predict(self, focal_length, fit_params):
+    def predict(self, unit_cam, fit_params):
         """
             Call the fitting procedure and fill the results.
 
         Parameters
         ----------
-        focal_length: float
-            Telescope focal length used to convert meters in the camera frame to degrees in the sky
+        unit_cam: astropy.units.unit
+            Unit used for the camera geometry and for spatial variable in the fit
         fit_params: array
             Parameters used to compute the likelihood but not fitted
 
@@ -379,8 +387,10 @@ class TimeWaveformFitter(TelescopeComponent):
             self.fit(fit_params)
             container.lhfit_TS = self.fcn
 
-            container.lhfit_x = self.end_parameters['x_cm'] * u.m
-            container.lhfit_y = self.end_parameters['y_cm'] * u.m
+            container.lhfit_x = (self.end_parameters['x_cm']*unit_cam).to(u.m)
+            container.lhfit_x_uncertainty = (self.error_parameters['x_cm']*unit_cam).to(u.m)
+            container.lhfit_y = (self.end_parameters['y_cm']*unit_cam).to(u.m)
+            container.lhfit_y_uncertainty = (self.error_parameters['y_cm']*unit_cam).to(u.m)
             container.lhfit_r = np.sqrt(container.lhfit_x ** 2 + container.lhfit_y ** 2)
             container.lhfit_phi = np.arctan2(container.lhfit_y, container.lhfit_x)
             if self.end_parameters['psi'] > np.pi:
@@ -388,22 +398,30 @@ class TimeWaveformFitter(TelescopeComponent):
             if self.end_parameters['psi'] < -np.pi:
                 self.end_parameters['psi'] += 2 * np.pi
             container.lhfit_psi = self.end_parameters['psi'] * u.rad
+            container.lhfit_psi_uncertainty = self.error_parameters['psi'] * u.rad
             length_asy = 1 + self.end_parameters['rl'] if self.end_parameters['rl'] >= 0 else 1 / (
                     1 - self.end_parameters['rl'])
-            lhfit_length_m = ((1.0 + length_asy)
-                              * self.end_parameters['length'] / 2.0) * u.m
-            container.lhfit_length = np.rad2deg(np.arctan(lhfit_length_m/focal_length))
+            lhfit_length = (((1.0 + length_asy)
+                             * self.end_parameters['length'] / 2.0) * unit_cam).to(u.deg)
+            container.lhfit_length = lhfit_length
+            lhfit_length_rel_err = self.error_parameters['length'] / self.end_parameters['length']
+            # We assume that the relative error is the same in the fitted and saved unit
+            container.lhfit_length_uncertainty = lhfit_length_rel_err * container.lhfit_length
             container.lhfit_width = self.end_parameters['wl'] * container.lhfit_length
 
             container.lhfit_time_gradient = self.end_parameters['v']
+            container.lhfit_time_gradient_uncertainty = self.error_parameters['v']
             container.lhfit_ref_time = self.end_parameters['t_cm']
+            container.lhfit_ref_time_uncertainty = self.error_parameters['t_cm']
 
             container.lhfit_wl = u.Quantity(self.end_parameters['wl'])
-            container.lhfit_intensity = self.end_parameters['charge']
+            container.lhfit_wl_uncertainty = u.Quantity(self.error_parameters['wl'])
+            container.lhfit_intensity_uncertainty = self.error_parameters['charge']
             container.lhfit_log_intensity = np.log10(container.lhfit_intensity)
             container.lhfit_t_68 = container.lhfit_length.value * container.lhfit_time_gradient
             container.lhfit_area = container.lhfit_length * container.lhfit_width
             container.lhfit_length_asymmetry = self.end_parameters['rl']
+            container.lhfit_length_asymmetry_uncertainty = self.error_parameters['rl']
         except ZeroDivisionError:
             # TODO Check occurrence rate and solve
             container = DL1LikelihoodParametersContainer(lhfit_call_status=-1)
@@ -439,7 +457,7 @@ class TimeWaveformFitter(TelescopeComponent):
         return np.sum(llh)
 
 
-def init_centroid(dl1_container, geom, image, no_asymmetry):
+def init_centroid(dl1_container, geom, unit, image, no_asymmetry):
     """
     Initialise the centroid position for the likelihood fit to the Hillas
     parameter centroid or using a centroid weighting as the signal squared
@@ -451,6 +469,8 @@ def init_centroid(dl1_container, geom, image, no_asymmetry):
         Hillas parameter container
     geom: `ctapipe.instrument.CameraGeometry`
         Camera geometry
+    unit: astropy.unit
+        Unit used to describe spatial quantities
     image : array_like
         Charge in each pixel
     no_asymmetry: bool
@@ -458,12 +478,12 @@ def init_centroid(dl1_container, geom, image, no_asymmetry):
 
     """
     if no_asymmetry:
-        return dl1_container.x, dl1_container.y
+        return dl1_container.x.to(unit), dl1_container.y.to(unit)
     else:
-        return asy_centroid(geom, image)
+        return asy_centroid(geom, unit, image)
 
 
-def asy_centroid(geom, image):
+def asy_centroid(geom, unit, image):
     """
     Compute a centroid weighting as the signal squared for asymmetric models
 
@@ -471,6 +491,8 @@ def asy_centroid(geom, image):
     ----------
     geom: `ctapipe.instrument.CameraGeometry`
         Camera geometry
+    unit: astropy.unit
+        Unit used to describe spatial quantities
     image : array_like
         Charge in each pixel
 
@@ -479,7 +501,6 @@ def asy_centroid(geom, image):
     (x_cm, y_cm): `astropy.units.Quantity`
 
     """
-    unit = geom.pix_x.unit
     pix_x = geom.pix_x.to_value(unit)
     pix_y = geom.pix_y.to_value(unit)
     image = np.asanyarray(image, dtype=np.float64)
