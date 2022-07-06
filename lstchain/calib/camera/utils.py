5ead55b7cf51030b1e884e7dac5f143e5d5e6224
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2022-07-06 11:40:15 +0000

    Always report total number of outliers

--- lstchain/calib/camera/utils.py
+++ lstchain/calib/camera/utils.py
@@ -6,13 +6,14 @@ def check_outlier_mask(outliers, log, kind):
     and create a warning message.
     """
     counts = np.count_nonzero(outliers, axis=-1)
+    log.info("Total number of outliers in %s sample: %d", kind, np.count_nonzero(outliers))
     for n_pixels_threshold in (10, 50):
         bad_index, bad_gain = np.nonzero(counts > n_pixels_threshold)
         if len(bad_index) >= 3:
             log.warning("Found large number of outliers in %s sample:", kind)
             log.warning(
-                "Outliers: %d events with more than %d pixels, %d total outliers",
-                len(bad_index), n_pixels_threshold, np.count_nonzero(outliers)
+                "Outliers: %d events with more than %d pixels",
+                len(bad_index), n_pixels_threshold
             )
             log.warning("Bad Index: %s", bad_index)
             log.warning("Bad gain: %s", bad_gain)
