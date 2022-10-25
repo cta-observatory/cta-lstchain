806e26f951e15918ad69ec439d6f75e3334ae618
chaimain <cpriyadarshi@ifae.es>
2022-10-25 16:00:46 +0200

    file-pattern arguments always within quotation marks

--- lstchain/tools/lstchain_create_dl3_index_files.py
+++ lstchain/tools/lstchain_create_dl3_index_files.py
@@ -35,14 +35,14 @@ class FITSIndexWriter(Tool):
     > lstchain_create_dl3_index_files
         -d /path/to/DL3/files/
         -o /path/to/DL3/index/files
-        -p dl3*[run_1-run_n]*.fits
+        -p "dl3*[run_1-run_n]*.fits"
         --overwrite
 
     Or if the DL3 files are stored in sub-directories:
     > lstchain_create_dl3_index_files
        -d /path/to/DL3/files/
        -o /path/to/DL3/index/files
-       -p /sub-directory*/dl3*[run_1-run_n]*.fits
+       -p "/sub-directory*/dl3*[run_1-run_n]*.fits"
        --overwrite
 
     Or if the DL3 files are stored in the current directory:
