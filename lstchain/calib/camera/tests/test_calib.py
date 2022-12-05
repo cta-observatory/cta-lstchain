755c369d19aeafad766811dafe2d8b99d81d769c
Maximilian Linhoff <maximilian.linhoff@tu-dortmund.de>
2022-12-05 14:59:07 +0100

    Make tests run again, not pass though ;)

--- lstchain/calib/camera/tests/test_calib.py
+++ lstchain/calib/camera/tests/test_calib.py
@@ -1,40 +1,39 @@
-from lstchain.calib import load_calibrator_from_config, load_gain_selector_from_config, load_image_extractor_from_config
+import pytest
+
 from ctapipe.instrument import SubarrayDescription, TelescopeDescription
+from lstchain.calib import load_calibrator_from_config, load_gain_selector_from_config, load_image_extractor_from_config
 from lstchain.io.config import get_standard_config
 
-subarray = SubarrayDescription(
-    "LST-1",
-    tel_positions={1: None},
-    tel_descriptions={
-        1: TelescopeDescription.from_name("LST", "LSTCam")
-    },
-)
+@pytest.fixture
+def lst1_subarray():
+    from ctapipe_io_lst import LSTEventSource
+    return LSTEventSource.create_subarray(tel_id=1)
 
-def test_load_calibrator_from_config():
+def test_load_calibrator_from_config(lst1_subarray):
     from lstchain.io.config import get_standard_config
     from ctapipe.calib import CameraCalibrator
     config = get_standard_config()
-    cal = load_calibrator_from_config(config, subarray)
+    cal = load_calibrator_from_config(config, lst1_subarray)
     assert isinstance(cal, CameraCalibrator)
 
 
-def test_load_calibrator_from_config_LocalPeakWindowSum():
+def test_load_calibrator_from_config_LocalPeakWindowSum(lst1_subarray):
     config = {"image_extractor": "LocalPeakWindowSum"}
-    cal = load_calibrator_from_config(config, subarray)
+    cal = load_calibrator_from_config(config, lst1_subarray)
     assert cal.image_extractor_type.tel[1] == 'LocalPeakWindowSum'
 
 
-def test_load_calibrator_from_config_GlobalPeakWindowSum():
+def test_load_calibrator_from_config_GlobalPeakWindowSum(lst1_subarray):
     config = {"image_extractor": "GlobalPeakWindowSum"}
-    cal = load_calibrator_from_config(config, subarray)
+    cal = load_calibrator_from_config(config, lst1_subarray)
     assert cal.image_extractor_type.tel[1] == 'GlobalPeakWindowSum'
 
 
-def test_load_image_extractor_from_config():
+def test_load_image_extractor_from_config(lst1_subarray):
     from ctapipe.image import LocalPeakWindowSum
 
     config = get_standard_config()
-    image_extractor = load_image_extractor_from_config(config, subarray)
+    image_extractor = load_image_extractor_from_config(config, lst1_subarray)
 
     assert isinstance(image_extractor, LocalPeakWindowSum)
     assert image_extractor.window_shift[0][2] == 4
@@ -47,7 +46,7 @@ def test_load_image_extractor_from_config():
               }
     }
 
-    image_extractor = load_image_extractor_from_config(config, subarray)
+    image_extractor = load_image_extractor_from_config(config, lst1_subarray)
 
     assert isinstance(image_extractor, LocalPeakWindowSum)
     assert image_extractor.window_shift[0][2] == 1
