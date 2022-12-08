58bdb4faaf01265381d4db9236e17ee966251ae4
Thomas Vuillaume <thomas.vuillaume@lapp.in2p3.fr>
2022-12-08 17:16:48 +0100

    commiting to quickly

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
         'gammapy~=0.19.0',
         'h5py',
         'iminuit>=2',
-        'joblib==1.1',
+        'joblib==1.2',
         'matplotlib~=3.5',
         'numba',
         'numpy<1.22.0a0',
