dcc6a6854830a547051fa90b8b27e1fb8af5a7f5
chaimain <cpriyadarshi@ifae.es>
2023-04-18 16:16:49 +0200

    Roll-back to pyirf v0.7 for the time being

--- lstchain/high_level/interpolate.py
+++ lstchain/high_level/interpolate.py
@@ -16,7 +16,7 @@ from pyirf.interpolation import (
     interpolate_effective_area_per_energy_and_fov,
     interpolate_energy_dispersion,
     interpolate_psf_table,
-    interpolate_rad_max,
+    # interpolate_rad_max,
 )
 from scipy.spatial import Delaunay, distance
 from scipy.interpolate import griddata
@@ -304,6 +304,38 @@ def interpolate_gh_cuts(
     """
 
     return griddata(grid_points, gh_cuts, target_point, method=method)
+
+
+def interpolate_rad_max(
+    rad_max, grid_points, target_point, method="linear",
+):
+    """
+    Interpolates a grid of RAD_MAX tables to a target-point.
+    Wrapper around scipy.interpolate.griddata [1].
+
+    Parameters
+    ----------
+    rad_max: numpy.ndarray, shape=(N, M, ...)
+        Theta-cuts for all combinations of grid-points, energy and
+        fov_offset.
+        Shape (N:n_grid_points, M:n_energy_bins, n_fov_offset_bins)
+    grid_points: numpy.ndarray, shape=(N, O)
+        Array of the N O-dimensional morphing parameter values corresponding
+        to the N input templates.
+    target_point: numpy.ndarray, shape=(O)
+        Value for which the interpolation is performed (target point)
+    method: 'linear’, ‘nearest’, ‘cubic’
+        Interpolation method for scipy.interpolate.griddata [1].
+        Defaults to 'linear'.
+
+    Returns
+    -------
+    rad_max_interp: numpy.ndarray, shape=(1, M, ...)
+        Theta-cuts for the target grid-point,
+        shape (1, M:n_energy_bins, n_fov_offset_bins)
+    """
+
+    return griddata(grid_points, rad_max, target_point, method=method)
 
 
 def interpolate_irf(irfs, data_pars, interp_method="linear"):
