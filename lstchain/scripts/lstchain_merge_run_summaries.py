ce55118df3657f442a48d78153ba33ab114e6543
Daniel Morcuende <dmorcuende@iaa.es>
2023-09-13 17:26:08 +0200

    update to v0.10 and include date option

--- lstchain/scripts/lstchain_merge_run_summaries.py
+++ lstchain/scripts/lstchain_merge_run_summaries.py
@@ -1,16 +1,23 @@
-from pathlib import Path
+"""
+Create a summary of all runs from daily run summaries,
+adding pointing information.
+
+It is also possible to append a single night to an already
+existing merged summary file when date is especified.
+"""
+
 import logging
 from argparse import ArgumentParser
 from datetime import datetime
+from pathlib import Path
 
-import numpy as np
-from astropy.table import vstack
-from astropy.time import Time
 import astropy.units as u
-
-from ctapipe_io_lst.pointing import PointingSource
-from ctapipe_io_lst.event_time import read_run_summary
+import numpy as np
+from astropy.table import Table, vstack
+from astropy.time import Time
 from ctapipe_io_lst import LSTEventSource
+from ctapipe_io_lst.event_time import read_run_summary
+from ctapipe_io_lst.pointing import PointingSource
 
 log = logging.getLogger('create_run_overview')
 
@@ -27,9 +34,19 @@ parser = ArgumentParser()
 parser.add_argument('output_file', type=Path)
 parser.add_argument('-m', '--monitoring-path', type=Path, default=base_path)
 parser.add_argument('-v', '--verbose', action='store_true')
+# Option to only append a single night to an existing merged summary file.
+# By default, the whole directory with nightly summaries is processed.
+parser.add_argument(
+    '-d',
+    '--date',
+    type=str,
+    default=None,
+    help='Date in YYYYMMDD format. When the date is given append only '
+    'the summary of that night to an existing merged summary file',
+)
 
 
-SUBARRAY = LSTEventSource.create_subarray(geometry_version=4)
+SUBARRAY = LSTEventSource.create_subarray()
 
 
 def get_pointing_info(times, drive_report):
@@ -39,10 +56,11 @@ def get_pointing_info(times, drive_report):
     )
     try:
         pointing_source._read_drive_report_for_tel(1)
-        valid = pointing_source.drive_report[1]['unix_time'] != 0
-        pointing_source.drive_report[1] = pointing_source.drive_report[1][valid]
+        valid = pointing_source.drive_log[1]['unix_time'] != 0
+        pointing_source.drive_log[1] = pointing_source.drive_log[1][valid]
         if np.count_nonzero(valid) < 2:
             raise ValueError('Not enough values')
+
     except:
         return {
             'ra': np.full(len(times), np.nan) * u.deg,
@@ -94,7 +112,19 @@ def merge_run_summary_with_pointing(run_summary, drive_report):
 
     table['run_start'].format = 'isot'
     # reorder columns
-    table = table[['date', 'run_id', 'run_type', 'n_subruns', 'run_start', 'ra', 'dec', 'alt', 'az']]
+    table = table[
+        [
+            'date',
+            'run_id',
+            'run_type',
+            'n_subruns',
+            'run_start',
+            'ra',
+            'dec',
+            'alt',
+            'az',
+        ]
+    ]
     return table
 
 
@@ -106,13 +136,30 @@ def main():
     else:
         logging.basicConfig(level=logging.INFO)
 
+    if args.date is not None:
+        # Append only the run summary corresponding to this date
+        night = datetime.strptime(args.date, '%Y%m%d')
+        if not args.output_file.is_file():
+            raise ValueError(f'Output file {args.output_file} does not exist.')
 
+        log.info(f'Appending {args.date} to {args.output_file}')
+        run_summary_dir = args.monitoring_path / 'RunSummary'
+        drive_report_dir = args.monitoring_path / 'DrivePositioning'
+        run_summary = run_summary_dir / f'RunSummary_{args.date}.ecsv'
+        drive_report = drive_report_dir / f'DrivePosition_log_{args.date}.txt'
+        summary = merge_run_summary_with_pointing(run_summary, drive_report)
+        if summary is not None:
+            old_summary = Table.read(args.output_file)
+            new_summary = vstack([old_summary, summary])
+            new_summary.write(args.output_file, overwrite=True)
+        return
+
+    # Otherwise, merge all summary files found
     run_summary_dir = args.monitoring_path / 'RunSummary'
     drive_report_dir = args.monitoring_path / 'DrivePositioning'
 
     run_summaries = sorted(run_summary_dir.glob('RunSummary*.ecsv'))
     log.info('Found %d run summaries', len(run_summaries))
-
 
     summaries = []
 
@@ -120,7 +167,7 @@ def main():
         log.debug('Processing %s', run_summary)
         night = datetime.strptime(run_summary.stem, 'RunSummary_%Y%m%d')
 
-        drive_report = drive_report_dir / f'drive_log_{night:%y_%m_%d}.txt'
+        drive_report = drive_report_dir / f'DrivePosition_log_{night:%Y%m%d}.txt'
 
         if not drive_report.is_file():
             log.error(f'No drive report found for {night:%Y-%m-%d}')
