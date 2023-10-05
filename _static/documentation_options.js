baa872b0a36183cf11cc5caf4dbae73382013517
vuillaut <thomas.vuillaume@lapp.in2p3.fr>
2023-10-05 14:15:49 +0000

    Deploying to gh-pages from @ cta-observatory/cta-lstchain@675b24544aa0c4c7c0c8f7739ee3a42c91ac3ec1 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.10.5.dev40+gbb39662e',
+    VERSION: '0.10.5.dev51+g675b2454',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
