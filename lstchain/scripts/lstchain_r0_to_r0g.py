6bc41e05b07e3f9239010bd59b20c9c08a9e728c
Abelardo Moralejo <moralejo@ifae.es>
2024-02-20 18:34:42 +0100

    Update lstchain_r0_to_r0g.py
    Addressed reviewer's comments

--- lstchain/scripts/lstchain_r0_to_r0g.py
+++ lstchain/scripts/lstchain_r0_to_r0g.py
@@ -13,10 +13,12 @@ given the occasional problems we have with the FF event tagging)
 import logging
 import protozfits
 import argparse
+import sys
 
 from ctapipe.io import EventSource
 from ctapipe.containers import EventType
 from lstchain.io import standard_config
+import lstchain.paths as paths
 from traitlets.config import Config
 
 import numpy as np
@@ -36,52 +38,62 @@ parser.add_argument('-o', '--output-dir', dest='output_dir',
 SAMPLE_START = 3
 SAMPLE_END = 39
 
+# Level of high gain (HG) required for switching to low gain (LG)
+THRESHOLD = 3900
+
+# Events for which gain selection will be applied:
+EVENT_TYPES_TO_REDUCE = [EventType.SUBARRAY, EventType.UNKNOWN]
+
 def main():
     args = parser.parse_args()
-
-    # Level of high gain (HG) required for switching to low gain (LG)
-    THRESHOLD = 3900
-
+  
     input_file = args.input_file
     output_dir = args.output_dir
-
+  
+    log = logging.getLogger("lstchain_r0_to_r0g")
+  
     # First identify properly interleaved pedestals (also in case there are
     # ucts jumps) and FF events (heuristically):
-    event_id, event_type = get_event_types(input_file)
+    event_type = get_event_types(input_file)
+    event_type_val = np.array([x.value for x in event_type.values()])
 
-    event_id = np.array(event_id)
-    event_type_val = np.array([x.value for x in event_type])
-
-    logging.info('Identified event types and number of events:')
-    for j in np.unique(event_type_val):
-        logging.info(f'{EventType(j)}:, {np.sum(event_type_val == j)}\n')
+    log.info('Identified event types and number of events:')
+    evtype, counts = np.unique(event_type_val, return_counts=True)
+    for j, n in zip(evtype, counts):
+        log.info('%s: %d', EventType(j), n)
 
     # Now loop over the files (4 streams) again to perform the actual gain
     # selection:
-    k = input_file.find('/LST-1.')
-    input_stream_names = [input_file[:k+7]+str(id_stream+1)+input_file[k+8:]
+    
+    run_info = pth.parse_r0_filename(input_file)
+    input_stream_names = [pth.Path(pth.Path(input_file).parent,
+                                   pth.run_to_r0_filename(run_info.tel_id, 
+                                                          run_info.run,
+                                                          run_info.subrun, 
+                                                          id_stream))
                           for id_stream in range(4)]
-    output_stream_names = [output_dir + name[k:] for name in input_stream_names]
+    output_stream_names = [pth.Path(output_dir, pth.Path(inputsn).name) 
+                           for inputsn in input_stream_names]
 
     input_streams = []
     for name in input_stream_names:
         input_streams.append(protozfits.File(name, pure_protobuf=True))
 
     try:
-        num_pixels = input_streams[0].CameraConfiguration[0].num_pixels
-        num_samples = input_streams[0].CameraConfiguration[
-            0].num_samples_nominal
-    except:
-        logging.error('CameraConfiguration not found! Is this R1v1 data?')
-        exit(1)
+      camera_config = input_streams[0].CameraConfiguration[0]
+    except (AttributeError, IndexError):
+      log.error("CameraConfiguration not found! Is this R1v1 data?')
+      sys.exit(1)
+
+    num_pixels = camera_config.num_pixels
+    num_samples = camera_config.num_samples_nominal
+
 
     with ExitStack() as stack:
         for i, name in enumerate(output_stream_names):
-
-            n_tiles = input_streams[i].Events.protobuf_i_fits.header[
-                'NAXIS2'].value
-            rows_per_tile = input_streams[i].Events.protobuf_i_fits.header[
-                'ZTILELEN'].value
+            header = input_streams[i].Events.header
+            n_tiles = header["NAXIS2"]
+            rows_per_tile = header["ZTILELEN"]
 
             stream = stack.enter_context(protozfits.ProtobufZOFits(
                     n_tiles=n_tiles,
@@ -90,10 +102,11 @@ def main():
                     defaul_compression="lst"))
             stream.open(name)
 
-            stream.move_to_new_table("CameraConfiguration")
-            stream.write_message(input_streams[i].CameraConfiguration[0])
             stream.move_to_new_table("DataStream")
             stream.write_message(input_streams[i].DataStream[0])
+          
+            stream.move_to_new_table("CameraConfiguration")
+            stream.write_message(input_streams[i].CameraConfiguration[0])
 
             stream.move_to_new_table("Events")
 
@@ -102,19 +115,16 @@ def main():
                 if event.event_id == 0:
                     continue
                 # Get the event type, from the first loop over the files:
-                evtype = event_type_val[event_id==event.event_id][0]
+                evtype = event_type[event.event_id]
+              
+                if evtype in EVENT_TYPES_TO_REDUCE:
+                    if event.num_channels != 2:
+                        log.error('You are attempting gain selection on '
+                                      'data with only one gain!')
+                        sys.exit(1)
 
-                if ((evtype == EventType.SUBARRAY.value) |
-                    (evtype == EventType.UNKNOWN.value)):
                     # Find pixels with HG above gain switch threshold:
                     wf = protozfits.any_array_to_numpy(event.waveform)
-                    num_gains = int(wf.size / num_pixels / num_samples)
-
-                    if num_gains != 2:
-                        logging.error('You are attempting gain selection on '
-                                      'data with only one gain!')
-                        exit(1)
-
                     wf = wf.reshape((num_gains, num_pixels, num_samples))
                     hg = wf[0]
                     lg = wf[1]
@@ -136,11 +146,13 @@ def main():
             stream.close()
             input_streams[i].close()
 
-    logging.info('R0 to R0G conversion finished successfully!')
-    return(0)
+    log.info('R0 to R0G conversion finished successfully!')
+    
 
 
 def get_event_types(input_file):
+
+    log = logging.getLogger("lstchain_r0_to_r0g")
 
     # For heuristic flat field identification (values refer to
     # baseline-subtracted HG integrated waveforms):
@@ -153,21 +165,21 @@ def get_event_types(input_file):
     standard_config['source_config']['LSTEventSource'][
         'pointing_information'] = False
 
-    event_type = []
-    event_id = []
+    event_type = dict()
+  
     with EventSource(input_url=input_file,
                      config=Config(standard_config['source_config'])) as source:
         source.pointing_information = False
         source.trigger_information = True
-        source.log.setLevel(logging.WARNING)
+        source.log.setLevel(log.WARNING)
 
         offset = source.data_stream.waveform_offset 
         try:
             for event in source:
                 if event.r0.tel[1].waveform is None:
-                    logging.error('The data seem to contain no R0 waveforms. '
+                    log.error('The data seem to contain no R0 waveforms. '
                                   'Is this already gain-selected data?')
-                    exit(1)
+                    sys.exit(1)
 
                 # Check if this may be a FF event
                 # Subtract baseline offset (convert to signed integer to
@@ -178,27 +190,27 @@ def get_event_types(input_file):
                 wf_hg_sum = np.sum(wf_hg, axis=1)
                 ff_pix = ((wf_hg_sum > MIN_FLATFIELD_ADC) &
                           (wf_hg_sum < MAX_FLATFIELD_ADC))
-                event_type.append(event.trigger.event_type)
 
+                evtype = event.trigger.event_type
                 # Check fraction of pixels with HG in "FF-like range"
                 if ff_pix.sum() / event.r0.tel[1].waveform.shape[1] > \
                         MIN_FLATFIELD_PIXEL_FRACTION:
                     # Looks like a FF event:
-                    event_type[-1] = EventType.FLATFIELD
-                elif event_type[-1] == EventType.FLATFIELD:
+                    evtype = EventType.FLATFIELD
+                elif evtype == EventType.FLATFIELD:
                     # If originally tagged as FF, but not looking like FF:
-                    event_type[-1] = EventType.UNKNOWN
+                    evtype = EventType.UNKNOWN
 
-                event_id.append(event.index.event_id)
+                event_type[event.index.event_id] = evtype
 
-            logging.info('Finished first loop over input files - all ok!')
+            log.info('Finished first loop over input files - all ok!')
 
         except Exception as err:
-            logging.error(err)
-            logging.error('Something went wrong!')
-            exit(1)
+            log.error(err)
+            log.error('Something went wrong!')
+            sys.exit(1)
 
-    return event_id, event_type
+    return event_type
 
 
 
