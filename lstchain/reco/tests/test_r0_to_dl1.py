6f0ab0558ee40838e12ae726205d6e40133cdd9a
Thomas Vuillaume <thomas.vuillaume@gmail.com>
2024-05-17 09:39:56 +0200

    Use None in test to make sure the default file is will applied

--- lstchain/reco/tests/test_r0_to_dl1.py
+++ lstchain/reco/tests/test_r0_to_dl1.py
@@ -25,5 +25,5 @@ def test_r0_to_dl1_nsb_tuning(tmp_path, mc_gamma_testfile):
     config = deepcopy(standard_config)
     config['source_config']['EventSource']['allowed_tels'] = [1]
     config['waveform_nsb_tuning']['nsb_tuning'] = True
-    config['waveform_nsb_tuning']['spe_location'] = get_resource_path("data/SinglePhE_ResponseInPhE_expo2Gaus.dat")
+    config['waveform_nsb_tuning']['spe_location'] = None  ## If None, the default file data/SinglePhE_ResponseInPhE_expo2Gaus.dat is used
     r0_to_dl1(mc_gamma_testfile, custom_config=config, output_filename=tmp_path / "tmp.h5")
