27e43fd56650d2da5e22aafb2cf4cfd7bf02c7a0
Abelardo Moralejo <moralejo@ifae.es>
2024-06-19 18:56:43 +0200

    Update lstchain/scripts/lstchain_dvr_pixselector.py
    Co-authored-by: Daniel Morcuende <dmorcuende@iaa.es>

--- lstchain/scripts/lstchain_dvr_pixselector.py
+++ lstchain/scripts/lstchain_dvr_pixselector.py
@@ -702,7 +702,7 @@ def get_typical_dvr_min_charge(dvrtable):
     if counts.max() / counts.sum() < min_fraction_of_good_subruns:
         log.warning('Unstable data? (noise-wise) Less than half of the subruns '
                  'had similar noise conditions!')
-        log.warn('Range of min_charge_for_certain_selection: %.1f - %.1f',
+        log.warning('Range of min_charge_for_certain_selection: %.1f - %.1f',
                  allqs.min(), allqs.max())
 
     mode = value[np.argmax(counts)]
