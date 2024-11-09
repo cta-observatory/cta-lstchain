e965a4e796566ead675234ed0f8d7a8a352f66c9
Maximilian Linhoff <maximilian.linhoff@tu-dortmund.de>
2024-11-09 20:38:29 +0100

    Update ctapipe

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     install_requires=[
         'astropy~=5.0',
         'bokeh~=2.0',
-        'ctapipe~=0.19.2',
+        'ctapipe~=0.21.2',
         'ctapipe_io_lst~=0.24.0',
         'ctaplot~=0.6.4',
         'eventio>=1.9.1,<2.0.0a0',  # at least 1.1.1, but not 2
