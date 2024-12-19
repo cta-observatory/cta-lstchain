8fd938c42fb9773d2631877b0243d466df0608a2
Abelardo Moralejo <moralejo@ifae.es>
2024-12-19 16:52:17 +0100

    Small change in logging message

--- lstchain/scripts/lstchain_dl1ab.py
+++ lstchain/scripts/lstchain_dl1ab.py
@@ -230,7 +230,7 @@ def main():
         optics = subarray_info.tel[tel_id].optics
         camera_geom = subarray_info.tel[tel_id].camera.geometry
     except OSError:
-        print("subarray table is not readable because of the version incompatibility.")
+        print("Subarray description table is not readable because of version incompatibility.")
         print("The standard LST optics and camera geometry will be used.")
         optics = OPTICS
         camera_geom = load_camera_geometry()
