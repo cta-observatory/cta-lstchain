2d5d21dd496849df58088bdfd4e4ce935ad3ae82
rlopezcoto <rlopezcoto@gmail.com>
2025-01-23 20:45:37 +0000

    Deploying to gh-pages from @ cta-observatory/cta-lstchain@51b1027701437bfa6788f9adf97f85fafe153509 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.10.15.dev33+g035f60fe',
+    VERSION: '0.10.15.dev35+g51b10277',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
