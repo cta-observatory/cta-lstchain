0aa24f185eb5dc2b988eb7b7fb267e9d35370430
Abelardo Moralejo <moralejo@ifae.es>
2025-04-24 19:11:21 +0200

    Adapt to ctapipe extractor api change

--- lstchain/calib/camera/pedestals.py
+++ lstchain/calib/camera/pedestals.py
@@ -138,7 +138,8 @@ class PedestalIntegrator(PedestalCalculator):
         if self.extractor:
             broken_pixels = event.mon.tel[self.tel_id].pixel_status.hardware_failing_pixels
          
-            dl1 = self.extractor(waveforms, self.tel_id, no_gain_selection, 
+            dl1 = self.extractor(waveforms, self.tel_id, 
+                                 selected_gain_channel=None, 
                                  broken_pixels=broken_pixels)
             charge = dl1.image
             peak_pos = dl1.peak_time
