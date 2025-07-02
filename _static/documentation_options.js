7d569773ddb388aa7e9af9103bcc9fc9df4fbe97
moralejo <moralejo@ifae.es>
2025-07-02 12:58:38 +0000

    Deploying to gh-pages from @ cta-observatory/cta-lstchain@d9677f606fbfa54fcb539fd976ac15191cb4634c 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.10.20.dev2+g995b4820',
+    VERSION: '0.10.20.dev4+gd9677f60',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
