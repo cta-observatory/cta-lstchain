635084d4b358396adeb16f394a204d8a3fc79e84
Elisa-Visentin <121040436+Elisa-Visentin@users.noreply.github.com>
2026-09-10 11:23:45 +0200

    Update modifier.py

--- lstchain/image/modifier.py
+++ lstchain/image/modifier.py
@@ -216,7 +216,7 @@ def calculate_noise_parameters(simtel_filename, data_dl1_filename,
         
         # Locate pixels with HG declared unusable either in original calibration or
         # in interleaved events:
-        bad_pixels = unusable[0][0]  # original calibration
+        bad_pixels = unusable[0]  # original calibration
         if unusable.shape[0] > 1:
             for tf in unusable[1:][0]:  # calibrations with interleaveds
                 bad_pixels = np.logical_or(bad_pixels, tf)
